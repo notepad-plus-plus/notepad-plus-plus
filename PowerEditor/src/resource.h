3412c5ce5d42e26f02dbe6860c6d42a202ac8f93
donho <donho@f5eea248-9336-0410-98b8-ebc06183d4e3>
2008-01-14 19:40:10 +0000

    [RELEASE_475] Fix invisible tabs bug while closing (Finally).
    git-svn-id: svn://svn.tuxfamily.org/svnroot/notepadplus/repository@104 f5eea248-9336-0410-98b8-ebc06183d4e3

--- PowerEditor/src/resource.h
+++ PowerEditor/src/resource.h
@@ -18,9 +18,9 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-#define NOTEPAD_PLUS_VERSION	"Notepad++ v4.7.4"
-#define VERSION_VALUE "4.74\0"  // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71 
-#define VERSION_DIGITALVALUE 4, 7, 4, 0
+#define NOTEPAD_PLUS_VERSION	"Notepad++ v4.7.5"
+#define VERSION_VALUE "4.75\0"  // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71 
+#define VERSION_DIGITALVALUE 4, 7, 5, 0
 
 #ifndef IDC_STATIC
 #define IDC_STATIC	-1

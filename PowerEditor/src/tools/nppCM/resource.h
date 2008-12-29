fdd736daa0475f3019f8a437dddefa69153b6db2
Don Ho <don.h@free.fr>
2008-12-29 01:32:52 +0000

    [ADD] Add a new Unicode compliant nppCM project.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/notepadplus/repository@377 f5eea248-9336-0410-98b8-ebc06183d4e3

---
+++ PowerEditor/src/tools/nppCM/resource.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by wscitecm.rc
+//
+#define IDI_SCITE                       101
+#define IDB_NPP                       102
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

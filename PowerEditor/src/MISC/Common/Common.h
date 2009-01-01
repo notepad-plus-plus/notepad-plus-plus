f7a1301d04636afca1a902149512c0e8e3d5fdd7
donho <donho@f5eea248-9336-0410-98b8-ebc06183d4e3>
2009-01-01 20:22:03 +0000

    [CLEAN] Remove redundant AddFontResource call.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/notepadplus/repository@382 f5eea248-9336-0410-98b8-ebc06183d4e3

--- PowerEditor/src/MISC/Common/Common.h
+++ PowerEditor/src/MISC/Common/Common.h
@@ -89,7 +89,7 @@ void systemMessage(const TCHAR *title);
 void printInt(int int2print);
 void printStr(const TCHAR *str2print);
 void printMsg(const TCHAR *msg2print, const TCHAR *title, DWORD flags = MB_OK);
-void writeLog(const TCHAR *logFileName, const TCHAR *log2write);
+void writeLog(const TCHAR *logFileName, const char *log2write);
 int filter(unsigned int code, struct _EXCEPTION_POINTERS *ep);
 int getCpFromStringValue(const char * encodingStr);
 std::generic_string purgeMenuItemString(const TCHAR * menuItemStr, bool keepAmpersand = false);

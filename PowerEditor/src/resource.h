c58677548a1b018070ad85c9f24ce093cf4b0548
Don Ho <don.h@free.fr>
2009-03-29 10:04:55 +0000

    [RELEASE] v5.3.1 OR.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/notepadplus/repository@443 f5eea248-9336-0410-98b8-ebc06183d4e3

--- PowerEditor/src/resource.h
+++ PowerEditor/src/resource.h
@@ -18,9 +18,9 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v5.3")
-#define VERSION_VALUE TEXT("5.3\0")  // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71 
-#define VERSION_DIGITALVALUE 5, 3, 0, 0
+#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v5.3.1")
+#define VERSION_VALUE TEXT("5.31\0")  // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71 
+#define VERSION_DIGITALVALUE 5, 3, 1, 0
 
 #ifdef UNICODE
 #define UNICODE_ANSI_MODE TEXT("(UNICODE)")

622282fc665b6bbfff3bdb5e6e2068ab40284eba
Don Ho <don.h@free.fr>
2010-11-25 21:54:48 +0000

    [NEW] Add ChangeIcon.exe into generation binary procedure.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/notepadplus/repository/trunk@718 f5eea248-9336-0410-98b8-ebc06183d4e3

--- PowerEditor/misc/vistaIconTool/changeIcon.bat
+++ PowerEditor/misc/vistaIconTool/changeIcon.bat
@@ -1,10 +1,9 @@
-REM To add vista style icon, you need ReplaceVistaIcon.exe in the same directory. You can find it on:
-REM http://www.rw-designer.com/compile-vista-icon
+REM To add vista style icon, you need ChangeIcon.exe in the same directory.
 
-REM %1 should be the path of ReplaceVistaIcon.exe
+REM %1 should be the path of ChangeIcon.exe
 REM %2 should be the path of notepad++.exe
 
 if exist %1 (
-%1 %2 "..\src\icons\nppNewIcon.ico"
+%1 "..\src\icons\nppNewIcon.ico" %2 100 1033
 )
 

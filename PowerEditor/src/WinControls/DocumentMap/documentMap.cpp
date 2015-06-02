4a149600900f8e2491afeeb61d3cd914fb02898b
Christian Grasser <christian.grasser@live.de>
2015-06-03 01:38:44 +0200

    correct whitespace merge difference to master

--- PowerEditor/src/WinControls/DocumentMap/documentMap.cpp
+++ PowerEditor/src/WinControls/DocumentMap/documentMap.cpp
@@ -443,8 +443,8 @@ BOOL CALLBACK ViewZoneDlg::run_dlgProc(UINT message, WPARAM wParam, LPARAM lPara
 			{
 				::SetWindowLongPtr(_viewZoneCanvas, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
 				_canvasDefaultProc = reinterpret_cast<WNDPROC>(::SetWindowLongPtr(_viewZoneCanvas, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(canvasStaticProc)));
-			return TRUE;
-		}
+				return TRUE;
+			}
 			break;
 		}
 

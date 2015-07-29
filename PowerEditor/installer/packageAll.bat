40019fd5b3f59f5c1aaf42ec5bdbf7a05f2b1633
Don Ho <don.h@free.fr>
2015-07-30 00:37:47 +0200

    [ENHANCEMENT] bat launches x86/64 NSIS
    Make script capable launch  NSIS in both x86/64 environment.

--- PowerEditor/installer/packageAll.bat
+++ PowerEditor/installer/packageAll.bat
@@ -142,8 +142,7 @@ If ErrorLevel 1 PAUSE
 If ErrorLevel 1 PAUSE
 "C:\Program Files\7-Zip\7z.exe" a -r .\build\npp.bin.7z .\zipped.package.release\*
 If ErrorLevel 1 PAUSE
-"C:\Program Files (x86)\NSIS\Unicode\makensis.exe" nppSetup.nsi
-rem "C:\Program Files\NSIS\Unicode\makensis.exe" nppSetup.nsi
+IF EXIST "%PROGRAMFILES(X86)%" ("C:\Program Files (x86)\NSIS\Unicode\makensis.exe" nppSetup.nsi) ELSE ("C:\Program Files\NSIS\Unicode\makensis.exe" nppSetup.nsi)
 
 
 @echo off

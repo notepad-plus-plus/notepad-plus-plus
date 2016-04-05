c5eaee2b0e7a496f35d7ac451dadee87268b6284
dail8859 <dail8859@yahoo.com>
2016-04-05 12:37:16 -0400

    Reset the smart highlighter after a fold change
    Closes #1717

--- PowerEditor/src/NppNotification.cpp
+++ PowerEditor/src/NppNotification.cpp
@@ -485,6 +485,7 @@ BOOL Notepad_plus::notify(SCNotification *notification)
 				_pEditView->marginClick(notification->position, notification->modifiers);
 				if (_pDocMap)
 					_pDocMap->fold(lineClick, _pEditView->isFolded(lineClick));
+				_smartHighlighter.highlightView(_pEditView);
 			}
 			else if ((notification->margin == ScintillaEditView::_SC_MARGE_SYBOLE) && !notification->modifiers)
 			{

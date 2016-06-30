d0a3c738626de1a7d46d598812249b44a799ad00
A-R-C-A <arca@tutamail.com>
2016-06-30 01:31:13 +0000

    Added new Option: Enable scrolling beyond last line
    Closes #2034
    
    This option configures the vertical scroll range of Scintilla.
    Disable this will configure the scroll range to end at last line
    (DEFAULT).
    Enable this will configure the scroll range to end one page beyond the
    last line.

--- PowerEditor/src/Parameters.h
+++ PowerEditor/src/Parameters.h
@@ -841,6 +841,7 @@ struct ScintillaViewParams
 	bool _whiteSpaceShow = false;
 	bool _eolShow;
 	int _borderWidth = 2;
+	bool _scrollBeyondLastLine = false;
 	bool _disableAdvancedScrolling = false;
 	bool _doSmoothFont = false;
 	bool _showBorderEdge = true;

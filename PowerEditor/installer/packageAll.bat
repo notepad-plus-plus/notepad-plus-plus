79028b33414aea84543f65b8bdc7b7475e5b34d9
Don HO <don.h@free.fr>
2016-07-12 09:19:56 +0200

    Appveyor artifacts debug and release for x86 & x64
    Fixes #1942 Closes #2079

--- PowerEditor/installer/packageAll.bat
+++ PowerEditor/installer/packageAll.bat
@@ -18,14 +18,18 @@ rem Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 echo on
 
+if %SIGN% == 0 goto NoSign
 signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin\notepad++.exe
+signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin64\notepad++.exe
 If ErrorLevel 1 goto End
 signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin\SciLexer.dll
+signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin64\SciLexer.dll
 If ErrorLevel 1 goto End
 
 signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin\NppShell_06.dll
 If ErrorLevel 1 goto End
 signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin\NppShell64_06.dll
+signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin64\NppShell64_06.dll
 If ErrorLevel 1 goto End
 
 sign them manuelly as they are updated
@@ -39,12 +43,15 @@ If ErrorLevel 1 goto End
 signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin\plugins\NppExport.dll
 If ErrorLevel 1 goto End
 signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin\plugins\mimeTools.dll
+signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin64\plugins\mimeTools.dll
 If ErrorLevel 1 goto End
 signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin\plugins\NppConverter.dll
+signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin64\plugins\NppConverter.dll
 If ErrorLevel 1 goto End
 signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ ..\bin\plugins\PluginManager.dll
 If ErrorLevel 1 goto End
 
+:NoSign
 
 
 rmdir /S /Q .\build
@@ -126,6 +133,12 @@ If ErrorLevel 1 goto End
 copy /Y "..\bin\plugins\NppConverter.dll" .\zipped.package.release\plugins\
 If ErrorLevel 1 goto End
 
+copy /Y "..\bin64\plugins\mimeTools.dll" .\zipped.package.release64\plugins\
+If ErrorLevel 1 goto End
+copy /Y "..\bin64\plugins\NppConverter.dll" .\zipped.package.release64\plugins\
+If ErrorLevel 1 goto End
+
+
 rem plugins manager and its updater 
 copy /Y "..\bin\plugins\PluginManager.dll" .\zipped.package.release\plugins\
 If ErrorLevel 1 goto End
@@ -136,13 +149,19 @@ If ErrorLevel 1 goto End
 rem localizations
 copy /Y ".\nativeLang\*.xml" .\zipped.package.release\localization\
 If ErrorLevel 1 goto End
+copy /Y ".\nativeLang\*.xml" .\zipped.package.release64\localization\
+If ErrorLevel 1 goto End
 
 rem files API
 copy /Y ".\APIs\*.xml" .\zipped.package.release\plugins\APIs\
 If ErrorLevel 1 goto End
+copy /Y ".\APIs\*.xml" .\zipped.package.release64\plugins\APIs\
+If ErrorLevel 1 goto End
 
 rem theme
 copy /Y ".\themes\*.xml" .\zipped.package.release\themes\
+If ErrorLevel 1 goto End
+copy /Y ".\themes\*.xml" .\zipped.package.release64\themes\
 If ErrorLevel 1 goto End
 
 
@@ -156,6 +175,47 @@ If ErrorLevel 1 goto End
 If ErrorLevel 1 goto End
 IF EXIST "%PROGRAMFILES(X86)%" ("%PROGRAMFILES(x86)%\NSIS\Unicode\makensis.exe" nppSetup.nsi) ELSE ("%PROGRAMFILES%\NSIS\Unicode\makensis.exe" nppSetup.nsi)
 
+rem Notepad++ Unicode package
+rmdir /S /Q .\zipped.package.release
+
+mkdir .\zipped.package.release64
+rem mkdir .\zipped.package.release64\updater
+mkdir .\zipped.package.release64\localization
+mkdir .\zipped.package.release64\themes
+mkdir .\zipped.package.release64\plugins
+mkdir .\zipped.package.release64\plugins\APIs
+mkdir .\zipped.package.release64\plugins\Config
+mkdir .\zipped.package.release64\plugins\doc
+
+copy /Y ..\bin\license.txt .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\bin\readme.txt .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\bin\change.log .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\src\config.model.xml .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\src\langs.model.xml .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\src\stylers.model.xml .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\src\contextMenu.xml .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\src\shortcuts.xml .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\src\functionList.xml .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\bin\doLocalConf.xml .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\bin64\"notepad++.exe" .\zipped.package.release64\
+If ErrorLevel 1 goto End
+copy /Y ..\bin64\SciLexer.dll .\zipped.package.release64\
+If ErrorLevel 1 goto End
+
+"C:\Program Files\7-Zip\7z.exe" a -tzip -r .\build\npp.bin64.zip .\zipped.package.release64\*
+If ErrorLevel 1 goto End
+
+
 
 @echo off
 
@@ -167,14 +227,20 @@ for %%a in (npp.*.Installer.exe) do (
   rem echo a = %%a
   set nppInstallerVar=%%a
   set zipvar=!nppInstallerVar:Installer.exe=bin.zip!
+  set 64zipvar=!nppInstallerVar:Installer.exe=bin64.zip!
   set 7zvar=!nppInstallerVar:Installer.exe=bin.7z!
   set 7zvarMin=!nppInstallerVar:Installer.exe=bin.minimalist.7z!
 )
+
+rem echo z=!zipvar!
 ren npp.bin.zip !zipvar!
+ren npp.bin64.zip !64zipvar!
 ren npp.bin.7z !7zvar!
 ren npp.bin.minimalist.7z !7zvarMin!
 
+if %SIGN% == 0 goto NoSignInstaller
 signtool.exe sign /f %NPP_CERT% /p %NPP_CERT_PWD% /d "Notepad++ Installer" /du https://notepad-plus-plus.org/ /t http://timestamp.digicert.com/ !nppInstallerVar!
+:NoSignInstaller
 
 cd ..
 

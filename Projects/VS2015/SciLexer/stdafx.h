f0e799e0ba9951bd4054ae98bf6d7025a13209bf
evpobr <evpobr@gmail.com>
2016-10-29 14:08:45 +0500

    VS2015: enable precompiled headers for SciLexer

---
+++ Projects/VS2015/SciLexer/stdafx.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <sdkddkver.h>
+
+#define NOMINMAX
+
+#include <windows.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <ctype.h>
+#include <time.h>
+#include <math.h>
+#include <limits.h>
+
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iterator>
+#include <new>
+#include <map>
+#include <string>
+#include <set>
+#include <stdexcept>
+#include <vector>
+
+#include <boost/regex.hpp>

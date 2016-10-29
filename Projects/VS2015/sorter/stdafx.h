064e520e50b5e95ce83a0f00c7df888d2d75c63c
evpobr <evpobr@gmail.com>
2016-10-29 14:16:26 +0500

    VS2015: enable precompiled headers for sorter

---
+++ Projects/VS2015/sorter/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <sdkddkver.h>
+
+#include <windows.h>
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>

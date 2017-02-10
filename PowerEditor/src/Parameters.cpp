4b20a113c58dab8e25230f36bc9cec4ceb8787af
eglibicky <eglibicky@gmail.com>
2017-02-10 09:44:55 -0800

    Update Parameters.cpp

--- PowerEditor/src/Parameters.cpp
+++ PowerEditor/src/Parameters.cpp
@@ -97,7 +97,7 @@ static const WinMenuKeyDefinition winKeyDefs[] =
 	{ VK_P,       IDM_FILE_PRINT,                               true,  false, false, nullptr },
 	{ VK_NULL,    IDM_FILE_PRINTNOW,                            false, false, false, nullptr },
 	{ VK_F4,      IDM_FILE_EXIT,                                false, true,  false, nullptr },
-	{ VK_T,      IDM_FILE_RESTORELASTCLOSEDFILE,               true,  false,  true, TEXT("Restore Recent Closed File")},
+	{ VK_T,       IDM_FILE_RESTORELASTCLOSEDFILE,               true,  false, true,  TEXT("Restore Recent Closed File")},
 
 //	{ VK_NULL,    IDM_EDIT_UNDO,                                false, false, false, nullptr },
 //	{ VK_NULL,    IDM_EDIT_REDO,                                false, false, false, nullptr },

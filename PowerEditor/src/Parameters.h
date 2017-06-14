f903e9b0c9da011d6390c5d520418501f7aa623e
Svyatoslav <razmyslov@viva64.com>
2017-06-14 12:33:35 +0300

    Fixed CWE-476 (NULL Pointer Dereference).

--- PowerEditor/src/Parameters.h
+++ PowerEditor/src/Parameters.h
@@ -1283,7 +1283,7 @@ public:
 	{
 		for (int i = 0 ; i < _nbLang ; ++i)
 		{
-			if ((_langList[i]->_langID == langID) || (!_langList[i]))
+			if ( _langList[i] && _langList[i]->_langID == langID )
 				return _langList[i];
 		}
 		return nullptr;

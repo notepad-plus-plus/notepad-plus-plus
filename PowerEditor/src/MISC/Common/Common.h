360d45db1aa44d890d9ce121894d62f2247e3ca6
Christophe Meriaux <cmeriaux@users.noreply.github.com>
2017-10-02 00:08:05 +0200

    Add filter capacity in the shortcut mapper
    Closes #4096, closes #2562

--- PowerEditor/src/MISC/Common/Common.h
+++ PowerEditor/src/MISC/Common/Common.h
@@ -172,6 +172,7 @@ generic_string PathRemoveFileSpec(generic_string & path);
 generic_string PathAppend(generic_string &strDest, const generic_string & str2append);
 COLORREF getCtrlBgColor(HWND hWnd);
 generic_string stringToUpper(generic_string strToConvert);
+generic_string stringToLower(generic_string strToConvert);
 generic_string stringReplace(generic_string subject, const generic_string& search, const generic_string& replace);
 std::vector<generic_string> stringSplit(const generic_string& input, const generic_string& delimiter);
 generic_string stringJoin(const std::vector<generic_string>& strings, const generic_string& separator);

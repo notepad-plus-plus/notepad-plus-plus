1725c8f8c1c90406e4be0c20f8a2b6fb91f3b3e9
Don Ho <don.h@free.fr>
2018-01-01 02:58:59 +0100

    Notepad++ release 7.5.4

--- PowerEditor/src/resource.h
+++ PowerEditor/src/resource.h
@@ -27,12 +27,12 @@
 
 #pragma once
 
-#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v7.5.3")
+#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v7.5.4")
 
 // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71
 // ex : #define VERSION_VALUE TEXT("5.63\0")
-#define VERSION_VALUE TEXT("7.53\0")
-#define VERSION_DIGITALVALUE 7, 5, 3, 0
+#define VERSION_VALUE TEXT("7.54\0")
+#define VERSION_DIGITALVALUE 7, 5, 4, 0
 
 
 

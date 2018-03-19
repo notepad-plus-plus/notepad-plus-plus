9733729bfbb4767269d63eedde7b35ac4536a138
Don Ho <don.h@free.fr>
2018-03-19 01:46:19 +0100

    Notepad++ 7.5.6 release

--- PowerEditor/src/resource.h
+++ PowerEditor/src/resource.h
@@ -27,12 +27,12 @@
 
 #pragma once
 
-#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v7.5.5")
+#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v7.5.6")
 
 // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71
 // ex : #define VERSION_VALUE TEXT("5.63\0")
-#define VERSION_VALUE TEXT("7.55\0")
-#define VERSION_DIGITALVALUE 7, 5, 5, 0
+#define VERSION_VALUE TEXT("7.56\0")
+#define VERSION_DIGITALVALUE 7, 5, 6, 0
 
 
 

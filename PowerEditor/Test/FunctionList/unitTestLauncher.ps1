4993d920d483133006726d1d9076edc8a98ff776
Baptiste Thémine <baptiste.themine@hotmail.fr>
2019-01-04 22:03:33 +0100

    Update unitTestLauncher.ps1
    Now runs multiple unitTest files per language folder -->
    runs
    .\language\[prefix]unitTest.

--- PowerEditor/Test/FunctionList/unitTestLauncher.ps1
+++ PowerEditor/Test/FunctionList/unitTestLauncher.ps1
@@ -1,46 +1,25 @@
-..\..\bin\notepad++.exe -export=functionList -lasm .\asm\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lautoit .\autoit\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lbash .\bash\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lbatch .\batch\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lc .\c\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lcpp .\cpp\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lcs .\cs\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lini .\ini\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -linno .\inno\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -ljava .\java\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -ljavascript .\javascript\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lnsis .\nsis\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lperl .\perl\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lphp .\php\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lpowershell .\powershell\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lpython .\python\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lruby .\ruby\unitTest | Out-Null
-..\..\bin\notepad++.exe -export=functionList -lxml .\xml\unitTest | Out-Null
+$languages = @("asm","autoit","bash","batch","c","cpp","cs","ini","inno","java","javascript","nsis","perl","php","powershell","python","ruby","xml")
 
+foreach ($language in $languages) {
 
-$testRoot = ".\"
+	echo $language
 
-Get-ChildItem $testRoot -Filter *.* | 
-Foreach-Object {
-    if ((Get-Item $testRoot$_) -is [System.IO.DirectoryInfo])
-    {        
-        $expectedRes = Get-Content $testRoot$_\unitTest.expected.result
-        $generatedRes = Get-Content $testRoot$_\unitTest.result.json
-        
-        if ($generatedRes -eq $expectedRes)
-        {
-           Remove-Item $testRoot$_\unitTest.result.json
-           ""
-           "OK"
-        }
-        else
-        {
-            "$generatedRes"
-            exit -1
-        }
-        
-    }
+	Get-ChildItem .\$language -Filter *unitTest | Foreach-Object {
+
+		..\..\bin\notepad++.exe -export=functionList "-l$language" .\$language\$_ | Out-Null
+
+		$expectedRes = Get-Content .\$language\$_.expected.result
+		$generatedRes = Get-Content .\$language\$_.result.json
+
+		if ($generatedRes -eq $expectedRes)
+		{
+			Remove-Item .\$language\$_.result.json
+			echo "OK"
+		}
+		else
+		{
+			"$generatedRes"
+			exit -1
+		}
+	}
 }
-
-exit 0
-

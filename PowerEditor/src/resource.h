1a356c20199172d12b50ddb8c8321a42ced99cf9
Don HO <don.h@free.fr>
2019-01-27 23:13:30 +0100

    Notepas++ 7.6.3 release

--- PowerEditor/src/resource.h
+++ PowerEditor/src/resource.h
@@ -27,12 +27,12 @@
 
 #pragma once
 
-#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v7.6.2")
+#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v7.6.3")
 
 // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71
 // ex : #define VERSION_VALUE TEXT("5.63\0")
-#define VERSION_VALUE TEXT("7.6.2\0")
-#define VERSION_DIGITALVALUE 7, 6, 2, 0
+#define VERSION_VALUE TEXT("7.6.3\0")
+#define VERSION_DIGITALVALUE 7, 6, 3, 0
 
 
 
@@ -41,9 +41,9 @@
 #endif
 
 #define IDI_M30ICON                100
-//#define IDI_CHAMELEON            101
+#define IDI_CHAMELEON            101
 //#define IDI_JESUISCHARLIE        102
-#define IDI_GILETJAUNE        102
+//#define IDI_GILETJAUNE        102
 
 #define IDR_RT_MANIFEST         103
 

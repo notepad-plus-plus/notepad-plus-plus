5085d9c95a491bcb9f3d813ed3ac997412e4293a
Don HO <don.h@free.fr>
2019-02-22 19:58:20 +0100

    [EU-FOSSA] Check string length to copy to avoid crash in purgeMenuItemString function

--- PowerEditor/src/MISC/Common/Common.cpp
+++ PowerEditor/src/MISC/Common/Common.cpp
@@ -361,9 +361,13 @@ bool isInList(const TCHAR *token, const TCHAR *list)
 
 generic_string purgeMenuItemString(const TCHAR * menuItemStr, bool keepAmpersand)
 {
-	TCHAR cleanedName[64] = TEXT("");
+	const size_t cleanedNameLen = 64;
+	TCHAR cleanedName[cleanedNameLen] = TEXT("");
 	size_t j = 0;
 	size_t menuNameLen = lstrlen(menuItemStr);
+	if (menuNameLen >= cleanedNameLen)
+		menuNameLen = cleanedNameLen - 1;
+
 	for (size_t k = 0 ; k < menuNameLen ; ++k)
 	{
 		if (menuItemStr[k] == '\t')

b0f38c923cf1cef632a24bd7f199f1639f10ab52
Waldi Ravens <wr86420@gmail.com>
2019-10-19 09:30:34 +0200

    Fix typo

--- PowerEditor/src/Parameters.h
+++ PowerEditor/src/Parameters.h
@@ -824,7 +824,7 @@ struct NppGUI final
 	char _rightmostDelimiter = ')';
 	bool _delimiterSelectionOnEntireDocument = false;
 	bool _backSlashIsEscapeCharacterForSql = true;
-	bool _monospacedFontFinfDlg = false;
+	bool _monospacedFontFindDlg = false;
 	bool _isWordCharDefault = true;
 	std::string _customWordChars;
 

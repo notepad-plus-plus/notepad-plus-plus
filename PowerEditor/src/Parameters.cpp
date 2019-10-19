88a2f68f05c6d43f4a2c901bdad5eb46927caff6
Waldi Ravens <wr86420@gmail.com>
2019-10-19 09:30:34 +0200

    Fix typo
    Close #6233

--- PowerEditor/src/Parameters.cpp
+++ PowerEditor/src/Parameters.cpp
@@ -5076,9 +5076,9 @@ void NppParameters::feedGUIParameters(TiXmlNode *node)
 			if (optNameBackSlashEscape && !lstrcmp(optNameBackSlashEscape, TEXT("no")))
 				_nppGUI._backSlashIsEscapeCharacterForSql = false;
 
-			const TCHAR * optNameMonoFont = element->Attribute(TEXT("monospacedFontFinfDlg"));
+			const TCHAR * optNameMonoFont = element->Attribute(TEXT("monospacedFontFindDlg"));
 			if (optNameMonoFont)
-				_nppGUI._monospacedFontFinfDlg = (lstrcmp(optNameMonoFont, TEXT("yes")) == 0);
+				_nppGUI._monospacedFontFindDlg = (lstrcmp(optNameMonoFont, TEXT("yes")) == 0);
 
 			const TCHAR * optNameNewStyleSaveDlg = element->Attribute(TEXT("newStyleSaveDlg"));
 			if (optNameNewStyleSaveDlg)
@@ -5836,7 +5836,7 @@ void NppParameters::createXmlTreeFromGUIParams()
 
 		GUIConfigElement->SetAttribute(TEXT("fileSwitcherWithoutExtColumn"), _nppGUI._fileSwitcherWithoutExtColumn ? TEXT("yes") : TEXT("no"));
 		GUIConfigElement->SetAttribute(TEXT("backSlashIsEscapeCharacterForSql"), _nppGUI._backSlashIsEscapeCharacterForSql ? TEXT("yes") : TEXT("no"));
-		GUIConfigElement->SetAttribute(TEXT("monospacedFontFinfDlg"), _nppGUI._monospacedFontFinfDlg ? TEXT("yes") : TEXT("no"));
+		GUIConfigElement->SetAttribute(TEXT("monospacedFontFindDlg"), _nppGUI._monospacedFontFindDlg ? TEXT("yes") : TEXT("no"));
 		GUIConfigElement->SetAttribute(TEXT("newStyleSaveDlg"), _nppGUI._useNewStyleSaveDlg ? TEXT("yes") : TEXT("no"));
 		GUIConfigElement->SetAttribute(TEXT("isFolderDroppedOpenFiles"), _nppGUI._isFolderDroppedOpenFiles ? TEXT("yes") : TEXT("no"));
 		GUIConfigElement->SetAttribute(TEXT("docPeekOnTab"), _nppGUI._isDocPeekOnTab ? TEXT("yes") : TEXT("no"));

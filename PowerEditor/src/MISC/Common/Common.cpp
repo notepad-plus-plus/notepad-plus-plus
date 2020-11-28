43c516efd6fb4ad263355c8c98b97cf054cc7c11
Scott Sumner <30118311+sasumner@users.noreply.github.com>
2020-11-28 12:59:30 -0500

    Trim spaces on Directory for FindInFiles

--- PowerEditor/src/MISC/Common/Common.cpp
+++ PowerEditor/src/MISC/Common/Common.cpp
@@ -1327,3 +1327,17 @@ void getFilesInFolder(std::vector<generic_string>& files, const generic_string& 
 	::FindClose(hFindFile);
 }
 
+void trim(generic_string& str)
+{
+	// remove any leading or trailing spaces from str
+
+	generic_string::size_type pos = str.find_last_not_of(' ');
+
+	if (pos != generic_string::npos)
+	{
+		str.erase(pos + 1);
+		pos = str.find_first_not_of(' ');
+		if (pos != generic_string::npos) str.erase(0, pos);
+	}
+	else str.erase(str.begin(), str.end());
+};

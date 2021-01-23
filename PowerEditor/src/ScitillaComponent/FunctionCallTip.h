caabb07526df768c72047835c3244b631e0556d2
Don HO <don.h@free.fr>
2021-01-23 04:39:05 +0100

    Restore original author's name

--- PowerEditor/src/ScitillaComponent/FunctionCallTip.h
+++ PowerEditor/src/ScitillaComponent/FunctionCallTip.h
@@ -1,5 +1,5 @@
 // This file is part of Notepad++ project
-// Copyright (C)2021 Don HO <don.h@free.fr>
+// Copyright (C)2008 Harry Bruin <harrybharry@users.sourceforge.net>
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by

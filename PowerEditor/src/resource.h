4399c1343b96aee0c5419f130ddd984dc86f02d7
Don HO <don.h@free.fr>
2021-03-22 22:46:05 +0100

    Notepad++ 7.9.5 release

--- PowerEditor/src/resource.h
+++ PowerEditor/src/resource.h
@@ -19,12 +19,12 @@
 //
 // Notepad++ version: begin
 //
-#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v7.9.4")
+#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v7.9.5")
 
 // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71
 // ex : #define VERSION_VALUE TEXT("5.63\0")
-#define VERSION_VALUE TEXT("7.94\0")
-#define VERSION_DIGITALVALUE 7, 9, 4, 0
+#define VERSION_VALUE TEXT("7.95\0")
+#define VERSION_DIGITALVALUE 7, 9, 5, 0
 
 // Notepad++ version: end
 

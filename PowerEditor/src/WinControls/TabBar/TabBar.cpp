1089e239ba7f6f31ee0c4ed379d30be71eb742d7
Adam D. Walling <adam.walling@gmail.com>
2021-05-09 22:32:35 -0400

    Add the capacity to customize the color of Notepad++ (not only dark)
    1. Customizable colors easily handled now by tweaking a few custom drawing implementations.
    2. make dark mode less intense by default.
    
    Close #9848

--- PowerEditor/src/WinControls/TabBar/TabBar.cpp
+++ PowerEditor/src/WinControls/TabBar/TabBar.cpp
@@ -866,7 +866,7 @@ LRESULT TabBarPlus::runProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lPara
 
 			RECT rc = { 0 };
 			GetClientRect(hwnd, &rc);
-			FillRect((HDC)wParam, &rc, NppDarkMode::getPureBackgroundBrush());
+			FillRect((HDC)wParam, &rc, NppDarkMode::getDarkerBackgroundBrush());
 
 			return 1;
 		}
@@ -886,7 +886,7 @@ LRESULT TabBarPlus::runProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lPara
 
 			PAINTSTRUCT ps;
 			HDC hdc = BeginPaint(hwnd, &ps);
-			FillRect(hdc, &ps.rcPaint, NppDarkMode::getPureBackgroundBrush());
+			FillRect(hdc, &ps.rcPaint, NppDarkMode::getDarkerBackgroundBrush());
 
 			UINT id = ::GetDlgCtrlID(hwnd);
 

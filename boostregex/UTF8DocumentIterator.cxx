2794c927b326fed299b52388a16f59df1970ec8f
mere-human <9664141+mere-human@users.noreply.github.com>
2021-06-19 12:04:27 +0300

    Fix the warnings issue during Scintilla's compiling
    Remove std::iterator from Boost Regex search code:
    std::iterator was deprecated in C++17.
    Remove it to fix the warnings and avoid other issues.
    
    Fix #10035, close #10036

--- boostregex/UTF8DocumentIterator.cxx
+++ boostregex/UTF8DocumentIterator.cxx
@@ -1,6 +1,20 @@
-﻿#include "UTF8DocumentIterator.h"
+﻿// This file is part of Notepad++ project
+// Copyright (C) 2021 Notepad++ authors.
 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#include "UTF8DocumentIterator.h"
 #include <string_view>
 #include <stdexcept>
 
@@ -8,8 +22,6 @@
 #include "ILexer.h"
 #include "Scintilla.h"
 #include "Platform.h"
-
-
 
 #include "CharacterCategory.h"
 #include "Position.h"

f12cdc89ec677cf55c1cca18aef9130281a58242
Ashfaaq18 <ashfaaq.riphque@gmail.com>
2021-06-20 05:42:52 +0530

    removed the empty line and aligned the comment

--- PowerEditor/src/Notepad_plus.cpp
+++ PowerEditor/src/Notepad_plus.cpp
@@ -535,8 +535,7 @@ LRESULT Notepad_plus::init(HWND hwnd)
 	_pluginsManager.setMenu(_mainMenuHandle, NULL, enablePluginAdmin);
 
 	//Search menu
-
-		//disable "Search Results Window" under Search Menu 
+	//disable "Search Results Window" under Search Menu 
 	::EnableMenuItem(_mainMenuHandle, IDM_FOCUS_ON_FOUND_RESULTS, MF_DISABLED | MF_GRAYED | MF_BYCOMMAND);
 
 	//Main menu is loaded, now load context menu items

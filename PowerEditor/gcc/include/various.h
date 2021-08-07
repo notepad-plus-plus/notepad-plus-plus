fe795d79de6053b99b5f59a78a35fe9f981cfa53
Christian Grasser <christian.grasser@live.de>
2021-08-07 20:56:14 +0200

    Fix building MinGW release twice in appveyor builds
    Improve also MinGW build (toolbar appears) by adding changes from patch https://github.com/notepad-plus-plus/notepad-plus-plus/files/6947163/notepad-gcc-build.patch.txt
    
    Fix #10328, fix #10185, close #10469

--- PowerEditor/gcc/include/various.h
+++ PowerEditor/gcc/include/various.h
@@ -4,24 +4,24 @@
 
 //GetLongPathName = 410
 //Multimonitor: 410
-#define _WIN32_WINDOWS 0x0410
+//#define _WIN32_WINDOWS 0x0410
 //Toolbar imagelist = 300
 //TCS_BOTTOM = 300
 //LVS_EX_BORDERSELECT = 500
 //TBSTYLE_EX_HIDECLIPPEDBUTTONS = 501
 //#define _WIN32_IE 0x501
 //Theme (uxtheme)
-#define _WIN32_WINNT 0x0600
+//#define _WIN32_WINNT 0x0600
 
 //#include <windows.h>
 
-#if (_WIN32_IE >= 0x0400)
-#define TCN_GETOBJECT           (TCN_FIRST - 3)
-#endif 
+//#if (_WIN32_IE >= 0x0400)
+//#define TCN_GETOBJECT           (TCN_FIRST - 3)
+//#endif
 
-#if (_WIN32_IE >= 0x0500)
-#define RBN_CHEVRONPUSHED   (RBN_FIRST - 10)
-#endif      // _WIN32_IE >= 0x0500
+//#if (_WIN32_IE >= 0x0500)
+//#define RBN_CHEVRONPUSHED   (RBN_FIRST - 10)
+//#endif      // _WIN32_IE >= 0x0500
 /*
 #ifndef max
 #define max(a,b)            (((a) > (b)) ? (a) : (b))
@@ -48,5 +48,5 @@ static inline int min(unsigned int a, unsigned int b) {
 //double _wtof(const wchar_t * string);
 
 // from MinGW include/driverspecs.h
-#define __inout
-#define __inout_opt
+//#define __inout
+//#define __inout_opt

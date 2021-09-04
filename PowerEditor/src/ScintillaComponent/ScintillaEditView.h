2d0976123e58dfa69c8af990ef447495261f42e9
Don Ho <don.h@free.fr>
2021-09-04 02:10:21 +0200

    Fix CSS pseudo-elements not recognized issue
    Update CSS keywords and add new styles.
    
    Fix #10425

--- PowerEditor/src/ScintillaComponent/ScintillaEditView.h
+++ PowerEditor/src/ScintillaComponent/ScintillaEditView.h
@@ -678,7 +678,7 @@ protected:
 
 	//Simple lexers
 	void setCssLexer() {
-		setLexer(SCLEX_CSS, L_CSS, LIST_0 | LIST_1);
+		setLexer(SCLEX_CSS, L_CSS, LIST_0 | LIST_1 | LIST_4);
 	};
 
 	void setLuaLexer() {

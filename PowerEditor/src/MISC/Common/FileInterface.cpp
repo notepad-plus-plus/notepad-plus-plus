72fa7a505826019e781b867534d367d6432a97b8
Don Ho <don.h@free.fr>
2021-10-02 02:57:44 +0200

    Add right author info in FileInterface

--- PowerEditor/src/MISC/Common/FileInterface.cpp
+++ PowerEditor/src/MISC/Common/FileInterface.cpp
@@ -1,5 +1,5 @@
 // This file is part of Notepad++ project
-// Copyright (C)2021 Don HO <don.h@free.fr>
+// Copyright (C)2021 Pavel Nedev (pg.nedev@gmail.com)
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by

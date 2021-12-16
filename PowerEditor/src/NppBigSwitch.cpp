notepad-plus-plus/notepad-plus-plus

commit ee884f87bbb66d1431f0d530d0cd25bb7cde6b35

A comprehensive fix for background clipping of dock panels
The (very detailed) explanation/information of fix:
https://github.com/notepad-plus-plus/notepad-plus-plus/issues/2937#issuecomment-992086848

Fix #2202, fix #2937, fix #5980, fix #8980, fix #9905, fix #10091, close #10910

PowerEditor/src/NppBigSwitch.cpp
@@ -1759,50 +1759,10 @@ LRESULT Notepad_plus::process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPa
 			return TRUE;
 		}
 
-		case WM_ACTIVATE:
+		case WM_SYNCPAINT:
 		{
-			if (wParam != WA_INACTIVE && _pEditView && _pNonEditView)
-			{
-				_pEditView->getFocus();
-				auto x = _pEditView->execute(SCI_GETXOFFSET);
-				_pEditView->execute(SCI_SETXOFFSET, x);
-				x = _pNonEditView->execute(SCI_GETXOFFSET);
-				_pNonEditView->execute(SCI_SETXOFFSET, x);
-
-				if (_pFileBrowser && _pFileBrowser->isVisible())
-				{
-					_pFileBrowser->redraw(true);
-				}
-
-				if (_pFuncList && _pFuncList->isVisible())
-				{
-					_pFuncList->redraw(true);
-				}
-
-				if (_pClipboardHistoryPanel && _pClipboardHistoryPanel->isVisible())
-				{
-					_pClipboardHistoryPanel->redraw(true);
-				}
-
-				if (_pDocMap && _pDocMap->isVisible())
-				{
-					_pDocMap->redraw(true);
-				}
-
-				if (_pProjectPanel_1 && _pProjectPanel_1->isVisible())
-				{
-					_pProjectPanel_1->redraw(true);
-				}
-				if (_pProjectPanel_2 && _pProjectPanel_2->isVisible())
-				{
-					_pProjectPanel_2->redraw(true);
-				}
-				if (_pProjectPanel_3 && _pProjectPanel_3->isVisible())
-				{
-					_pProjectPanel_3->redraw(true);
-				}
-			}
-			return TRUE;
+			RedrawWindow(hwnd, NULL, NULL, RDW_INVALIDATE | RDW_ERASE | RDW_ALLCHILDREN);
+			break;
 		}
 
 		case WM_DROPFILES:

commit ac52f406f355ddb6e1c3f5a8f71f797c040df722

Fix no focus on edit zone regression
Fix no focus on edit zone after switching back to Notepad++.
This regression is introduced by ee884f87bbb66d1431f0d530d0cd25bb7cde6b35 (#10910)

ref:
https://github.com/notepad-plus-plus/notepad-plus-plus/commit/ee884f87bbb66d1431f0d530d0cd25bb7cde6b35#commitcomment-62032223

PowerEditor/src/NppBigSwitch.cpp
@@ -1759,6 +1759,20 @@ LRESULT Notepad_plus::process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPa
 			return TRUE;
 		}
 
+
+		case WM_ACTIVATE:
+		{
+			if (wParam != WA_INACTIVE && _pEditView && _pNonEditView)
+			{
+				_pEditView->getFocus();
+				auto x = _pEditView->execute(SCI_GETXOFFSET);
+				_pEditView->execute(SCI_SETXOFFSET, x);
+				x = _pNonEditView->execute(SCI_GETXOFFSET);
+				_pNonEditView->execute(SCI_SETXOFFSET, x);
+			}
+			return TRUE;
+		}
+
 		case WM_SYNCPAINT:
 		{
 			RedrawWindow(hwnd, NULL, NULL, RDW_INVALIDATE | RDW_ERASE | RDW_ALLCHILDREN);
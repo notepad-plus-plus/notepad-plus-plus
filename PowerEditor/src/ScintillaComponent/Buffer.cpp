738b3fdbafcb0fee3a21ba639dd13fc2e12aba1a
Don Ho <don.h@free.fr>
2021-12-24 02:23:50 +0100

    Make crontab file use syntax highlighting of bash
    Fix #3377

--- PowerEditor/src/ScintillaComponent/Buffer.cpp
+++ PowerEditor/src/ScintillaComponent/Buffer.cpp
@@ -222,6 +222,8 @@ void Buffer::setFileName(const TCHAR *fn, LangType defaultLang)
 			newLang = L_PYTHON;
 		else if ((OrdinalIgnoreCaseCompareStrings(_fileName, TEXT("Rakefile")) == 0) || (OrdinalIgnoreCaseCompareStrings(_fileName, TEXT("Vagrantfile")) == 0))
 			newLang = L_RUBY;
+		else if ((OrdinalIgnoreCaseCompareStrings(_fileName, TEXT("crontab")) == 0))
+			newLang = L_BASH;
 	}
 
 	updateTimeStamp();

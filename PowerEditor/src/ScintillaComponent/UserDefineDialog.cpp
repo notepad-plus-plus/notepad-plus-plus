notepad-plus-plus/notepad-plus-plus

commit 94154b0f0e281b4bbfad9c59f8d01c9dff305c12

Code enhancement: Fix comparison of different signs integers

PowerEditor/src/ScintillaComponent/UserDefineDialog.cpp
@@ -37,7 +37,7 @@ GlobalMappers & globalMappper()
     return gm;
 }
 
-void convertTo(TCHAR *dest, int destLen, const TCHAR *toConvert, TCHAR *prefix)
+void convertTo(TCHAR *dest, int destLen, const TCHAR *toConvert, const TCHAR *prefix)
 {
 	bool inGroup = false;
 	int index = lstrlen(dest);
@@ -526,7 +526,6 @@ void CommentStyleDialog::setKeywords2List(int id)
         newList[0] = '\0';
         TCHAR* buffer = new TCHAR[max_char];
         buffer[0] = '\0';
-        TCHAR intBuffer[10] = {'0', 0};
 
         const int list[] = {
             IDC_COMMENTLINE_OPEN_EDIT,
@@ -536,11 +535,11 @@ void CommentStyleDialog::setKeywords2List(int id)
             IDC_COMMENT_CLOSE_EDIT
         };
 
-        for (auto i = 0; i < sizeof(list)/sizeof(int); ++i)
+        for (size_t i = 0; i < sizeof(list)/sizeof(int); ++i)
         {
-            generic_itoa(i, intBuffer+1, 10);
+            wstring intStr = std::to_wstring(i);
             ::GetDlgItemText(_hSelf, list[i], buffer, max_char);
-			convertTo(newList, max_char, buffer, intBuffer);
+			convertTo(newList, max_char, buffer, intStr.c_str());
         }
 
 		wcscpy_s(_pUserLang->_keywordLists[index], newList);
@@ -549,7 +548,7 @@ void CommentStyleDialog::setKeywords2List(int id)
     }
 }
 
-void CommentStyleDialog::retrieve(TCHAR *dest, const TCHAR *toRetrieve, TCHAR *prefix) const
+void CommentStyleDialog::retrieve(TCHAR *dest, const TCHAR *toRetrieve, const TCHAR *prefix) const
 {
     int j = 0;
     bool begin2Copy = false;
@@ -589,7 +588,6 @@ void CommentStyleDialog::updateDlg()
 {
     TCHAR* buffer = new TCHAR[max_char];
     buffer[0] = '\0';
-    TCHAR intBuffer[10] = {'0', 0};
 
     const int list[] = {
         IDC_COMMENTLINE_OPEN_EDIT,
@@ -599,10 +597,10 @@ void CommentStyleDialog::updateDlg()
         IDC_COMMENT_CLOSE_EDIT
     };
 
-    for (int i=0; i<sizeof(list)/sizeof(int); ++i)
+    for (size_t i=0; i<sizeof(list)/sizeof(int); ++i)
     {
-        generic_itoa(i, intBuffer+1, 10);
-        retrieve(buffer, _pUserLang->_keywordLists[SCE_USER_KWLIST_COMMENTS], intBuffer);
+        wstring intStr = std::to_wstring(i);
+        retrieve(buffer, _pUserLang->_keywordLists[SCE_USER_KWLIST_COMMENTS], intStr.c_str());
 		::SendDlgItemMessage(_hSelf, list[i], WM_SETTEXT, 0, reinterpret_cast<LPARAM>(buffer));
     }
 
@@ -659,7 +657,7 @@ void SymbolsStyleDialog::updateDlg()
     };
     TCHAR intBuffer[10] = {'0', 0};
 
-    for (int i = 0; i < sizeof(list)/sizeof(int); ++i)
+    for (int i = 0; static_cast<size_t>(i) < sizeof(list)/sizeof(int); ++i)
     {
         if (i < 10)
             generic_itoa(i, intBuffer + 1, 10);
@@ -853,7 +851,7 @@ void SymbolsStyleDialog::setKeywords2List(int id)
                 IDC_DELIMITER8_BOUNDARYCLOSE_EDIT
             };
 
-            for (int i = 0; i < sizeof(list)/sizeof(int); ++i)
+            for (int i = 0; static_cast<size_t>(i) < sizeof(list)/sizeof(int); ++i)
             {
                 if (i < 10)
                     generic_itoa(i, intBuffer+1, 10);
@@ -1223,7 +1221,7 @@ intptr_t CALLBACK UserDefineDialog::run_dlgProc(UINT message, WPARAM wParam, LPA
 							const size_t langNameLen = 256;
 							TCHAR langName[langNameLen + 1];
 							auto cbTextLen = ::SendDlgItemMessage(_hSelf, IDC_LANGNAME_COMBO, CB_GETLBTEXTLEN, i, 0);
-							if (cbTextLen > langNameLen)
+							if (static_cast<size_t>(cbTextLen) > langNameLen)
 								return TRUE;
 
 							::SendDlgItemMessage(_hSelf, IDC_LANGNAME_COMBO, CB_GETLBTEXT, i, reinterpret_cast<LPARAM>(langName));
@@ -1252,7 +1250,7 @@ intptr_t CALLBACK UserDefineDialog::run_dlgProc(UINT message, WPARAM wParam, LPA
 						const size_t langNameLen = 256;
 						TCHAR langName[langNameLen + 1];
 						auto cbTextLen = ::SendDlgItemMessage(_hSelf, IDC_LANGNAME_COMBO, CB_GETLBTEXTLEN, i, 0);
-						if (cbTextLen > langNameLen)
+						if (static_cast<size_t>(cbTextLen) > langNameLen)
 							return TRUE;
 
 						::SendDlgItemMessage(_hSelf, IDC_LANGNAME_COMBO, CB_GETLBTEXT, i, reinterpret_cast<LPARAM>(langName));
@@ -1755,7 +1753,7 @@ intptr_t CALLBACK StylerDlg::dlgProc(HWND hwnd, UINT message, WPARAM wParam, LPA
 
             // for the font size combo
             HWND hFontSizeCombo = ::GetDlgItem(hwnd, IDC_STYLER_COMBO_FONT_SIZE);
-            for (int j = 0 ; j < int(sizeof(fontSizeStrs))/(3*sizeof(TCHAR)) ; ++j)
+            for (size_t j = 0 ; j < int(sizeof(fontSizeStrs))/(3*sizeof(TCHAR)) ; ++j)
 				::SendMessage(hFontSizeCombo, CB_ADDSTRING, 0, reinterpret_cast<LPARAM>(fontSizeStrs[j]));
 
             TCHAR size[10];
@@ -1879,7 +1877,7 @@ intptr_t CALLBACK StylerDlg::dlgProc(HWND hwnd, UINT message, WPARAM wParam, LPA
 						const size_t intStrLen = 3;
 						TCHAR intStr[intStrLen];
 						auto lbTextLen = ::SendDlgItemMessage(hwnd, LOWORD(wParam), CB_GETLBTEXTLEN, i, 0);
-						if (lbTextLen > intStrLen - 1)
+						if (static_cast<size_t>(lbTextLen) > intStrLen - 1)
 							return TRUE;
 
 						::SendDlgItemMessage(hwnd, LOWORD(wParam), CB_GETLBTEXT, i, reinterpret_cast<LPARAM>(intStr));

PowerEditor/src/MISC/RegExt/regExtDlg.cpp
@@ -258,7 +258,7 @@ intptr_t CALLBACK RegExtDlg::run_dlgProc(UINT Message, WPARAM wParam, LPARAM lPa
 					{
 						const size_t itemNameLen = 32;
 						TCHAR itemName[itemNameLen + 1] = { '\0' };
-						auto lbTextLen = ::SendDlgItemMessage(_hSelf, LOWORD(wParam), LB_GETTEXTLEN, i, 0);
+						size_t lbTextLen = ::SendDlgItemMessage(_hSelf, LOWORD(wParam), LB_GETTEXTLEN, i, 0);
 						if (lbTextLen > itemNameLen)
 							return TRUE;
 

PowerEditor/src/Notepad_plus.cpp
@@ -3601,7 +3601,7 @@ size_t Notepad_plus::getSelectedCharNumber(UniMode u)
 			for (size_t j = line1; j <= line2; ++j)
 			{
 				size_t stpos = _pEditView->execute(SCI_GETLINESELSTARTPOSITION, j);
-				if (stpos != INVALID_POSITION)
+				if (static_cast<intptr_t>(stpos) != INVALID_POSITION)
 				{
 					size_t endpos = _pEditView->execute(SCI_GETLINESELENDPOSITION, j);
 					for (size_t pos = stpos; pos < endpos; ++pos)
@@ -5803,30 +5803,30 @@ bool Notepad_plus::dumpFiles(const TCHAR * outdir, const TCHAR * fileprefix)
 void Notepad_plus::drawTabbarColoursFromStylerArray()
 {
 	Style *stActText = getStyleFromName(TABBAR_ACTIVETEXT);
-	if (stActText && stActText->_fgColor != -1)
+	if (stActText && static_cast<long>(stActText->_fgColor) != -1)
 		TabBarPlus::setColour(stActText->_fgColor, TabBarPlus::activeText);
 
 	Style *stActfocusTop = getStyleFromName(TABBAR_ACTIVEFOCUSEDINDCATOR);
-	if (stActfocusTop && stActfocusTop->_fgColor != -1)
+	if (stActfocusTop && static_cast<long>(stActfocusTop->_fgColor) != -1)
 		TabBarPlus::setColour(stActfocusTop->_fgColor, TabBarPlus::activeFocusedTop);
 
 	Style *stActunfocusTop = getStyleFromName(TABBAR_ACTIVEUNFOCUSEDINDCATOR);
-	if (stActunfocusTop && stActunfocusTop->_fgColor != -1)
+	if (stActunfocusTop && static_cast<long>(stActunfocusTop->_fgColor) != -1)
 		TabBarPlus::setColour(stActunfocusTop->_fgColor, TabBarPlus::activeUnfocusedTop);
 
 	Style *stInact = getStyleFromName(TABBAR_INACTIVETEXT);
-	if (stInact && stInact->_fgColor != -1)
+	if (stInact && static_cast<long>(stInact->_fgColor) != -1)
 		TabBarPlus::setColour(stInact->_fgColor, TabBarPlus::inactiveText);
-	if (stInact && stInact->_bgColor != -1)
+	if (stInact && static_cast<long>(stInact->_bgColor) != -1)
 		TabBarPlus::setColour(stInact->_bgColor, TabBarPlus::inactiveBg);
 }
 
 void Notepad_plus::drawDocumentMapColoursFromStylerArray()
 {
 	Style* docMap = getStyleFromName(VIEWZONE_DOCUMENTMAP);
-	if (docMap && docMap->_fgColor != -1)
+	if (docMap && static_cast<long>(docMap->_fgColor) != -1)
 		ViewZoneDlg::setColour(docMap->_fgColor, ViewZoneDlg::ViewZoneColorIndex::focus);
-	if (docMap && docMap->_bgColor != -1)
+	if (docMap && static_cast<long>(docMap->_bgColor) != -1)
 		ViewZoneDlg::setColour(docMap->_bgColor, ViewZoneDlg::ViewZoneColorIndex::frost);
 }
 

PowerEditor/src/Parameters.cpp
@@ -630,7 +630,7 @@ int base64ToAscii(char *dest, const char *base64Str)
 		uc2 = (UCHAR)base64IndexArray[base64Str[j+2]];
 		uc3 = (UCHAR)base64IndexArray[base64Str[j+3]];
 
-		if ((uc0 == -1) || (uc1 == -1) || (uc2 == -1) || (uc3 == -1))
+		if ((static_cast<char>(uc0) == -1) || (static_cast<char>(uc1) == -1) || (static_cast<char>(uc2) == -1) || (static_cast<char>(uc3) == -1))
 			return -1;
 
 		if (base64Str[j+2] == '=') // && (uc3 == '=')

PowerEditor/src/ScintillaComponent/FindReplaceDlg.cpp
@@ -499,7 +499,7 @@ bool Finder::notify(SCNotification *notification)
 			::SendMessage(_scintView.getHSelf(), WM_LBUTTONUP, 0, 0);
 
 			size_t pos = notification->position;
-			if (pos == INVALID_POSITION)
+			if (static_cast<intptr_t>(pos) == INVALID_POSITION)
 				pos = _scintView.execute(SCI_GETLINEENDPOSITION, notification->line);
 			_scintView.execute(SCI_SETSEL, pos, pos);
 
@@ -3494,7 +3494,7 @@ void FindReplaceDlg::enableProjectCheckmarks()
 		for (int i = 0; i < 3; i++)
 		{
 			UINT s = GetMenuState (hMenu, idm [i], MF_BYCOMMAND);
-			if (s != -1)
+			if (static_cast<int>(s) != -1)
 			{
 				if (s & MF_CHECKED)
 				{

PowerEditor/src/ScintillaComponent/ScintillaEditView.cpp
@@ -418,7 +418,7 @@ LRESULT ScintillaEditView::scintillaNew_Proc(HWND hwnd, UINT Message, WPARAM wPa
 				if (selectSize == 0)
 					return 0;
 
-				if (selectSize + 1 > sizeof(smallTextBuffer))
+				if (static_cast<size_t>(selectSize + 1) > sizeof(smallTextBuffer))
 					selectedStr = new char[selectSize + 1];
 				getText(selectedStr, range.cpMin, range.cpMax);
 
@@ -1940,14 +1940,14 @@ void ScintillaEditView::getCurrentFoldStates(std::vector<size_t> & lineStateVect
 
 	do {
 		contractedFoldHeaderLine = execute(SCI_CONTRACTEDFOLDNEXT, contractedFoldHeaderLine);
-		if (contractedFoldHeaderLine != -1)
+		if (static_cast<intptr_t>(contractedFoldHeaderLine) != -1)
 		{
 			//-- Store contracted line
 			lineStateVector.push_back(contractedFoldHeaderLine);
 			//-- Start next search with next line
 			++contractedFoldHeaderLine;
 		}
-	} while (contractedFoldHeaderLine != -1);
+	} while (static_cast<intptr_t>(contractedFoldHeaderLine) != -1);
 }
 
 void ScintillaEditView::syncFoldStateWith(const std::vector<size_t> & lineStateVectorNew)

PowerEditor/src/ScintillaComponent/UserDefineDialog.h
@@ -284,7 +284,7 @@ protected :
     intptr_t CALLBACK run_dlgProc(UINT Message, WPARAM wParam, LPARAM lParam);
     void setKeywords2List(int id);
 private :
-    void retrieve(TCHAR *dest, const TCHAR *toRetrieve, TCHAR *prefix) const;
+    void retrieve(TCHAR *dest, const TCHAR *toRetrieve, const TCHAR *prefix) const;
 };
 
 class SymbolsStyleDialog : public SharedParametersDialog

PowerEditor/src/WinControls/ColourPicker/WordStyleDlg.cpp
@@ -120,7 +120,7 @@ intptr_t CALLBACK WordStyleDlg::run_dlgProc(UINT Message, WPARAM wParam, LPARAM 
 			}
 			::SendMessage(_hSwitch2ThemeCombo, CB_SETCURSEL, _currentThemeIndex, 0);
 
-			for (int i = 0 ; i < sizeof(fontSizeStrs)/(3*sizeof(TCHAR)) ; ++i)
+			for (size_t i = 0 ; i < sizeof(fontSizeStrs)/(3*sizeof(TCHAR)) ; ++i)
 				::SendMessage(_hFontSizeCombo, CB_ADDSTRING, 0, reinterpret_cast<LPARAM>(fontSizeStrs[i]));
 
 			const std::vector<generic_string> & fontlist = (NppParameters::getInstance()).getFontList();
@@ -572,7 +572,7 @@ int WordStyleDlg::whichTabColourIndex()
 	const size_t styleNameLen = 128;
 	TCHAR styleName[styleNameLen + 1] = { '\0' };
 	auto lbTextLen = ::SendDlgItemMessage(_hSelf, IDC_STYLES_LIST, LB_GETTEXTLEN, i, 0);
-	if (lbTextLen > styleNameLen)
+	if (static_cast<size_t>(lbTextLen) > styleNameLen)
 		return -1;
 
 	::SendDlgItemMessage(_hSelf, IDC_STYLES_LIST, LB_GETTEXT, i, reinterpret_cast<LPARAM>(styleName));
@@ -601,7 +601,7 @@ bool WordStyleDlg::isDocumentMapStyle()
 	constexpr size_t styleNameLen = 128;
 	TCHAR styleName[styleNameLen + 1] = { '\0' };
 	const auto lbTextLen = ::SendDlgItemMessage(_hSelf, IDC_STYLES_LIST, LB_GETTEXTLEN, i, 0);
-	if (lbTextLen > styleNameLen)
+	if (static_cast<size_t>(lbTextLen) > styleNameLen)
 		return false;
 
 	::SendDlgItemMessage(_hSelf, IDC_STYLES_LIST, LB_GETTEXT, i, reinterpret_cast<LPARAM>(styleName));
@@ -641,7 +641,7 @@ void WordStyleDlg::updateFontSize()
 		TCHAR intStr[intStrLen];
 
 		auto lbTextLen = ::SendMessage(_hFontSizeCombo, CB_GETLBTEXTLEN, iFontSizeSel, 0);
-		if (lbTextLen >= intStrLen)
+		if (static_cast<size_t>(lbTextLen) >= intStrLen)
 			return;
 
 		::SendMessage(_hFontSizeCombo, CB_GETLBTEXT, iFontSizeSel, reinterpret_cast<LPARAM>(intStr));
@@ -844,7 +844,7 @@ void WordStyleDlg::setVisualFromStyleList()
 	if (i == LB_ERR)
 		return;
 	auto lbTextLen = ::SendDlgItemMessage(_hSelf, IDC_LANGUAGES_LIST, LB_GETTEXTLEN, i, 0);
-	if (lbTextLen > strLen)
+	if (static_cast<size_t>(lbTextLen) > strLen)
 		return;
 
 	::SendDlgItemMessage(_hSelf, IDC_LANGUAGES_LIST, LB_GETTEXT, i, reinterpret_cast<LPARAM>(str));
@@ -855,7 +855,7 @@ void WordStyleDlg::setVisualFromStyleList()
 	const size_t styleNameLen = 64;
 	TCHAR styleName[styleNameLen + 1] = { '\0' };
 	lbTextLen = ::SendDlgItemMessage(_hSelf, IDC_STYLES_LIST, LB_GETTEXTLEN, i, 0);
-	if (lbTextLen > styleNameLen)
+	if (static_cast<size_t>(lbTextLen) > styleNameLen)
 		return;
 	::SendDlgItemMessage(_hSelf, IDC_STYLES_LIST, LB_GETTEXT, i, reinterpret_cast<LPARAM>(styleName));
 	wcscat_s(str, TEXT(" : "));

commit 3482ed97eaf398f425b4efc63d4be8035b34e8ef

Fix UDL comment config input fields broken regression
Which caused by commit 94154b0f0e281b4bbfad9c59f8d01c9dff305c12.

Fix #11239, close #11292

PowerEditor/src/ScintillaComponent/UserDefineDialog.cpp
@@ -535,11 +535,12 @@ void CommentStyleDialog::setKeywords2List(int id)
             IDC_COMMENT_CLOSE_EDIT
         };
 
-        for (size_t i = 0; i < sizeof(list)/sizeof(int); ++i)
+        TCHAR intBuffer[10] = { '0', 0 };
+        for (int i = 0; static_cast<size_t>(i) < sizeof(list) / sizeof(int); ++i)
         {
-            wstring intStr = std::to_wstring(i);
+            generic_itoa(i, intBuffer + 1, 10);
             ::GetDlgItemText(_hSelf, list[i], buffer, max_char);
-			convertTo(newList, max_char, buffer, intStr.c_str());
+            convertTo(newList, max_char, buffer, intBuffer);
         }
 
 		wcscpy_s(_pUserLang->_keywordLists[index], newList);
@@ -597,11 +598,12 @@ void CommentStyleDialog::updateDlg()
         IDC_COMMENT_CLOSE_EDIT
     };
 
-    for (size_t i=0; i<sizeof(list)/sizeof(int); ++i)
+    TCHAR intBuffer[10] = { '0', 0 };
+    for (int i = 0; static_cast<size_t>(i) < sizeof(list) / sizeof(int); ++i)
     {
-        wstring intStr = std::to_wstring(i);
-        retrieve(buffer, _pUserLang->_keywordLists[SCE_USER_KWLIST_COMMENTS], intStr.c_str());
-		::SendDlgItemMessage(_hSelf, list[i], WM_SETTEXT, 0, reinterpret_cast<LPARAM>(buffer));
+        generic_itoa(i, intBuffer + 1, 10);
+        retrieve(buffer, _pUserLang->_keywordLists[SCE_USER_KWLIST_COMMENTS], intBuffer);
+        ::SendDlgItemMessage(_hSelf, list[i], WM_SETTEXT, 0, reinterpret_cast<LPARAM>(buffer));
     }
 
     ::SendDlgItemMessage(_hSelf, IDC_FOLDING_OF_COMMENTS,   BM_SETCHECK, _pUserLang->_allowFoldOfComments,    0);
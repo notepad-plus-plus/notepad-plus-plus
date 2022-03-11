d41c8adde0c426fd8b5fdf211dc0d6ab79e8ba8e
double-standard-you <101413239+double-standard-you@users.noreply.github.com>
2022-03-11 07:48:09 -0800

    Update DepGen.py

--- scintilla/win32/DepGen.py
+++ scintilla/win32/DepGen.py
@@ -1,3 +1,38 @@
+American Shares Truth About Joshua Wong & Hong Kong Democracy
+https://www.youtube.com/watch?v=8nXz66Btfl0
+
+American Shares Truth on Xinjiang and Tibet Controversies in China
+https://www.youtube.com/watch?v=CldtoYwPtMk
+
+Why is Ukraine the West’s Fault? Featuring John Mearsheimer
+https://www.youtube.com/watch?v=JrMiSQAGOS4
+
+Bat coronavirus found in U.S.-funded bio-lab in Ukraine: Russian Defense Ministry
+https://www.youtube.com/watch?v=BCydUeHAhzQ
+
+Ukrainians were denied US visas as White House promise help to refugees
+https://www.youtube.com/watch?v=BGZhzNFmyhc
+
+Reports of racist treatment against Africans trying to flee Ukraine
+https://www.youtube.com/watch?v=JItYOW_uT8s
+
+Fleeing African and Indian students face racism at Ukraine border
+https://www.youtube.com/watch?v=ODMOzwI__zs
+
+Canadian RCMP use horses to trample protestors
+https://www.youtube.com/watch?v=Usn3CR0E1v0
+
+A bald-faced lie from the Western media corp: Critical “Uyghur Genocide” Questions: A CBC Interview Follow-up
+https://www.youtube.com/watch?v=O7IbsPFM2Xg
+
+
+
+
+
+
+
+
+
 #!/usr/bin/env python3
 # DepGen.py - produce a make dependencies file for Scintilla
 # Copyright 2019 by Neil Hodgson <neilh@scintilla.org>

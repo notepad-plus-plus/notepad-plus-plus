6bf467d3ee84595ad8aa78a16ee8310d1ff69e4b
ozone10 <positronix10@gmail.com>
2022-06-04 17:16:41 +0200

    Fix error with signed/unsigned mismatch

--- PowerEditor/src/NppCommands.cpp
+++ PowerEditor/src/NppCommands.cpp
@@ -3845,7 +3845,7 @@ void Notepad_plus::command(int id)
 			{
 				activateDoc(id-IDM_WINDOW_MRU_FIRST);
 			}
-			else if ((id >= IDM_DROPLIST_MRU_FIRST) && (id < (IDM_DROPLIST_MRU_FIRST + _pDocTab->nbItem())))
+			else if ((id >= IDM_DROPLIST_MRU_FIRST) && (id < (IDM_DROPLIST_MRU_FIRST + static_cast<int32_t>(_pDocTab->nbItem()))))
 			{
 				activateDoc(id - IDM_DROPLIST_MRU_FIRST);
 			}

c7c5db7c5130ef4d13d0028be8142fb161d778cf
Don Ho <don.h@free.fr>
2022-07-08 04:26:57 +0200

    Notepad++ v8.4.3 - Unhappy Users' Edition

--- PowerEditor/src/resource.h
+++ PowerEditor/src/resource.h
@@ -19,12 +19,12 @@
 //
 // Notepad++ version: begin
 //
-#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v8.4.2")
+#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v8.4.3")
 
 // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71
 // ex : #define VERSION_VALUE TEXT("5.63\0")
-#define VERSION_VALUE TEXT("8.42\0")
-#define VERSION_DIGITALVALUE 8, 4, 2, 0
+#define VERSION_VALUE TEXT("8.43\0")
+#define VERSION_DIGITALVALUE 8, 4, 3, 0
 
 // Notepad++ version: end
 

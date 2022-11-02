286485c6283cf017097ed899af9949b1c2de9139
Arkadiusz Michalski <nospam@please.com>
2022-11-02 03:13:51 +0100

    Test13

--- z.ps1
+++
@@ -1,5 +0,0 @@
-
-Write-Output "Tools version:"
-Write-Output (gcc --version) | select-object -first 1
-Write-Output (mingw32-make --version) | select-object -first 1
-Write-Output (sh --version) | select-object -first 1

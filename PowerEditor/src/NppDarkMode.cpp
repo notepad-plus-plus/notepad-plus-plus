69e9d5d3bbdfb4ab6c8af098376ab2fd82392dc0
ozone10 <positronix10@gmail.com>
2023-01-03 15:36:42 +0100

    Fix docking GUI for plugin in light mode

--- PowerEditor/src/NppDarkMode.cpp
+++ PowerEditor/src/NppDarkMode.cpp
@@ -1742,12 +1742,25 @@ namespace NppDarkMode
 
 				auto holdBrush = ::SelectObject(hdc, NppDarkMode::getDarkerBackgroundBrush());
 
-				auto dpiManager = NppParameters::getInstance()._dpiManager;
+				auto& dpiManager = NppParameters::getInstance()._dpiManager;
 
-				RECT rcArrow = {
-				rc.right - dpiManager.scaleX(17), rc.top + 1,
-				rc.right - 1, rc.bottom - 1
-				};
+				RECT rcArrow{};
+
+				COMBOBOXINFO cbi{};
+				cbi.cbSize = sizeof(COMBOBOXINFO);
+				const bool resultCbi = ::GetComboBoxInfo(hWnd, &cbi) != FALSE;
+				if (resultCbi)
+				{
+					rcArrow = cbi.rcButton;
+					rcArrow.left -= 1;
+				}
+				else
+				{
+					rcArrow = {
+					rc.right - dpiManager.scaleX(17), rc.top + 1,
+					rc.right - 1, rc.bottom - 1
+					};
+				}
 
 				bool hasFocus = false;
 
@@ -1757,11 +1770,21 @@ namespace NppDarkMode
 				{
 					hasFocus = ::GetFocus() == hWnd;
 
-					RECT rcTextBg = rc;
-					rcTextBg.left += 1;
-					rcTextBg.top += 1;
-					rcTextBg.right = rcArrow.left - 1;
-					rcTextBg.bottom -= 1;
+					RECT rcTextBg{};
+					if (resultCbi)
+					{
+						rcTextBg = cbi.rcItem;
+					}
+					else
+					{
+						rcTextBg = rc;
+
+						rcTextBg.left += 1;
+						rcTextBg.top += 1;
+						rcTextBg.right = rcArrow.left - 1;
+						rcTextBg.bottom -= 1;
+					}
+
 					::FillRect(hdc, &rcTextBg, NppDarkMode::getBackgroundBrush()); // erase background on item change
 
 					auto index = static_cast<int>(::SendMessage(hWnd, CB_GETCURSEL, 0, 0));
@@ -1773,12 +1796,12 @@ namespace NppDarkMode
 						TCHAR* buffer = new TCHAR[(bufferLen + 1)];
 						::SendMessage(hWnd, CB_GETLBTEXT, index, reinterpret_cast<LPARAM>(buffer));
 
-						RECT rcText = rc;
+						RECT rcText = rcTextBg;
 						rcText.left += 4;
-						rcText.right = rcArrow.left - 5;
+						rcText.right -= 4;
 
 						::DrawText(hdc, buffer, -1, &rcText, DT_NOPREFIX | DT_LEFT | DT_VCENTER | DT_SINGLELINE);
-						delete[]buffer;
+						delete[] buffer;
 					}
 				}
 				else if ((style & CBS_DROPDOWN) == CBS_DROPDOWN && hwndEdit != NULL)
@@ -1797,13 +1820,9 @@ namespace NppDarkMode
 				auto colorEnabledText = isHot ? NppDarkMode::getTextColor() : NppDarkMode::getDarkerTextColor();
 				::SetTextColor(hdc, isWindowEnabled ? colorEnabledText : NppDarkMode::getDisabledTextColor());
 				::SetBkColor(hdc, isHot ? NppDarkMode::getHotBackgroundColor() : NppDarkMode::getBackgroundColor());
-				::ExtTextOut(hdc,
-					rcArrow.left + (rcArrow.right - rcArrow.left) / 2 - dpiManager.scaleX(4),
-					rcArrow.top + 3,
-					ETO_OPAQUE | ETO_CLIPPED,
-					&rcArrow, L"˅",
-					1,
-					nullptr);
+				::FillRect(hdc, &rcArrow, isHot ? NppDarkMode::getHotBackgroundBrush() : NppDarkMode::getBackgroundBrush());
+				TCHAR arrow[] = L"˅";
+				::DrawText(hdc, arrow, -1, &rcArrow, DT_NOPREFIX | DT_CENTER | DT_VCENTER | DT_SINGLELINE | DT_NOCLIP);
 				::SetBkColor(hdc, NppDarkMode::getBackgroundColor());
 
 				auto hEnabledPen = (isHot || hasFocus) ? NppDarkMode::getHotEdgePen() : NppDarkMode::getEdgePen();
@@ -2424,17 +2443,30 @@ namespace NppDarkMode
 
 			case WM_CTLCOLOREDIT:
 			{
-				return NppDarkMode::onCtlColorSofter(reinterpret_cast<HDC>(wParam));
+				if (NppDarkMode::isEnabled())
+				{
+					return NppDarkMode::onCtlColorSofter(reinterpret_cast<HDC>(wParam));
+				}
+				break;
 			}
 
 			case WM_CTLCOLORLISTBOX:
 			{
-				return NppDarkMode::onCtlColorListbox(wParam, lParam);
+				if (NppDarkMode::isEnabled())
+				{
+					return NppDarkMode::onCtlColorListbox(wParam, lParam);
+				}
+				break;
 			}
 
 			case WM_CTLCOLORDLG:
 			{
-				return NppDarkMode::onCtlColorDarker(reinterpret_cast<HDC>(wParam));
+
+				if (NppDarkMode::isEnabled())
+				{
+					return NppDarkMode::onCtlColorDarker(reinterpret_cast<HDC>(wParam));
+				}
+				break;
 			}
 
 			case WM_CTLCOLORSTATIC:

2773f69dd0660f056d78a47f4db68c7463986068
Alan Kilborn <alankilborn@gmail.com>
2023-02-17 13:43:29 -0500

    Correct parameter error to call of doOpen

--- PowerEditor/src/NppIO.cpp
+++ PowerEditor/src/NppIO.cpp
@@ -1991,7 +1991,7 @@ void Notepad_plus::fileOpen()
 	size_t sz = fns.size();
 	for (size_t i = 0 ; i < sz ; ++i)
 	{
-		BufferID test = doOpen(fns.at(i).c_str(), fDlg.isReadOnly());
+		BufferID test = doOpen(fns.at(i).c_str(), false, fDlg.isReadOnly());
 		if (test != BUFFER_INVALID)
 			lastOpened = test;
 	}

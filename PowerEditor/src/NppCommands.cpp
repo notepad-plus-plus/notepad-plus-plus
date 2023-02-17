96a32b15703d0087c3481eecb475188bce7e8ba3
Alan Kilborn <alankilborn@gmail.com>
2023-02-17 14:43:55 -0500

    Volatile Find respect Find window settings

--- PowerEditor/src/NppCommands.cpp
+++ PowerEditor/src/NppCommands.cpp
@@ -1372,15 +1372,26 @@ void Notepad_plus::command(int id)
 		case IDM_SEARCH_VOLATILE_FINDNEXT :
 		case IDM_SEARCH_VOLATILE_FINDPREV :
 		{
-			TCHAR text2Find[MAX_PATH] = { '\0' };
-			_pEditView->getGenericSelectedText(text2Find, MAX_PATH);
+			bool isFirstTime = !_findReplaceDlg.isCreated();
+			if (isFirstTime)
+			{
+				_findReplaceDlg.doDialog(FIND_DLG, _nativeLangSpeaker.isRTL(), false);
+			}
 
-			FindOption op;
-			op._isWholeWord = false;
-			op._whichDirection = (id == IDM_SEARCH_VOLATILE_FINDNEXT?DIR_DOWN:DIR_UP);
+			const int strSize = FINDREPLACE_MAXLENGTH;
+			TCHAR str[strSize] = { '\0' };
+			_pEditView->getGenericSelectedText(str, strSize);
+			if (isFirstTime)
+			{
+				_nativeLangSpeaker.changeFindReplaceDlgLang(_findReplaceDlg);
+			}
+
+			FindOption op = _findReplaceDlg.getCurrentOptions();
+			op._searchType = FindNormal;
+			op._whichDirection = (id == IDM_SEARCH_VOLATILE_FINDNEXT ? DIR_DOWN : DIR_UP);
 
 			FindStatus status = FSNoMessage;
-			_findReplaceDlg.processFindNext(text2Find, &op, &status);
+			_findReplaceDlg.processFindNext(str, &op, &status);
 			if (status == FSEndReached)
 			{
 				generic_string msg = _nativeLangSpeaker.getLocalizedStrFromID("find-status-end-reached", TEXT("Find: Found the 1st occurrence from the top. The end of the document has been reached."));

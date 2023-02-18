e877707ecb58fb9e7b4ba5a44b3bc35aa248f980
Christian Grasser <christian.grasser@live.de>
2023-02-18 19:25:06 +0100

    Update to boost 1.85.0
    Update to https://www.boost.org/users/history/version_1_85_0.html
    - no functional changes on regex
    - support for newer compiler (e.g. VS2022) versions/features/workarounds
    
    Close #15040

--- boostregex/boost/config/assert_cxx14.hpp
+++ boostregex/boost/config/assert_cxx14.hpp
@@ -1,4 +1,4 @@
-//  This file was automatically generated on Sun Jun  5 16:50:18 2022
+//  This file was automatically generated on Fri Oct 13 19:09:38 2023
 //  by libs/config/tools/generate.cpp
 //  Copyright John Maddock 2002-21.
 //  Use, modification and distribution are subject to the 

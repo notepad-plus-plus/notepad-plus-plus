1d949b6d967a119952866af835bb78eaffd1b67b
Christian Grasser <christian.grasser@live.de>
2023-07-06 00:10:36 +0200

    - update CMakeLists.txt to contain latest changes sha1 and sha512.cpp - fixed uppercase issue on cross platform build with mingw on linux

--- PowerEditor/src/MISC/sha512/sha512.cpp
+++ PowerEditor/src/MISC/sha512/sha512.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include <Windows.h>
+#include <windows.h>
 #include <wincrypt.h>
 #include "sha512.h"
 

4df27c8e8bc6238cea6af3a630c4240a9be25f39
Don Ho <don.h@free.fr>
2023-07-27 02:55:28 +0200

    Fix Project panel inaccurate save dialog initilized path
    Fix #13086, close #13936

--- PowerEditor/src/WinControls/ProjectPanel/ProjectPanel.cpp
+++ PowerEditor/src/WinControls/ProjectPanel/ProjectPanel.cpp
@@ -1192,6 +1192,7 @@ bool ProjectPanel::saveWorkSpaceAs(bool saveCopyAs)
 	CustomFileDialog fDlg(_hSelf);
 	setFileExtFilter(fDlg);
 	fDlg.setExtIndex(0);		// 0 index for "custom extention" type if any else for "All types *.*"
+	fDlg.setFolder(getWorkSpaceFilePath());
 
 	const generic_string fn = fDlg.doSaveDlg();
 	if (fn.empty())

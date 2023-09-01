6567a890947293f47aae79d37a9782aeeb16c4e6
Jaroslav Lobačevski <jarlob@github.com>
2023-09-01 17:28:58 +0200

    Add comment

--- PowerEditor/src/Utf8_16.h
+++ PowerEditor/src/Utf8_16.h
@@ -53,6 +53,8 @@ public:
 	bool get(utf8 *c);
 	void operator++();
 	eState getState() { return m_eState; };
+        // Utf8_16::read always consumes two bytes,
+        // thus the bool operator checks if m_pRead is less than m_pEnd by two bytes.
 	operator bool() { return (m_pRead + 1 < m_pEnd) || (m_out1st != m_outLst); };
 
 protected:

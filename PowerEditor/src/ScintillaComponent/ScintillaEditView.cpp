de297b1954d8867b857313fb5bcd4331e6951a08
Don Ho <don.h@free.fr>
2023-11-28 18:47:57 +0100

    Fix visual glichy on multi-paste
    Remove also Shift-DEL, Ctrl-INS & Shift-INS shortcut from Scintilla.
    Ref: https://github.com/notepad-plus-plus/notepad-plus-plus/issues/14401#issuecomment-1830302635
    
    Fix #14410

--- PowerEditor/src/ScintillaComponent/ScintillaEditView.cpp
+++ PowerEditor/src/ScintillaComponent/ScintillaEditView.cpp
@@ -509,16 +509,6 @@ LRESULT ScintillaEditView::scintillaNew_Proc(HWND hwnd, UINT Message, WPARAM wPa
 				::SendMessage(_hParent, WM_NOTIFY, LINKTRIGGERED, reinterpret_cast<LPARAM>(&notification));
 
 			}
-			else if (wParam == 'V')
-			{
-				if (_isMultiPasteActive)
-				{
-					Buffer* buf = getCurrentBuffer();
-					buf->setUserReadOnly(false);
-					_isMultiPasteActive = false;
-					::SendMessage(_hParent, NPPM_INTERNAL_CHECKUNDOREDOSTATE, 0, 0);
-				}
-			}
 			break;
 		}
 
@@ -647,48 +637,6 @@ LRESULT ScintillaEditView::scintillaNew_Proc(HWND hwnd, UINT Message, WPARAM wPa
 
 				}
 
-			}
-			else
-			{
-				switch (wParam)
-				{
-					//
-					// 2 shortcuts:
-					// Ctrl + C: without selected text, it will copy the whole line.
-					// Ctrl + X: without selected text, it will cut the whole line.
-					//
-					case 'C':
-					case 'X':
-					{
-						if (((ctrl & 0x8000) && !(alt & 0x8000) && !(shift & 0x8000)) && !hasSelection())
-						{
-							execute(wParam == 'C' ? SCI_LINECOPY : SCI_LINECUT);
-							//return TRUE;
-							// No return and let Scintilla procedure to continue
-						}
-					}
-					break;
-
-					case 'V':
-					{
-						if ((ctrl & 0x8000) && !(alt & 0x8000) && !(shift & 0x8000))
-						{
-							Buffer* buf = getCurrentBuffer();
-							bool isRO = buf->isReadOnly();
-							size_t nbSelections = execute(SCI_GETSELECTIONS);
-							if (nbSelections > 1 && !isRO)
-							{
-								if (pasteToMultiSelection())
-								{
-									// Hack for preventing the char "SYN" (0x16) from being adding into edit zone 
-									buf->setUserReadOnly(true);
-
-									_isMultiPasteActive = true; // It will be set false with WM_KEYUP message
-								}
-							}
-						}
-					}
-				}
 			}
 			break;
 		}

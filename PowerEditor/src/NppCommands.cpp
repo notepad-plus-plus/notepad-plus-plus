notepad-plus-plus/notepad-plus-plus

commit f7c44b4413457e42e0ba56ae84f34ef0540029e8

Restore Shift-DEL, Ctrl-INS and Shift-INS & fix Ctrl+V can't paste into the text fields of forms for certain plugins
This PR restores shortcuts Shift-DEL, Ctrl-INS and Shift-INS for Cut / Copy / Paste respectively.
the action to delete entire line without selection is removed in this PR due to its shortcut Shift-DEL has triggered several critical issues.

To delete entire line, the users are encouraged to use Scintilla default shortcuts set in Shortcut Mapper: Ctrl-Shift-L (SCI_LINEDELETE). User can remap it to another shortcut via Shortcut mapper.

Fix #14568, fix #14470, fix #14571, close #14557, close #14569

PowerEditor/src/NppCommands.cpp
@@ -360,14 +360,19 @@ void Notepad_plus::command(int id)
 			HWND focusedHwnd = ::GetFocus();
 			if (focusedHwnd == _pEditView->getHSelf())
 			{
-				if (_pEditView->hasSelection())
-					_pEditView->execute(WM_CUT);
-				else
+				if (_pEditView->hasSelection()) // Cut normally
 				{
-					bool useLinCopyCut = (NppParameters::getInstance()).useLineCopyCutDelete();
-					if (useLinCopyCut)
-						_pEditView->execute(SCI_LINECUT); // Ctrl + X: without selected text, it will cut the whole line.
+					_pEditView->execute(WM_CUT);
 				}
+				else // Cul the entire line
+				{
+					_pEditView->execute(SCI_COPYALLOWLINE);
+					_pEditView->execute(SCI_LINEDELETE);
+				}
+			}
+			else
+			{
+				::SendMessage(focusedHwnd, WM_CUT, 0, 0);
 			}
 			break;
 		}
@@ -377,20 +382,16 @@ void Notepad_plus::command(int id)
 			HWND focusedHwnd = ::GetFocus();
 			if (focusedHwnd == _pEditView->getHSelf())
 			{
-				if (_pEditView->hasSelection())
-					_pEditView->execute(WM_COPY);
-				else
-				{
-					bool useLinCopyCut = (NppParameters::getInstance()).useLineCopyCutDelete();
-					if (useLinCopyCut)
-						_pEditView->execute(SCI_LINECOPY); // Ctrl + C: without selected text, it will copy the whole line.
-				}
+				_pEditView->execute(SCI_COPYALLOWLINE); // Copy selected text if any.
+														// Otherwise copy the entire line with EOL, for pasting before any line where the caret is.
 			}
-			else  // Search result
+			else
 			{
 				Finder* finder = _findReplaceDlg.getFinderFrom(focusedHwnd);
-				if (finder)
+				if (finder)  // Search result
 					finder->scintillaExecute(WM_COPY);
+				else
+					::SendMessage(focusedHwnd, WM_COPY, 0, 0);
 			}
 
 			break;
@@ -487,6 +488,10 @@ void Notepad_plus::command(int id)
 				}
 
 				_pEditView->execute(SCI_PASTE);
+			}
+			else
+			{
+				::SendMessage(focusedHwnd, WM_PASTE, 0, 0);
 			}
 		}
 		break;

PowerEditor/src/Parameters.cpp
@@ -84,9 +84,16 @@ static const WinMenuKeyDefinition winKeyDefs[] =
 
 //	{ VK_NULL,    IDM_EDIT_UNDO,                                false, false, false, nullptr },
 //	{ VK_NULL,    IDM_EDIT_REDO,                                false, false, false, nullptr },
+
+	{ VK_DELETE,  IDM_EDIT_CUT,                                 false, false, true,  nullptr },
 	{ VK_X,       IDM_EDIT_CUT,                                 true,  false, false, nullptr },
+
+	{ VK_INSERT,  IDM_EDIT_COPY,                                true,  false, false, nullptr },
 	{ VK_C,       IDM_EDIT_COPY,                                true,  false, false, nullptr },
+
+	{ VK_INSERT,  IDM_EDIT_PASTE,                               false, false, true,  nullptr },
 	{ VK_V,       IDM_EDIT_PASTE,                               true,  false, false, nullptr },
+
 //	{ VK_NULL,    IDM_EDIT_DELETE,                              false, false, false, nullptr },
 //	{ VK_NULL,    IDM_EDIT_SELECTALL,                           false, false, false, nullptr },
 	{ VK_B,       IDM_EDIT_BEGINENDSELECT,                      true,  false, true,  nullptr },
@@ -472,11 +479,12 @@ static const ScintillaKeyDefinition scintKeyDefs[] =
     //Scintilla command name,             SCINTILLA_CMD_ID,            Ctrl,  Alt,   Shift, V_KEY,       NOTEPAD++_CMD_ID
 	// -------------------------------------------------------------------------------------------------------------------
 	//
-//	{TEXT("SCI_CUT"),                     SCI_CUT,                     false, false, true,  VK_DELETE,   0},
-//	{TEXT("SCI_COPY"),                    SCI_COPY,                    true,  false, false, VK_INSERT,   0},
-//	{TEXT("SCI_PASTE"),                   SCI_PASTE,                   false, false, true,  VK_INSERT,   0},
-//	the above 3 shortcuts will be added dynamically if "disableLineCopyCutDelete.xml" is present.
-
+	//{TEXT("SCI_CUT"),                     SCI_CUT,                     true,  false, false, VK_X,        IDM_EDIT_CUT},
+	//{TEXT(""),                            SCI_CUT,                     false, false, true,  VK_DELETE,   0},
+	//{TEXT("SCI_COPY"),                    SCI_COPY,                    true,  false, false, VK_C,        IDM_EDIT_COPY},
+	//{TEXT(""),                            SCI_COPY,                    true,  false, false, VK_INSERT,   0},
+	//{TEXT("SCI_PASTE"),                   SCI_PASTE,                   true,  false, false, VK_V,        IDM_EDIT_PASTE},
+	//{TEXT("SCI_PASTE"),                   SCI_PASTE,                   false, false, true,  VK_INSERT,   IDM_EDIT_PASTE},
 	{TEXT("SCI_SELECTALL"),               SCI_SELECTALL,               true,  false, false, VK_A,        IDM_EDIT_SELECTALL},
 	{TEXT("SCI_CLEAR"),                   SCI_CLEAR,                   false, false, false, VK_DELETE,   IDM_EDIT_DELETE},
 	{TEXT("SCI_CLEARALL"),                SCI_CLEARALL,                false, false, false, 0,           0},
@@ -1474,35 +1482,6 @@ bool NppParameters::load()
 		delete _pXmlToolIconsDoc;
 		_pXmlToolIconsDoc = nullptr;
 		isAllLaoded = false;
-	}
-
-
-	//-----------------------------------------------------------------------------------//
-	// disableLineCopyCutDelete.xml                                                      //
-	// This empty xml file is optional - user adds this empty file manually to :         //
-	// 1. prevent hard coded Shift-DEL shortcut deletes whole line while no selection.   //
-	// 2. prevent Copy command (Ctrl-C) copies whole line (without selection).           //
-	// 3. prevent Cut command (Ctrl-X) cuts whole line (without selection).              //
-	// 4. add SCI_CUT (Shift-DEL), SCI_COPY (Ctrl-INS) & SCI_PASTE (Shift-INS) shortcuts //
-	//-----------------------------------------------------------------------------------//
-	std::wstring disableLineCopyCutDeletePath = _userPath;
-	pathAppend(disableLineCopyCutDeletePath, TEXT("disableLineCopyCutDelete.xml"));
-
-	if (PathFileExists(disableLineCopyCutDeletePath.c_str()))
-	{
-		_useLineCopyCutDelete = false;
-		
-		//
-		// Add back SCI_CUT (Shift-DEL), SCI_COPY (Ctrl-INS) & SCI_PASTE (Shift-INS) shortcuts
-		//
-		ScintillaKeyMap sci_cut = ScintillaKeyMap(Shortcut("SCI_CUT", false, false, true, static_cast<unsigned char>(VK_DELETE)), SCI_CUT, 0);
-		_scintillaKeyCommands.push_back(sci_cut);
-
-		ScintillaKeyMap sci_copy = ScintillaKeyMap(Shortcut("SCI_COPY", true, false, false, static_cast<unsigned char>(VK_INSERT)), SCI_COPY, 0);
-		_scintillaKeyCommands.push_back(sci_copy);
-
-		ScintillaKeyMap sci_paste = ScintillaKeyMap(Shortcut("SCI_PASTE", false, false, true, static_cast<unsigned char>(VK_INSERT)), SCI_PASTE, 0);
-		_scintillaKeyCommands.push_back(sci_paste);
 	}
 
 	//------------------------------//

PowerEditor/src/Notepad_plus.cpp
@@ -2567,8 +2567,11 @@ void Notepad_plus::checkClipboard()
 {
 	bool hasSelection = _pEditView->hasSelection();
 	bool canPaste = (_pEditView->execute(SCI_CANPASTE) != 0);
+	//enableCommand(IDM_EDIT_CUT, hasSelection, MENU | TOOLBAR);
+	//enableCommand(IDM_EDIT_COPY, hasSelection, MENU | TOOLBAR);
 
 	enableCommand(IDM_EDIT_PASTE, canPaste, MENU | TOOLBAR);
+	enableCommand(IDM_EDIT_DELETE, hasSelection, MENU | TOOLBAR);
 	enableCommand(IDM_EDIT_UPPERCASE, hasSelection, MENU);
 	enableCommand(IDM_EDIT_LOWERCASE, hasSelection, MENU);
 	enableCommand(IDM_EDIT_PROPERCASE_FORCE, hasSelection, MENU);

PowerEditor/src/ScintillaComponent/ScintillaEditView.cpp
@@ -525,19 +525,10 @@ LRESULT ScintillaEditView::scintillaNew_Proc(HWND hwnd, UINT Message, WPARAM wPa
 			SHORT shift = GetKeyState(VK_SHIFT);
 			bool isColumnSelection = (execute(SCI_GETSELECTIONMODE) == SC_SEL_RECTANGLE) || (execute(SCI_GETSELECTIONMODE) == SC_SEL_THIN);
 			bool column2MultSelect = (NppParameters::getInstance()).doColumn2MultiSelect();
-			bool useHardCodedShiftDelete = (NppParameters::getInstance()).useLineCopyCutDelete();
 
 			if (wParam == VK_DELETE)
 			{
-				// 1 shortcut:
-				// Shift + Delete: without selected text, it will delete the whole line.
-				//
-				if ((shift & 0x8000) && !(ctrl & 0x8000) && !(alt & 0x8000) && !hasSelection() && useHardCodedShiftDelete) // Shift-DEL & no selection
-				{
-					execute(SCI_LINEDELETE);
-					return TRUE;
-				}
-				else if (!(shift & 0x8000) && !(ctrl & 0x8000) && !(alt & 0x8000)) // DEL & Multi-edit
+				if (!(shift & 0x8000) && !(ctrl & 0x8000) && !(alt & 0x8000)) // DEL & Multi-edit
 				{
 					size_t nbSelections = execute(SCI_GETSELECTIONS);
 					if (nbSelections > 1) // Multi-edit

commit 75ae73c17333e6a83ce84c11ef052b6e2e5693ca

Fix Copy/Cut/Paste shortcut modification conflict problem
The bug is introduced by:
https://github.com/notepad-plus-plus/notepad-plus-plus/commit/f7c44b4413457e42e0ba56ae84f34ef0540029e8

Fix #14585, close #14586

PowerEditor/src/NppCommands.cpp
@@ -364,7 +364,7 @@ void Notepad_plus::command(int id)
 				{
 					_pEditView->execute(WM_CUT);
 				}
-				else // Cul the entire line
+				else // Cut the entire line
 				{
 					_pEditView->execute(SCI_COPYALLOWLINE);
 					_pEditView->execute(SCI_LINEDELETE);

PowerEditor/src/Parameters.cpp
@@ -2082,11 +2082,13 @@ void NppParameters::initMenuKeys()
 {
 	int nbCommands = sizeof(winKeyDefs)/sizeof(WinMenuKeyDefinition);
 	WinMenuKeyDefinition wkd;
+	int previousFuncID = 0;
 	for (int i = 0; i < nbCommands; ++i)
 	{
 		wkd = winKeyDefs[i];
 		Shortcut sc((wkd.specialName ? wstring2string(wkd.specialName, CP_UTF8).c_str() : ""), wkd.isCtrl, wkd.isAlt, wkd.isShift, static_cast<unsigned char>(wkd.vKey));
-		_shortcuts.push_back( CommandShortcut(sc, wkd.functionId) );
+		_shortcuts.push_back( CommandShortcut(sc, wkd.functionId, previousFuncID == wkd.functionId) );
+		previousFuncID = wkd.functionId;
 	}
 }
 
@@ -2839,12 +2841,15 @@ void NppParameters::feedShortcut(TiXmlNodeA *node)
 		{
 			//find the commandid that matches this Shortcut sc and alter it, push back its index in the modified list, if not present
 			size_t len = _shortcuts.size();
-			for (size_t i = 0; i < len; ++i)
+			bool isFound = false;
+			for (size_t i = 0; i < len, !isFound; ++i)
 			{
 				if (_shortcuts[i].getID() == (unsigned long)id)
 				{	//found our match
-					getShortcuts(childNode, _shortcuts[i]);
-					addUserModifiedIndex(i);
+					isFound = getInternalCommandShortcuts(childNode, _shortcuts[i]);
+
+					if (isFound)
+						addUserModifiedIndex(i);
 				}
 			}
 		}
@@ -3031,6 +3036,52 @@ void NppParameters::feedScintKeys(TiXmlNodeA *node)
 	}
 }
 
+bool NppParameters::getInternalCommandShortcuts(TiXmlNodeA *node, CommandShortcut & cs, string* folderName)
+{
+	if (!node) return false;
+
+	const char* name = (node->ToElement())->Attribute("name");
+	if (!name)
+		name = "";
+
+	bool isCtrl = false;
+	const char* isCtrlStr = (node->ToElement())->Attribute("Ctrl");
+	if (isCtrlStr)
+		isCtrl = (strcmp("yes", isCtrlStr) == 0);
+
+	bool isAlt = false;
+	const char* isAltStr = (node->ToElement())->Attribute("Alt");
+	if (isAltStr)
+		isAlt = (strcmp("yes", isAltStr) == 0);
+
+	bool isShift = false;
+	const char* isShiftStr = (node->ToElement())->Attribute("Shift");
+	if (isShiftStr)
+		isShift = (strcmp("yes", isShiftStr) == 0);
+
+	int key;
+	const char* keyStr = (node->ToElement())->Attribute("Key", &key);
+	if (!keyStr)
+		return false;
+
+	int nth = -1; // 0 based
+	const char* nthStr = (node->ToElement())->Attribute("nth", &nth);
+	if (nthStr && nth == 1)
+	{
+		if (cs.getNth() != nth)
+			return false;
+	}
+		
+	if (folderName)
+	{
+		const char* fn = (node->ToElement())->Attribute("FolderName");
+		*folderName = fn ? fn : "";
+	}
+
+	cs = Shortcut(name, isCtrl, isAlt, isShift, static_cast<unsigned char>(key));
+	return true;
+}
+
 bool NppParameters::getShortcuts(TiXmlNodeA *node, Shortcut & sc, string* folderName)
 {
 	if (!node) return false;
@@ -3058,6 +3109,7 @@ bool NppParameters::getShortcuts(TiXmlNodeA *node, Shortcut & sc, string* folder
 	const char* keyStr = (node->ToElement())->Attribute("Key", &key);
 	if (!keyStr)
 		return false;
+
 
 	if (folderName)
 	{
@@ -3458,6 +3510,8 @@ void NppParameters::insertCmd(TiXmlNodeA *shortcutsRoot, const CommandShortcut &
 	sc->ToElement()->SetAttribute("Alt", key._isAlt?"yes":"no");
 	sc->ToElement()->SetAttribute("Shift", key._isShift?"yes":"no");
 	sc->ToElement()->SetAttribute("Key", key._key);
+	if (cmd.getNth() != 0)
+		sc->ToElement()->SetAttribute("nth", cmd.getNth());
 }
 
 

PowerEditor/src/Parameters.h
@@ -1603,7 +1603,6 @@ public:
 	bool isRemappingShortcut() const {return _shortcuts.size() != 0;};
 
 	std::vector<CommandShortcut> & getUserShortcuts() { return _shortcuts; };
-	std::vector<size_t> & getUserModifiedShortcuts() { return _customizedShortcuts; };
 	void addUserModifiedIndex(size_t index);
 
 	std::vector<MacroShortcut> & getMacroList() { return _macros; };
@@ -2017,6 +2016,7 @@ private:
 
 	void getActions(TiXmlNodeA *node, Macro & macro);
 	bool getShortcuts(TiXmlNodeA *node, Shortcut & sc, std::string* folderName = nullptr);
+	bool getInternalCommandShortcuts(TiXmlNodeA* node, CommandShortcut& cs, std::string* folderName = nullptr);
 
 	void writeStyle2Element(const Style & style2Write, Style & style2Sync, TiXmlElement *element);
 	void insertUserLang2Tree(TiXmlNode *node, UserLangContainer *userLang);

PowerEditor/src/WinControls/shortcut/shortcut.cpp
@@ -1264,3 +1264,13 @@ void CommandShortcut::setCategoryFromMenu(HMENU hMenu)
 	else
 		pNativeSpeaker->getMainMenuEntryName(_category, hMenu, "run", L"Run");
 }
+
+CommandShortcut& CommandShortcut::operator = (const Shortcut& sct)
+{
+	if (this != &sct)
+	{
+		strcpy(this->_name, sct.getName());
+		this->_keyCombo = sct.getKeyCombo();
+	}
+	return *this;
+}

PowerEditor/src/WinControls/shortcut/shortcut.h
@@ -184,19 +184,26 @@ protected :
 		 
 class CommandShortcut : public Shortcut {
 public:
-	CommandShortcut(const Shortcut& sc, long id) : Shortcut(sc), _id(id){
+	CommandShortcut(const Shortcut& sc, long id, bool isDuplicated = false) : Shortcut(sc), _id(id) {
 		_shortcutName = string2wstring(getName(), CP_UTF8);
+		if (isDuplicated) _nth = 1;
 	};
+
+	CommandShortcut& operator = (const Shortcut& sct);
 	void setCategoryFromMenu(HMENU hMenu);
 	unsigned long getID() const {return _id;};
 	void setID(unsigned long id) { _id = id;};
+	int getNth() const { return _nth; };
 	const TCHAR * getCategory() const { return _category.c_str(); };
 	const TCHAR * getShortcutName() const { return _shortcutName.c_str(); };
 
 private :
-	unsigned long _id;
+	unsigned long _id = 0;
 	generic_string _category;
 	generic_string _shortcutName;
+	int _nth = 0; // Allow several shortcuts for the same command (_id).
+	              // If there is the 2nd identical command in winKeyDefs array, the value of _nth will be 1.
+	              // This variable member allows the application to distinguish the different shortcuts assigned to the same command.
 };
 
 
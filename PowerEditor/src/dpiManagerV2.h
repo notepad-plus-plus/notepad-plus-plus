8b99917da7f43a43246d1b6ac33eafd941d2599d
ozone10 <positronix10@gmail.com>
2024-03-12 14:53:11 +0100

    Requested changes, coding std

--- PowerEditor/src/dpiManagerV2.h
+++ PowerEditor/src/dpiManagerV2.h
@@ -21,184 +21,93 @@
 class DPIManagerV2
 {
 public:
-	DPIManagerV2()
-	{
+	DPIManagerV2() {
 		setDpiWithSystem();
 	}
 	virtual ~DPIManagerV2() = default;
 
 	enum class FontType { menu, status, message, caption, smcaption };
 
-	static UINT getDpiForSystem()
-	{
-		if (NppDarkMode::isWindows10())
-		{
-			return ::GetDpiForSystem();
-		}
-
-		UINT dpi = USER_DEFAULT_SCREEN_DPI;
-		HDC hdc = ::GetDC(nullptr);
-		if (hdc != nullptr)
-		{
-			dpi = ::GetDeviceCaps(hdc, LOGPIXELSX);
-			::ReleaseDC(nullptr, hdc);
-		}
-		return dpi;
-	}
-
-	static UINT getDpiForWindow(HWND hWnd)
-	{
-		if (NppDarkMode::isWindows10())
-		{
-			const auto dpi = ::GetDpiForWindow(hWnd);
-			if (dpi > 0)
-			{
-				return dpi;
-			}
-		}
-		return getDpiForSystem();
-	}
-
-	static UINT getDpiForParent(HWND hWnd)
-	{
+	static UINT getDpiForSystem();
+	static UINT getDpiForWindow(HWND hWnd);
+	static UINT getDpiForParent(HWND hWnd) {
 		return getDpiForWindow(::GetParent(hWnd));
 	}
 
-	void setDpiWithSystem()
-	{
+	void setDpiWithSystem() {
 		_dpi = getDpiForSystem();
 	}
 
-	void setDpi(WPARAM wParam)
-	{
+	// parameter is WPARAM
+	void setDpiWP(WPARAM wParam) {
 		_dpi = LOWORD(wParam);
 	}
 
-	void setDpiValue(UINT newDpi)
-	{
+	void setDpi(UINT newDpi) {
 		_dpi = newDpi;
 	}
 
-	void setDpi(HWND hWnd)
-	{
-		if (NppDarkMode::isWindows10())
-		{
-			setDpiValue(getDpiForWindow(hWnd));
-		}
-		else
-		{
-			setDpiWithSystem();
-		}
+	void setDpi(HWND hWnd) {
+		setDpi(getDpiForWindow(hWnd));
 	}
 
-	void setDpiWithParent(HWND hWnd)
-	{
+	void setDpiWithParent(HWND hWnd) {
 		setDpi(::GetParent(hWnd));
 	}
 
-	UINT getDpi() const { return _dpi; }
-
-	static void setPositionDpi(LPARAM lParam, HWND hWnd)
-	{
-		const auto prcNewWindow = reinterpret_cast<RECT*>(lParam);
-
-		::SetWindowPos(hWnd,
-			nullptr,
-			prcNewWindow->left,
-			prcNewWindow->top,
-			prcNewWindow->right - prcNewWindow->left,
-			prcNewWindow->bottom - prcNewWindow->top,
-			SWP_NOZORDER | SWP_NOACTIVATE);
+	UINT getDpi() const {
+		return _dpi;
 	}
 
-	static int scale(int x, UINT dpi, UINT dpi2) { return MulDiv(x, dpi, dpi2); }
-	static int scale(int x, UINT dpi) { return scale(x, dpi, USER_DEFAULT_SCREEN_DPI); }
-	static int unscale(int x, UINT dpi) { return scale(x, USER_DEFAULT_SCREEN_DPI, dpi); }
+	static void setPositionDpi(LPARAM lParam, HWND hWnd);
 
-	int scale(int x) { return scale(x, _dpi); }
-	int unscale(int x) { return unscale(x, _dpi); }
-
-	int scaleX(int x) { return scale(x); }
-	int unscaleX(int x) { return unscale(x); }
-
-	int scaleY(int y) { return scale(y); }
-	int unscaleY(int y) { return unscale(y); }
-
-	static int scaleFont(int pt, UINT dpi) { return -(scale(pt, dpi, 72)); }
-	int scaleFont(int pt) { return scaleFont(pt, _dpi); }
-
-	static LOGFONT getDefaultGUIFontForDpi(UINT dpi, FontType type = FontType::message)
-	{
-		int result = 0;
-		LOGFONT lf{};
-		NONCLIENTMETRICS ncm{};
-		ncm.cbSize = sizeof(NONCLIENTMETRICS);
-		if (NppDarkMode::isWindows10()
-			&& (::SystemParametersInfoForDpi(SPI_GETNONCLIENTMETRICS, sizeof(NONCLIENTMETRICS), &ncm, 0, dpi) != FALSE))
-		{
-			result = 2;
-		}
-		else if (::SystemParametersInfo(SPI_GETNONCLIENTMETRICS, sizeof(NONCLIENTMETRICS), &ncm, 0) != FALSE)
-		{
-			result = 1;
-		}
-
-		if (result > 0)
-		{
-			switch (type)
-			{
-				case FontType::menu:
-				{
-					lf = ncm.lfMenuFont;
-					break;
-				}
-
-				case FontType::status:
-				{
-					lf = ncm.lfStatusFont;
-					break;
-				}
-
-				case FontType::caption:
-				{
-					lf = ncm.lfCaptionFont;
-					break;
-				}
-
-				case FontType::smcaption:
-				{
-					lf = ncm.lfSmCaptionFont;
-					break;
-				}
-				//case FontType::message:
-				default:
-				{
-					lf = ncm.lfMessageFont;
-					break;
-				}
-			}
-		}
-		else // should not happen, fallback
-		{
-			auto hf = static_cast<HFONT>(::GetStockObject(DEFAULT_GUI_FONT));
-			::GetObject(hf, sizeof(LOGFONT), &lf);
-		}
-
-		if (result < 2)
-		{
-			lf.lfHeight = scaleFont(lf.lfHeight, dpi);
-		}
-
-		return lf;
+	static int scale(int x, UINT dpi, UINT dpi2) {
+		return MulDiv(x, dpi, dpi2);
 	}
 
-	static LOGFONT getDefaultGUIFontForDpi(HWND hWnd, FontType type = FontType::message)
-	{
-		if (NppDarkMode::isWindows10())
-		{
-			return getDefaultGUIFontForDpi(getDpiForWindow(hWnd), type);
-		}
-		return getDefaultGUIFontForDpi(getDpiForSystem(), type);
+	static int scale(int x, UINT dpi) {
+		return scale(x, dpi, USER_DEFAULT_SCREEN_DPI);
+	}
+
+	static int unscale(int x, UINT dpi) {
+		return scale(x, USER_DEFAULT_SCREEN_DPI, dpi);
+	}
+
+	int scale(int x) {
+		return scale(x, _dpi);
+	}
+
+	int unscale(int x) {
+		return unscale(x, _dpi);
+	}
+
+	int scaleX(int x) {
+		return scale(x);
+	}
+
+	int unscaleX(int x) {
+		return unscale(x);
+	}
+
+	int scaleY(int y) {
+		return scale(y);
+	}
+
+	int unscaleY(int y) {
+		return unscale(y);
+	}
+
+	static int scaleFont(int pt, UINT dpi) {
+		return -(scale(pt, dpi, 72));
+	}
+
+	int scaleFont(int pt) {
+		return scaleFont(pt, _dpi);
+	}
+
+	static LOGFONT getDefaultGUIFontForDpi(UINT dpi, FontType type = FontType::message);
+	static LOGFONT getDefaultGUIFontForDpi(HWND hWnd, FontType type = FontType::message) {
+		return getDefaultGUIFontForDpi(getDpiForWindow(hWnd), type);
 	}
 
 private:

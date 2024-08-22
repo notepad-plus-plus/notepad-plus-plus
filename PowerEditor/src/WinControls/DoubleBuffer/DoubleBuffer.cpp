132b369ee7cd05ccdb8b19b2fe493db108d4563c
Christian Grasser <christian.grasser@live.de>
2024-08-23 00:56:54 +0200

    avoid linux issue
    ../src/WinControls/DoubleBuffer/DoubleBuffer.cpp:18:10: fatal error: CommCtrl.h: No such file or directory
       18 | #include <CommCtrl.h>
          |          ^~~~~~~~~~~~

--- PowerEditor/src/WinControls/DoubleBuffer/DoubleBuffer.cpp
+++ PowerEditor/src/WinControls/DoubleBuffer/DoubleBuffer.cpp
@@ -15,7 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "DoubleBuffer.h"
-#include <CommCtrl.h>
+#include <commctrl.h>
 #include <cassert>
 #include <memory>
 

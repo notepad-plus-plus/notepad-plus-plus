85c390efded4255dba52bc2e3ba2bd41faae8a31
Don Ho <don.h@free.fr>
2024-09-03 19:20:51 +0200

    Fix NPPM_GETNATIVELANGFILENAME's comment documentation & 2 tipos
    Fix #15599

--- PowerEditor/src/MISC/PluginsManager/Notepad_plus_msgs.h
+++ PowerEditor/src/MISC/PluginsManager/Notepad_plus_msgs.h
@@ -892,7 +892,7 @@ enum Platform { PF_UNKNOWN, PF_X86, PF_X64, PF_IA64, PF_ARM64 };
 	// Users should call it with commandLineStr as NULL to get the required number of wchar_t (not including the terminating nul character),
 	// allocate commandLineStr buffer with the return value + 1, then call it again to get the current command line string.
 	// wParam[in]: strLen is "commandLineStr" buffer length
-	// lParam[out]: commandLineStr recieves all copied command line string
+	// lParam[out]: commandLineStr receives all copied command line string
 	// Return the number of wchar_t copied/to copy
 
 
@@ -984,9 +984,9 @@ enum Platform { PF_UNKNOWN, PF_X86, PF_X64, PF_IA64, PF_ARM64 };
 	// int NPPM_GETNATIVELANGFILENAME(size_t strLen, char* nativeLangFileName)
 	// Get the Current native language file name string. Use it after getting NPPN_READY notification to find out which native language is used.
 	// Users should call it with nativeLangFileName as NULL to get the required number of char (not including the terminating nul character),
-	// allocate commandLineStr buffer with the return value + 1, then call it again to get the current native language file name string.
-	// wParam[in]: strLen is "commandLineStr" buffer length
-	// lParam[out]: commandLineStr recieves all copied native language file name string
+	// allocate language file name string buffer with the return value + 1, then call it again to get the current native language file name string.
+	// wParam[in]: strLen is "language file name string" buffer length
+	// lParam[out]: language file name string receives all copied native language file name string
 	// Return the number of char copied/to copy
 
 	// For RUNCOMMAND_USER

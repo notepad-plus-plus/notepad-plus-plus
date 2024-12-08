abbe60e74c495b94809f0171c16b2db7040a2ee0
xomx <xomx@users.noreply.github.com>
2024-12-08 03:30:07 +0100

    Allow using the Scintilla DirectWrite in non-Core Windows Server
    Fix #15916, close #15921

--- PowerEditor/src/ScintillaComponent/ScintillaEditView.cpp
+++ PowerEditor/src/ScintillaComponent/ScintillaEditView.cpp
@@ -320,8 +320,16 @@ void ScintillaEditView::init(HINSTANCE hInst, HWND hPere)
 		isWINE = ::GetProcAddress(hNtdllModule, "wine_get_version");
 
 	if (isWINE || // There is a performance issue under WINE when DirectWrite is ON, so we turn it off if user uses Notepad++ under WINE
-		::IsWindowsServer()) // In the case of Windows Server Core, DirectWrite cannot be on.
-		nppGui._writeTechnologyEngine = defaultTechnology;
+		isCoreWindows()) // In the case of Windows Server Core, DirectWrite cannot be on.
+	{
+		nppGui._writeTechnologyEngine = directWriteTechnologyUnavailable;
+	}
+	else
+	{
+		// allow IDC_CHECK_DIRECTWRITE_ENABLE to be set in Preferences > MISC. again
+		if (nppGui._writeTechnologyEngine == directWriteTechnologyUnavailable)
+			nppGui._writeTechnologyEngine = defaultTechnology;
+	}
 
 	if (nppGui._writeTechnologyEngine == directWriteTechnology)
 	{

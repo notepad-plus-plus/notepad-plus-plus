notepad-plus-plus/notepad-plus-plus

commit dd0b557e145fc664e2c2c10bda2227b1b46e1921

Fix encoding of nfo file cannot be changed bug
Fix #8823, fix #9153, fix #13905, close #15902

PowerEditor/src/ScintillaComponent/ScintillaEditView.cpp
@@ -1859,12 +1859,11 @@ void ScintillaEditView::defineDocType(LangType typeDoc)
 			setSpecialStyle(nfoStyle);
 			execute(SCI_STYLECLEARALL);
 
-			Buffer * buf = MainFileManager.getBufferByID(_currentBufferID);
+			Buffer* buf = MainFileManager.getBufferByID(_currentBufferID);
 
-			if (buf->getEncoding() != NPP_CP_DOS_437)
+			if (buf->getEncoding() == NPP_CP_DOS_437)
 			{
-			   buf->setEncoding(NPP_CP_DOS_437);
-			   ::SendMessage(_hParent, WM_COMMAND, IDM_FILE_RELOAD, 0);
+				MainFileManager.reloadBuffer(buf);
 			}
 		}
 		break;

PowerEditor/src/ScintillaComponent/Buffer.cpp
@@ -816,7 +816,10 @@ BufferID FileManager::loadFile(const wchar_t* filename, Document doc, int encodi
 			newBuf->_timeStamp = fileNameTimestamp;
 
 		// restore the encoding (ANSI based) while opening the existing file
-		newBuf->setEncoding(-1);
+		if (newBuf->_lang == L_ASCII)
+			newBuf->setEncoding(NPP_CP_DOS_437);
+		else
+			newBuf->setEncoding(-1);
 
 		// if not a large file, no file extension, and the language has been detected,  we use the detected value
 		if (!newBuf->_isLargeFile && ((newBuf->getLangType() == L_TEXT) && (loadedFileFormat._language != L_TEXT)))

commit d39d7faee1825a7b4a389247309033dca7aa6d76

Fix nfo file losing modification issue (regression)
The regression is introduced by:
https://github.com/notepad-plus-plus/notepad-plus-plus/commit/dd0b557e145fc664e2c2c10bda2227b1b46e1921#diff-d88ddee57a027ab23daf332c4778ced0cee352edcb34efdda1b218e8a75c61b2R1864

Fix #15964

PowerEditor/src/ScintillaComponent/ScintillaEditView.cpp
@@ -1861,7 +1861,7 @@ void ScintillaEditView::defineDocType(LangType typeDoc)
 
 			Buffer* buf = MainFileManager.getBufferByID(_currentBufferID);
 
-			if (buf->getEncoding() == NPP_CP_DOS_437)
+			if (buf->getEncoding() == NPP_CP_DOS_437 && !buf->isDirty())
 			{
 				MainFileManager.reloadBuffer(buf);
 			}
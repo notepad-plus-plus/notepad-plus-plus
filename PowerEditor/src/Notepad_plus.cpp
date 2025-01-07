notepad-plus-plus/notepad-plus-plus

commit 71be434a334d300487c5d23114dc4afd084b94aa

Improve performance while disabling clickable URL link
Ref: https://github.com/notepad-plus-plus/notepad-plus-plus/pull/15981#issuecomment-2570310216

Close #16021

PowerEditor/src/Notepad_plus.cpp
@@ -3444,6 +3444,7 @@ void Notepad_plus::addHotSpot(ScintillaEditView* view)
 {
 	if (_isAttemptingCloseOnQuit)
 		return; // don't recalculate URLs when shutting down
+
 	ScintillaEditView* pView = view ? view : _pEditView;
 	Buffer* currentBuf = pView->getCurrentBuffer();
 
@@ -3466,6 +3467,7 @@ void Notepad_plus::addHotSpot(ScintillaEditView* view)
 	pView->getVisibleStartAndEndPosition(&startPos, &endPos);
 	if (startPos >= endPos) return;
 	pView->execute(SCI_SETINDICATORCURRENT, URL_INDIC);
+
 	if (urlAction == urlDisable || !currentBuf->allowClickableLink())
 	{
 		pView->execute(SCI_INDICATORCLEARRANGE, startPos, endPos - startPos);

PowerEditor/src/NppBigSwitch.cpp
@@ -2276,11 +2276,19 @@ LRESULT Notepad_plus::process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPa
 			//reset styler for change in Stylers.xml
 			_mainEditView.defineDocType(_mainEditView.getCurrentBuffer()->getLangType());
 			_mainEditView.performGlobalStyles();
-			addHotSpot(& _mainEditView);
 
 			_subEditView.defineDocType(_subEditView.getCurrentBuffer()->getLangType());
 			_subEditView.performGlobalStyles();
-			addHotSpot(& _subEditView);
+
+			int urlAction = nppParam.getNppGUI()._styleURL;
+			if (urlAction != urlDisable)
+			{
+				if (_mainEditView.getCurrentBuffer()->allowClickableLink())
+					addHotSpot(&_mainEditView);
+
+				if (_subEditView.getCurrentBuffer()->allowClickableLink())
+					addHotSpot(&_subEditView);
+			}
 
 			_findReplaceDlg.updateFinderScintilla();
 			
@@ -3692,14 +3700,23 @@ LRESULT Notepad_plus::process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPa
 		case NPPM_INTERNAL_UPDATECLICKABLELINKS:
 		{
 			ScintillaEditView* pView = reinterpret_cast<ScintillaEditView*>(wParam);
-			if (pView == NULL)
+
+			int urlAction = nppParam.getNppGUI()._styleURL;
+			if (urlAction != urlDisable)
 			{
-				addHotSpot(_pEditView);
-				addHotSpot(_pNonEditView);
-			}
-			else
-			{
-				addHotSpot(pView);
+				if (pView == NULL)
+				{
+					if (_pEditView->getCurrentBuffer()->allowClickableLink())
+						addHotSpot(_pEditView);
+
+					if (_pNonEditView->getCurrentBuffer()->allowClickableLink())
+						addHotSpot(_pNonEditView);
+				}
+				else
+				{
+					if (pView->getCurrentBuffer()->allowClickableLink())
+						addHotSpot(pView);
+				}
 			}
 			return TRUE;
 		}
@@ -3734,8 +3751,16 @@ LRESULT Notepad_plus::process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPa
 
 		case NPPM_INTERNAL_DOCMODIFIEDBYREPLACEALL:
 		{
-			if (wParam == reinterpret_cast<WPARAM>(_pEditView->getCurrentBuffer()))
-				addHotSpot(_pEditView);
+			Buffer* currentBuf = _pEditView->getCurrentBuffer();
+			if (wParam == reinterpret_cast<WPARAM>(currentBuf))
+			{
+				int urlAction = nppParam.getNppGUI()._styleURL;
+				if (urlAction != urlDisable && currentBuf->allowClickableLink())
+				{
+					addHotSpot(_pEditView);
+				}
+			}
+
 			SCNotification scnN{};
 			scnN.nmhdr.code = NPPN_GLOBALMODIFIED;
 			scnN.nmhdr.hwndFrom = reinterpret_cast<void*>(wParam);

PowerEditor/src/NppNotification.cpp
@@ -188,7 +188,12 @@ BOOL Notepad_plus::notify(SCNotification *notification)
 
 				if (!_isFolding)
 				{
-					addHotSpot();
+					int urlAction = (NppParameters::getInstance()).getNppGUI()._styleURL;
+					Buffer* currentBuf = _pEditView->getCurrentBuffer();
+					if (urlAction != urlDisable && currentBuf->allowClickableLink())
+					{
+						addHotSpot();
+					}
 				}
 
 				if (_pDocMap)
@@ -405,17 +410,22 @@ BOOL Notepad_plus::notify(SCNotification *notification)
 			NppParameters& nppParam = NppParameters::getInstance();
 			NppGUI& nppGui = nppParam.getNppGUI();
 
+			Buffer* currentBuf = notifyView->getCurrentBuffer();
+			
 			// replacement for obsolete custom SCN_SCROLLED
 			if (notification->updated & SC_UPDATE_V_SCROLL)
 			{
-				addHotSpot(notifyView);
+				int urlAction = (NppParameters::getInstance()).getNppGUI()._styleURL;
+				if (urlAction != urlDisable && currentBuf->allowClickableLink())
+				{
+					addHotSpot(notifyView);
+				}
 			}
 
 			// if it's searching/replacing, then do nothing
 			if (nppParam._isFindReplacing)
 				break;
 
-			Buffer* currentBuf = _pEditView->getCurrentBuffer();
 			if (notification->nmhdr.hwndFrom != _pEditView->getHSelf() && currentBuf->allowSmartHilite()) // notification come from unfocus view - both views ae visible
 			{
 				if (nppGui._smartHiliteOnAnotherView)
@@ -516,7 +526,12 @@ BOOL Notepad_plus::notify(SCNotification *notification)
 			// if it's searching/replacing, then do nothing
 			if ((_linkTriggered && !nppParam._isFindReplacing) || notification->wParam == LINKTRIGGERED)
 			{
-				addHotSpot();
+				int urlAction = (NppParameters::getInstance()).getNppGUI()._styleURL;
+				Buffer* currentBuf = _pEditView->getCurrentBuffer();
+				if (urlAction != urlDisable && currentBuf->allowClickableLink())
+				{
+					addHotSpot();
+				}
 				_linkTriggered = false;
 			}
 

commit 5a5b996edae393771a42a43b9087d84824ddef79

Fix regressionn while disabling clickable link
The regression was introduced by commit 71be434a334d300487c5d23114dc4afd084b94aa

Ref: https://github.com/notepad-plus-plus/notepad-plus-plus/pull/15926#issuecomment-2572268207

PowerEditor/src/Notepad_plus.cpp
@@ -3440,6 +3440,24 @@ bool isUrl(wchar_t * text, int textLen, int start, int* segmentLen)
 	return false;
 }
 
+void Notepad_plus::removeAllHotSpot()
+{
+	DocTabView* twoDocView[] { &_mainDocTab, &_subDocTab };
+	for (DocTabView* pDocView : twoDocView)
+	{
+		for (size_t i = 0; i < pDocView->nbItem(); ++i)
+		{
+			BufferID id = pDocView->getBufferByIndex(i);
+			Buffer* buf = MainFileManager.getBufferByID(id);
+
+			if (buf->allowClickableLink()) // if it's not allowed clickabled link in the buffer, there's nothing to be cleared
+			{
+				MainFileManager.removeHotSpot(buf);
+			}
+		}
+	}
+}
+
 void Notepad_plus::addHotSpot(ScintillaEditView* view)
 {
 	if (_isAttemptingCloseOnQuit)

PowerEditor/src/Notepad_plus.h
@@ -514,6 +514,7 @@ private:
 	void maintainIndentation(wchar_t ch);
 
 	void addHotSpot(ScintillaEditView* view = nullptr);
+	void removeAllHotSpot();
 
     void bookmarkAdd(intptr_t lineno) const {
 		if (lineno == -1)

PowerEditor/src/NppBigSwitch.cpp
@@ -3699,6 +3699,12 @@ LRESULT Notepad_plus::process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPa
 
 		case NPPM_INTERNAL_UPDATECLICKABLELINKS:
 		{
+			if (wParam == 1)
+			{
+				removeAllHotSpot();
+				return TRUE;
+			}
+
 			ScintillaEditView* pView = reinterpret_cast<ScintillaEditView*>(wParam);
 
 			int urlAction = nppParam.getNppGUI()._styleURL;

PowerEditor/src/ScintillaComponent/Buffer.cpp
@@ -2007,3 +2007,16 @@ size_t FileManager::docLength(Buffer* buffer) const
 	_pscratchTilla->execute(SCI_SETDOCPOINTER, 0, curDoc);
 	return docLen;
 }
+
+
+void FileManager::removeHotSpot(Buffer* buffer) const
+{
+	Document curDoc = _pscratchTilla->execute(SCI_GETDOCPOINTER);
+	_pscratchTilla->execute(SCI_SETDOCPOINTER, 0, buffer->_doc);
+
+	_pscratchTilla->execute(SCI_SETINDICATORCURRENT, URL_INDIC);
+	size_t docLen = _pscratchTilla->getCurrentDocLen();
+	_pscratchTilla->execute(SCI_INDICATORCLEARRANGE, 0, docLen);
+
+	_pscratchTilla->execute(SCI_SETDOCPOINTER, 0, curDoc);
+}

PowerEditor/src/ScintillaComponent/Buffer.h
@@ -113,6 +113,7 @@ public:
 	};
 	int getFileNameFromBuffer(BufferID id, wchar_t * fn2copy);
 	size_t docLength(Buffer * buffer) const;
+	void removeHotSpot(Buffer * buffer) const;
 	size_t nextUntitledNewNumber() const;
 
 private:

PowerEditor/src/WinControls/Preference/preferenceDlg.cpp
@@ -5799,7 +5799,7 @@ intptr_t CALLBACK CloudAndLinkSubDlg::run_dlgProc(UINT message, WPARAM wParam, L
 
 					nppGUI._styleURL = isChecked ? urlUnderLineFg : urlDisable;
 					HWND grandParent = ::GetParent(_hParent);
-					::SendMessage(grandParent, NPPM_INTERNAL_UPDATECLICKABLELINKS, 0, 0);
+					::SendMessage(grandParent, NPPM_INTERNAL_UPDATECLICKABLELINKS, isChecked ? 0 : 1, 0);
 				}
 				return TRUE;
 
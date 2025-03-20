1ae25cb50fb720c46ca006f60b984ad5d179bda9
xomx <xomx@users.noreply.github.com>
2025-03-20 17:24:52 +0100

    final test

--- PowerEditor/src/NppLibsVersion.h
+++ PowerEditor/src/NppLibsVersion.h
@@ -1,5 +1,5 @@
 // NppLibsVersion.h
 // - maintained by NppLibsVersionH-generator.bat
-#define NPP_SCINTILLA_VERSION "5.5.4"
-#define NPP_LEXILLA_VERSION "5.4.2"
-#define NPP_BOOST_REGEX_VERSION "1_666"
+#define NPP_SCINTILLA_VERSION "1.1.1"
+#define NPP_LEXILLA_VERSION "1.1.2"
+#define NPP_BOOST_REGEX_VERSION "1_13"

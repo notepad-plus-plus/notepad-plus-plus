4bc0d7f74b285483cca68fcc3e16e60042acc903
Don Ho <don.h@free.fr>
2025-04-28 17:25:18 +0200

    Notepad++ release 8.8

--- PowerEditor/src/resource.h
+++ PowerEditor/src/resource.h
@@ -19,14 +19,14 @@
 
 //************ Notepad++ version **************************
 
-#define NOTEPAD_PLUS_VERSION L"Notepad++ v8.7.9"
+#define NOTEPAD_PLUS_VERSION L"Notepad++ v8.8"
 
 // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71
 // ex : #define VERSION_VALUE L"5.63\0"
-#define VERSION_INTERNAL_VALUE L"8.79\0"
+#define VERSION_INTERNAL_VALUE L"8.8\0"
 
-#define VERSION_PRODUCT_VALUE L"8.7.9\0"
-#define VERSION_DIGITALVALUE 8, 7, 9, 0
+#define VERSION_PRODUCT_VALUE L"8.8\0"
+#define VERSION_DIGITALVALUE 8, 8, 0, 0
 
 //**********************************************************
 

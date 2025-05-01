7b42308b882603f50d3500617712c469b6356bbc
Don Ho <don.h@free.fr>
2025-05-01 03:36:27 +0200

    Fix controls' jumping problem & magic number

--- PowerEditor/src/ScintillaComponent/FindReplaceDlg.cpp
+++ PowerEditor/src/ScintillaComponent/FindReplaceDlg.cpp
@@ -1304,8 +1304,7 @@ void FindReplaceDlg::resizeDialogElements()
 	RECT rcSwapBtn{};
 	HWND hSwapBtn = ::GetDlgItem(_hSelf, IDD_FINDREPLACE_SWAP_BUTTON);
 	getMappedChildRect(hSwapBtn, rcSwapBtn);
-	const int moreRoom = 60;
-	hdwp = setOrDeferWindowPos(hdwp, hSwapBtn, nullptr, rcOkBtn.left - getRcWidth(rcSwapBtn) - gap - moreRoom, rcSwapBtn.top, 0, 0, SWP_NOSIZE | flags);
+	hdwp = setOrDeferWindowPos(hdwp, hSwapBtn, nullptr, rcOkBtn.left - getRcWidth(rcSwapBtn) - gap * 6, rcSwapBtn.top, 0, 0, SWP_NOSIZE | flags);
 
 	if (hdwp)
 		::EndDeferWindowPos(hdwp);

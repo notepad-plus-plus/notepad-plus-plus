0e249b1bb9f16a4746a428f19c9b0037dafe38e5
Don Ho <don.h@free.fr>
2025-05-14 18:51:34 +0200

    Sync plugin header (part 4)

--- PowerEditor/src/WinControls/DockingWnd/dockingResource.h
+++ PowerEditor/src/WinControls/DockingWnd/dockingResource.h
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+// Contribution by Don HO <don.h@free.fr>
+// Date of Contribution: 2025-05-14
+// Description of Contribution: Minor change
 
 #pragma once
 
@@ -32,36 +35,27 @@
 #define IDC_BTN_CAPTION                 1050
 
 #define DMM_MSG                         0x5000
-    #define DMM_CLOSE                   (DMM_MSG + 1)
-    #define DMM_DOCK                    (DMM_MSG + 2)
-    #define DMM_FLOAT                   (DMM_MSG + 3)
-    #define DMM_DOCKALL                 (DMM_MSG + 4)
-    #define DMM_FLOATALL                (DMM_MSG + 5)
-    #define DMM_MOVE                    (DMM_MSG + 6)
-    #define DMM_UPDATEDISPINFO          (DMM_MSG + 7)
-    //#define DMM_GETIMAGELIST            (DMM_MSG + 8)
-    //#define DMM_GETICONPOS              (DMM_MSG + 9)
-    #define DMM_DROPDATA				(DMM_MSG + 10)
-    #define DMM_MOVE_SPLITTER		    (DMM_MSG + 11)
-	#define DMM_CANCEL_MOVE				(DMM_MSG + 12)
-	#define DMM_LBUTTONUP				(DMM_MSG + 13)
+	#define DMM_CLOSE                   (DMM_MSG + 1)
+	#define DMM_DOCK                    (DMM_MSG + 2)
+	#define DMM_FLOAT                   (DMM_MSG + 3)
+	#define DMM_DOCKALL                 (DMM_MSG + 4)
+	#define DMM_FLOATALL                (DMM_MSG + 5)
+	#define DMM_MOVE                    (DMM_MSG + 6)
+	#define DMM_UPDATEDISPINFO          (DMM_MSG + 7)
+	//#define DMM_GETIMAGELIST            (DMM_MSG + 8)
+	//#define DMM_GETICONPOS              (DMM_MSG + 9)
+	#define DMM_DROPDATA                (DMM_MSG + 10)
+	#define DMM_MOVE_SPLITTER           (DMM_MSG + 11)
+	#define DMM_CANCEL_MOVE             (DMM_MSG + 12)
+	#define DMM_LBUTTONUP               (DMM_MSG + 13)
 
 #define DMN_FIRST 1050
-	#define DMN_CLOSE					(DMN_FIRST + 1)
-	//nmhdr.code = DWORD(DMN_CLOSE, 0));
-	//nmhdr.hwndFrom = hwndNpp;
-	//nmhdr.idFrom = ctrlIdNpp;
-
-	#define DMN_DOCK		            (DMN_FIRST + 2)
-    #define DMN_FLOAT					(DMN_FIRST + 3)
-	//nmhdr.code = DWORD(DMN_XXX, int newContainer);
-	//nmhdr.hwndFrom = hwndNpp;
-	//nmhdr.idFrom = ctrlIdNpp;
-
-	#define DMN_SWITCHIN	            (DMN_FIRST + 4)
-	#define DMN_SWITCHOFF	            (DMN_FIRST + 5)
-	#define DMN_FLOATDROPPED			(DMN_FIRST + 6)
+	#define DMN_CLOSE                   (DMN_FIRST + 1)
+	#define DMN_DOCK                    (DMN_FIRST + 2)
+	#define DMN_FLOAT                   (DMN_FIRST + 3)
+	#define DMN_SWITCHIN                (DMN_FIRST + 4)
+	#define DMN_SWITCHOFF               (DMN_FIRST + 5)
+	#define DMN_FLOATDROPPED            (DMN_FIRST + 6)
 	//nmhdr.code = DWORD(DMN_XXX, 0);
 	//nmhdr.hwndFrom = DockingCont::_hself;
 	//nmhdr.idFrom = 0;
-

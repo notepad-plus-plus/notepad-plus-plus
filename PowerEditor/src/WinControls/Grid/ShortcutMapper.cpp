bc55ab32497255202fb37076a6c7185305798a22
Niranjen P <niranjen.p@gmail.com>
2025-07-02 17:03:32 +0930

    Reverted encoding of ShortcutMapper.cpp (accidentally changed) , UTF -8 BOM encoding is needed for shortcutmapper.rc to display the requested character

--- PowerEditor/src/WinControls/Grid/ShortcutMapper.cpp
+++ PowerEditor/src/WinControls/Grid/ShortcutMapper.cpp
@@ -1,4 +1,4 @@
-﻿// This file is part of Notepad++ project
+// This file is part of Notepad++ project
 // Copyright (C)2021 Don HO <don.h@free.fr>
 
 // This program is free software: you can redistribute it and/or modify

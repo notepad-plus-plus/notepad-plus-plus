593a0dbfac4ae9c707b8af7986fd9091abaab6fa
Don Ho <don.h@free.fr>
2025-09-24 19:58:28 +0200

    Fix NPPN_CMDLINEPLUGINMSG not working issue
    Fix #17024, fix #17022

--- PowerEditor/src/Parameters.h
+++ PowerEditor/src/Parameters.h
@@ -324,7 +324,7 @@ struct CmdLineParamsDTO
 
 	LangType _langType = L_EXTERNAL;
 	wchar_t _udlName[MAX_PATH] = {'\0'};
-	wchar_t _pluginMessage[MAX_PATH] = {'\0'};
+	wchar_t _pluginMessage[MAX_PATH * 2] = {'\0'};
 
 	static CmdLineParamsDTO FromCmdLineParams(const CmdLineParams& params)
 	{
@@ -342,7 +342,7 @@ struct CmdLineParamsDTO
 
 		dto._langType = params._langType;
 		wcsncpy(dto._udlName, params._udlName.c_str(), MAX_PATH);
-		wcsncpy(dto._pluginMessage, params._pluginMessage.c_str(), MAX_PATH);
+		wcsncpy(dto._pluginMessage, params._pluginMessage.c_str(), MAX_PATH * 2);
 		return dto;
 	}
 };

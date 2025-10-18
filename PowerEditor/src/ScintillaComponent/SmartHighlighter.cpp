ad444433be375fb1c738d5ef9e910bd0e5079fd2
Ekopalypse <47723516+Ekopalypse@users.noreply.github.com>
2025-10-18 19:10:33 +0200

    Implement null check for selected text in highlighting
    Added null check for text2FindW to prevent potential crashes

--- PowerEditor/src/ScintillaComponent/SmartHighlighter.cpp
+++ PowerEditor/src/ScintillaComponent/SmartHighlighter.cpp
@@ -153,6 +153,10 @@ void SmartHighlighter::highlightView(ScintillaEditView * pHighlightView, Scintil
 	
 	const wchar_t * text2FindW = pHighlightView->getSelectedTextToWChar(false); //do not expand selection (false)
 
+	if (text2FindW == nullptr) {
+		return;
+	}
+	
 	highlightViewWithWord(pHighlightView, text2FindW);
 
 	if (nppGUI._smartHiliteOnAnotherView && unfocusView && unfocusView->isVisible())
@@ -167,3 +171,4 @@ void SmartHighlighter::highlightView(ScintillaEditView * pHighlightView, Scintil
 		highlightViewWithWord(unfocusView, text2FindW);
 	}
 }
+

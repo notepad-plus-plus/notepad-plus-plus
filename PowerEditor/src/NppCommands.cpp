notepad-plus-plus/notepad-plus-plus

commit be59048c5e73f497526beb8a482fdb03d260525e

Fix filling Find what length not accurate problem
+ a lot of refactoring

Ref: https://github.com/notepad-plus-plus/notepad-plus-plus/issues/16955#issuecomment-3287770729

Fix #16955, fix #16992, close #17001

PowerEditor/src/NppCommands.cpp
@@ -439,7 +439,7 @@ void Notepad_plus::command(int id)
 			auto pBinText = std::make_unique<char[]>(strSize);
 			std::fill_n(pBinText.get(), strSize, '\0');
 
-			_pEditView->getSelectedText(pBinText.get(), textLen + 1);
+			_pEditView->getSelectedTextToMultiChar(pBinText.get(), textLen + 1);
 
 			// Open the clipboard and empty it.
 			if (!::OpenClipboard(NULL))
@@ -1356,16 +1356,6 @@ void Notepad_plus::command(int id)
 		case IDM_SEARCH_REPLACE :
 		case IDM_SEARCH_MARK :
 		{
-			const int strSize = FINDREPLACE_MAXLENGTH;
-			auto str = std::make_unique<wchar_t[]>(strSize);
-			std::fill_n(str.get(), strSize, L'\0');
-
-			const NppGUI& nppGui = (NppParameters::getInstance()).getNppGUI();
-			if (nppGui._fillFindFieldWithSelected)
-			{
-				_pEditView->getGenericSelectedText(str.get(), strSize, nppGui._fillFindFieldSelectCaret);
-			}
-
 			bool isFirstTime = !_findReplaceDlg.isCreated();
 
 			DIALOG_TYPE dlgID = FIND_DLG;
@@ -1375,14 +1365,7 @@ void Notepad_plus::command(int id)
 				dlgID = MARK_DLG;
 			_findReplaceDlg.doDialog(dlgID, _nativeLangSpeaker.isRTL());
 
-			if (nppGui._fillFindFieldWithSelected)
-			{
-				int selLen = lstrlen(str.get());
-				if (selLen > 0 && selLen <= nppGui._fillFindWhatThreshold)
-				{
-					_findReplaceDlg.setSearchText(str.get());
-				}
-			}
+			_findReplaceDlg.setSearchTextWithSettings();
 
 			setFindReplaceFolderFilter(NULL, NULL);
 
@@ -1399,10 +1382,6 @@ void Notepad_plus::command(int id)
 
 		case IDM_SEARCH_FINDINCREMENT :
 		{
-			const int strSize = FINDREPLACE_MAXLENGTH;
-			auto str = std::make_unique<wchar_t[]>(strSize);
-			std::fill_n(str.get(), strSize, L'\0');
-
 			static bool isFirstTime = true;
 			if (isFirstTime)
 			{
@@ -1410,9 +1389,9 @@ void Notepad_plus::command(int id)
 				isFirstTime = false;
 			}
 
-			_pEditView->getGenericSelectedText(str.get(), strSize, false);
-			if (0 != str[0])         // the selected text is not empty, then use it
-				_incrementFindDlg.setSearchText(str.get(), _pEditView->getCurrentBuffer()->getUnicodeMode() != uni8Bit);
+			auto str = _pEditView->getSelectedTextToWChar(false);
+			if (str)         // the selected text is not empty, then use it
+				_incrementFindDlg.setSearchText(str, _pEditView->getCurrentBuffer()->getUnicodeMode() != uni8Bit);
 
 			_incrementFindDlg.display();
 		}
@@ -1460,13 +1439,11 @@ void Notepad_plus::command(int id)
 			if (isFirstTime)
 				_findReplaceDlg.doDialog(FIND_DLG, _nativeLangSpeaker.isRTL(), false);
 
-			const int strSize = FINDREPLACE_MAXLENGTH;
-			auto str = std::make_unique<wchar_t[]>(strSize);
-			std::fill_n(str.get(), strSize, L'\0');
+			const wchar_t* str = _findReplaceDlg.setSearchTextWithSettings();
+			if (!str) return;
 
-			_pEditView->getGenericSelectedText(str.get(), strSize);
-			_findReplaceDlg.setSearchText(str.get());
-			_findReplaceDlg._env->_str2Search = str.get();
+			_findReplaceDlg._env->_str2Search = str;
+
 			setFindReplaceFolderFilter(NULL, NULL);
 			if (isFirstTime)
 				_nativeLangSpeaker.changeFindReplaceDlgLang(_findReplaceDlg);
@@ -1476,7 +1453,7 @@ void Notepad_plus::command(int id)
 			op._whichDirection = (id == IDM_SEARCH_SETANDFINDNEXT?DIR_DOWN:DIR_UP);
 
 			FindStatus status = FSNoMessage;
-			_findReplaceDlg.processFindNext(str.get(), &op, &status);
+			_findReplaceDlg.processFindNext(str, &op, &status);
 			if (status == FSEndReached)
 			{
 				wstring msg = _nativeLangSpeaker.getLocalizedStrFromID("find-status-end-reached", FIND_STATUS_END_REACHED_TEXT);
@@ -1515,10 +1492,8 @@ void Notepad_plus::command(int id)
 		case IDM_SEARCH_VOLATILE_FINDNEXT :
 		case IDM_SEARCH_VOLATILE_FINDPREV :
 		{
-			const int strSize = FINDREPLACE_MAXLENGTH;
-			auto str = std::make_unique<wchar_t[]>(strSize);
-			std::fill_n(str.get(), strSize, L'\0');
-			_pEditView->getGenericSelectedText(str.get(), strSize);
+			auto str = _pEditView->getSelectedTextToWChar();
+			if (!str) return;
 
 			FindOption op;
 			op._isMatchCase = false;
@@ -1528,7 +1503,7 @@ void Notepad_plus::command(int id)
 			op._whichDirection = (id == IDM_SEARCH_VOLATILE_FINDNEXT ? DIR_DOWN : DIR_UP);
 
 			FindStatus status = FSNoMessage;
-			_findReplaceDlg.processFindNext(str.get(), &op, &status);
+			_findReplaceDlg.processFindNext(str, &op, &status);
 			if (status == FSEndReached)
 			{
 				wstring msg = _nativeLangSpeaker.getLocalizedStrFromID("find-status-end-reached", FIND_STATUS_END_REACHED_TEXT);
@@ -1560,26 +1535,11 @@ void Notepad_plus::command(int id)
 			else // (id == IDM_SEARCH_MARKALLEXT5)
 				styleID = SCE_UNIVERSAL_FOUND_STYLE_EXT5;
 
-			const int strSize = FINDREPLACE_MAXLENGTH;
-			auto selectedText = std::make_unique<wchar_t[]>(strSize);
-			std::fill_n(selectedText.get(), strSize, L'\0');
+			auto selectedText = _pEditView->getSelectedTextToWChar(true);
 
-			auto wordOnCaret = std::make_unique<wchar_t[]>(strSize);
-			std::fill_n(wordOnCaret.get(), strSize, L'\0');
-
-			_pEditView->getGenericSelectedText(selectedText.get(), strSize, false);
-			_pEditView->getGenericWordOnCaretPos(wordOnCaret.get(), strSize);
-
-			if (selectedText[0] == '\0')
+			if (selectedText)
 			{
-				if (lstrlen(wordOnCaret.get()) > 0)
-				{
-					_findReplaceDlg.markAll(wordOnCaret.get(), styleID);
-				}
-			}
-			else
-			{
-				_findReplaceDlg.markAll(selectedText.get(), styleID);
+				_findReplaceDlg.markAll(selectedText, styleID);
 			}
 		}
 		break;
@@ -3506,20 +3466,16 @@ void Notepad_plus::command(int id)
         case IDM_ABOUT:
 		{
 			bool doAboutDlg = false;
-			const int maxSelLen = 64;
 			auto textLen = _pEditView->execute(SCI_GETSELTEXT, 0, 0);
 			if (textLen <= 0)
-				doAboutDlg = true;
-			if (textLen > maxSelLen)
 				doAboutDlg = true;
 
 			if (!doAboutDlg)
 			{
-				char author[maxSelLen+1] = "";
-				_pEditView->getSelectedText(author, maxSelLen + 1);
-				WcharMbcsConvertor& wmc = WcharMbcsConvertor::getInstance();
-				const wchar_t * authorW = wmc.char2wchar(author, _nativeLangSpeaker.getLangEncoding());
-				int iQuote = getQuoteIndexFrom(authorW);
+				int iQuote = -1;
+				auto authorW = _pEditView->getSelectedTextToWChar();
+				if (authorW)
+					iQuote = getQuoteIndexFrom(authorW);
 
 				if (iQuote == -1)
 				{

PowerEditor/src/ScintillaComponent/FindReplaceDlg.cpp
@@ -1579,8 +1579,8 @@ intptr_t CALLBACK FindReplaceDlg::run_dlgProc(UINT message, WPARAM wParam, LPARA
 			HWND hFindCombo = ::GetDlgItem(_hSelf, IDFINDWHAT);
 			HWND hReplaceCombo = ::GetDlgItem(_hSelf, IDREPLACEWITH);
 
-			::SendMessage(hFindCombo, CB_LIMITTEXT, FINDREPLACE_MAXLENGTH - 1, 0);
-			::SendMessage(hReplaceCombo, CB_LIMITTEXT, FINDREPLACE_MAXLENGTH - 1, 0);
+			::SendMessage(hFindCombo, CB_LIMITTEXT, FINDREPLACE_MAXLENGTH * 2 - 1, 0);
+			::SendMessage(hReplaceCombo, CB_LIMITTEXT, FINDREPLACE_MAXLENGTH * 2 - 1, 0);
 
 			HWND hFiltersCombo = ::GetDlgItem(_hSelf, IDD_FINDINFILES_FILTERS_COMBO);
 			HWND hDirCombo = ::GetDlgItem(_hSelf, IDD_FINDINFILES_DIR_COMBO);
@@ -1971,17 +1971,41 @@ intptr_t CALLBACK FindReplaceDlg::run_dlgProc(UINT message, WPARAM wParam, LPARA
 				{
 					if (HIWORD(wParam) == CBN_EDITUPDATE)
 					{
-						HWND hComboBox = ::GetDlgItem(_hSelf, LOWORD(wParam));	
-						LRESULT length = ::GetWindowTextLength(hComboBox);
+						HWND hComboBox = ::GetDlgItem(_hSelf, LOWORD(wParam));
+						HWND hEdit = GetWindow(hComboBox, GW_CHILD);
+						if (!hEdit)
+							return FALSE;
 
+						static int maxLength = (int)SendMessage(hEdit, EM_GETLIMITTEXT, 0, 0);
+						LRESULT length = ::GetWindowTextLength(hEdit);
 
-						if (length >= FINDREPLACE_MAXLENGTH - 1)
+						if (length >= maxLength) // should be (FINDREPLACE_MAXLENGTH * 2 - 1)
 						{
 							if (!_maxLenOnSearchTip.isValid()) // Create the tooltip and add the tool ONLY ONCE
 							{
 								NativeLangSpeaker* pNativeSpeaker = nppParamInst.getNativeLangSpeaker();
-								wstring tip = pNativeSpeaker->getLocalizedStrFromID("max-len-on-search-tip", L"Only $INT_REPLACE$ characters are allowed for the find/replace text length - your input could be truncated, and it won't be saved for the next session.");
-								tip = stringReplace(tip, L"$INT_REPLACE$", std::to_wstring(FINDREPLACE_MAXLENGTH - 1));
+								wstring tip = pNativeSpeaker->getLocalizedStrFromID("len-limit-exceeded-tip", L"Length limit exceeded: Your input may exceed the limit allowed and could have been truncated, and it won't be saved for the next session.");
+
+								static wstring maxLenOnSearchTip = tip;
+
+								bool isSuccessful = _maxLenOnSearchTip.init(_hInst, hComboBox, _hSelf, maxLenOnSearchTip.c_str(), _isRTL, 0, 170);
+
+								if (!isSuccessful)
+								{
+									return FALSE;
+								}
+
+								NppDarkMode::setDarkTooltips(_maxLenOnSearchTip.getTipHandle(), NppDarkMode::ToolTipsType::tooltip);
+							}
+							_maxLenOnSearchTip.show(ControlInfoTip::showPosition::beginning);
+							
+						}
+						else if (length > FINDREPLACE_MAXLENGTH - 1)
+						{
+							if (!_maxLenOnSearchTip.isValid()) // Create the tooltip and add the tool ONLY ONCE
+							{
+								NativeLangSpeaker* pNativeSpeaker = nppParamInst.getNativeLangSpeaker();
+								wstring tip = pNativeSpeaker->getLocalizedStrFromID("max-len-on-search-tip", L"Your input may exceed the limit allowed and could have been truncated, and it won't be saved for the next session.");
 
 								static wstring maxLenOnSearchTip = tip;
 
@@ -2001,8 +2025,7 @@ intptr_t CALLBACK FindReplaceDlg::run_dlgProc(UINT message, WPARAM wParam, LPARA
 							if (!_maxLenOnSearchTip.isValid()) // Create the tooltip and add the tool ONLY ONCE
 							{
 								NativeLangSpeaker* pNativeSpeaker = nppParamInst.getNativeLangSpeaker();
-								wstring tip = pNativeSpeaker->getLocalizedStrFromID("max-len-on-save-tip", L"This search input (> $INT_REPLACE$ characters) won't be saved for the next session");
-								tip = stringReplace(tip, L"$INT_REPLACE$", std::to_wstring(FINDREPLACE_MAXLENGTH2SAVE - 1));
+								wstring tip = pNativeSpeaker->getLocalizedStrFromID("max-len-on-save-tip", L"The length of your input is very long and may not be saved for your next session.");
 
 								static wstring maxLenOnSaveTip = tip;
 
@@ -4044,7 +4067,7 @@ int FindReplaceDlg::regexBackwardMsgBox()
 	return msgboxID;
 }
 
-void FindReplaceDlg::setSearchText(wchar_t * txt2find)
+void FindReplaceDlg::setSearchText(const wchar_t * txt2find)
 {
 	HWND hCombo = ::GetDlgItem(_hSelf, IDFINDWHAT);
 	if (txt2find && txt2find[0])
@@ -5357,7 +5380,7 @@ bool FindReplaceDlg::replaceInProjectsConfirmCheck()
 	return confirmed;
 }
 
-bool FindReplaceDlg::replaceInOpenDocsConfirmCheck(void)
+bool FindReplaceDlg::replaceInOpenDocsConfirmCheck()
 {
 	bool confirmed = false;
 
@@ -5373,6 +5396,26 @@ bool FindReplaceDlg::replaceInOpenDocsConfirmCheck(void)
 	}
 
 	return confirmed;
+}
+
+// return NULL if nothing to set in find field.
+// Otherwise return string pointer (wchar_t *) in which the selected text was copied.
+// Note that the string pointer don't need to and should not be deallocated.
+const wchar_t* FindReplaceDlg::setSearchTextWithSettings()
+{
+	const NppGUI& nppGui = NppParameters::getInstance().getNppGUI();
+	if (nppGui._fillFindFieldWithSelected)
+	{
+		Sci_Position selStrCharNum = 0;
+		const wchar_t* selStr = (*_ppEditView)->getSelectedTextToWChar(nppGui._fillFindFieldSelectCaret, &selStrCharNum);
+
+		if (selStr && selStrCharNum <= nppGui._fillFindWhatThreshold)
+		{
+			setSearchText(selStr);
+			return selStr;
+		}
+	}
+	return nullptr;
 }
 
 wstring Finder::getHitsString(int count) const
@@ -6284,12 +6327,11 @@ void FindIncrementDlg::markSelectedTextInc(bool enable, FindOption *opt)
 	if (range.cpMin == range.cpMax)
 		return;
 
-	const int strSize = FINDREPLACE_MAXLENGTH;
-	auto text2Find = std::make_unique<wchar_t[]>(strSize);
-	std::fill_n(text2Find.get(), strSize, L'\0');
+	auto text2Find = (*(_pFRDlg->_ppEditView))->getSelectedTextToWChar(false);	//do not expand selection (false)
+	if (!text2Find)
+		return;
 
-	(*(_pFRDlg->_ppEditView))->getGenericSelectedText(text2Find.get(), FINDREPLACE_MAXLENGTH, false);	//do not expand selection (false)
-	opt->_str2Search = text2Find.get();
+	opt->_str2Search = text2Find;
 	_pFRDlg->markAllInc(opt);
 }
 

PowerEditor/src/ScintillaComponent/FindReplaceDlg.h
@@ -287,7 +287,7 @@ public :
 
 	void replaceAllInOpenedDocs();
 	void findAllIn(InWhat op);
-	void setSearchText(wchar_t * txt2find);
+	void setSearchText(const wchar_t * txt2find);
 
 	void gotoNextFoundResult(int direction = 0) const {
 		if (_pFinder) _pFinder->gotoNextFoundResult(direction);
@@ -336,14 +336,13 @@ public :
 		_tab.getCurrentTitle(label, MAX_PATH);
 		::SetWindowText(_hSelf, label);
 	}
-	void beginNewFilesSearch()
-	{
+
+	void beginNewFilesSearch() {
 		_pFinder->beginNewFilesSearch();
 		_pFinder->addSearchLine(getText2search().c_str());
 	}
 
-	void finishFilesSearch(int count, int searchedCount, bool searchedEntireNotSelection)
-	{
+	void finishFilesSearch(int count, int searchedCount, bool searchedEntireNotSelection) {
 		_pFinder->finishFilesSearch(count, searchedCount, searchedEntireNotSelection, _env);
 	}
 
@@ -421,6 +420,7 @@ public :
 	DIALOG_TYPE getCurrentStatus() {return _currentStatus;};
 	Finder* getFinderFrom(HWND hwnd);
 	int regexBackwardMsgBox();
+	const wchar_t* setSearchTextWithSettings();
 
 protected :
 	void resizeDialogElements();
@@ -526,7 +526,7 @@ private:
 	void drawItem(LPDRAWITEMSTRUCT lpDrawItemStruct);
 	bool replaceInFilesConfirmCheck(const std::wstring& directory, const std::wstring& fileTypes);
 	bool replaceInProjectsConfirmCheck();
-	bool replaceInOpenDocsConfirmCheck(void);
+	bool replaceInOpenDocsConfirmCheck();
 
 	ContextMenu _swapPopupMenu;
 	enum SwapButtonStatus {swap, down, up} _swapButtonStatus = swap;

PowerEditor/installer/nativeLang/english.xml
@@ -1867,7 +1867,7 @@ Click on &quot;?&quot; button on right to open website with User Manual."/>
 			<npcCustomColor-tip value="Go to Style Configurator to change the default custom color for selected whitespace and non-printing characters (&quot;Non-printing characters custom color&quot;)."/>
 			<npcIncludeCcUniEol-tip value="Apply non-printing characters appearance settings to C0, C1 control and Unicode EOL (next line, line separator and paragraph separator) characters."/>
 			<searchingInSelThresh-tip value="Minimum number of selected characters in edit zone to automatically check the &quot;In selection&quot; checkbox when the Find dialog is activated. The maximum value is $INT_REPLACE$. Set the value to 0 to disable auto-checking."/>
-			<searchingFillFindWhat-tip value="Maximun number of selected characters in edit zone to fill automatically the &quot;Find what&quot; field when the Ctrl-F is triggered. The maximum value is $INT_REPLACE$, which is the maximum size of &quot;Find what&quot; field, limited by the system."/>
+			<searchingFillFindWhat-tip value="Maximum number of selected characters in edit zone to fill automatically the &quot;Find what&quot; field when the Ctrl-F is triggered. The maximum value is $INT_REPLACE$, which is the maximum size of &quot;Find what&quot; field, limited by the system."/>
 			<verticalEdge-tip value="Add your column marker by indicating its position with a decimal number. You can define several column markers by using white space to separate the different numbers."/>
 			<fileSaveAsCopySaveButton-tip value="Hold Shift while pressing Save to open the copy after saving."/>
 			<autoIndentBasic-tip value="Ensure that the indentation of the current line (i.e. the new line created by pressing the ENTER key) matches the indentation of the previous line."/>
@@ -1891,8 +1891,9 @@ If you select advanced mode but do not edit files in the aforementioned language
 			<statusbar-Sel value="Sel: "/>
 			<statusbar-Sel-number value="Sel"/>
 			<toolbar-accent-tip value="This option makes your toolbar icons follow Windows system accent color. Accent color is the highlight color used in buttons, borders, and Start menu tiles in Windows. To change it, go to Settings &gt; Personalization &gt; Colors, then select your preferred accent color."/>
-			<max-len-on-search-tip value="Only $INT_REPLACE$ characters are allowed for the find/replace text length - your input could be truncated, and it won't be saved for the next session."/>
-			<max-len-on-save-tip value="This search input (> $INT_REPLACE$ characters) won't be saved for the next session."/>
+			<len-limit-exceeded-tip value="Length limit exceeded: Your input may exceed the limit allowed and could have been truncated, and it won't be saved for the next session."/>
+			<max-len-on-search-tip value="Your input may exceed the limit allowed and could have been truncated, and it won't be saved for the next session."/>
+			<max-len-on-save-tip value="The length of your input is very long and may not be saved for your next session."/>
 			<goto-setting-tip value="Find your setting here"/>
 		</MiscStrings>
 	</Native-Langue>

PowerEditor/installer/nativeLang/english_customizable.xml
@@ -1867,7 +1867,7 @@ Click on &quot;?&quot; button on right to open website with User Manual."/>
 			<npcCustomColor-tip value="Go to Style Configurator to change the default custom color for selected whitespace and non-printing characters (&quot;Non-printing characters custom color&quot;)."/>
 			<npcIncludeCcUniEol-tip value="Apply non-printing characters appearance settings to C0, C1 control and Unicode EOL (next line, line separator and paragraph separator) characters."/>
 			<searchingInSelThresh-tip value="Minimum number of selected characters in edit zone to automatically check the &quot;In selection&quot; checkbox when the Find dialog is activated. The maximum value is $INT_REPLACE$. Set the value to 0 to disable auto-checking."/>
-			<searchingFillFindWhat-tip value="Maximun number of selected characters in edit zone to fill automatically the &quot;Find what&quot; field when the Ctrl-F is triggered. The maximum value is $INT_REPLACE$, which is the maximum size of &quot;Find what&quot; field, limited by the system."/>
+			<searchingFillFindWhat-tip value="Maximum number of selected characters in edit zone to fill automatically the &quot;Find what&quot; field when the Ctrl-F is triggered. The maximum value is $INT_REPLACE$, which is the maximum size of &quot;Find what&quot; field, limited by the system."/>
 			<verticalEdge-tip value="Add your column marker by indicating its position with a decimal number. You can define several column markers by using white space to separate the different numbers."/>
 			<fileSaveAsCopySaveButton-tip value="Hold Shift while pressing Save to open the copy after saving."/>
 			<autoIndentBasic-tip value="Ensure that the indentation of the current line (i.e. the new line created by pressing the ENTER key) matches the indentation of the previous line."/>
@@ -1891,8 +1891,9 @@ If you select advanced mode but do not edit files in the aforementioned language
 			<statusbar-Sel value="Sel: "/>
 			<statusbar-Sel-number value="Sel"/>
 			<toolbar-accent-tip value="This option makes your toolbar icons follow Windows system accent color. Accent color is the highlight color used in buttons, borders, and Start menu tiles in Windows. To change it, go to Settings &gt; Personalization &gt; Colors, then select your preferred accent color."/>
-			<max-len-on-search-tip value="Only $INT_REPLACE$ characters are allowed for the find/replace text length - your input could be truncated, and it won't be saved for the next session."/>
-			<max-len-on-save-tip value="This search input (> $INT_REPLACE$ characters) won't be saved for the next session."/>
+			<len-limit-exceeded-tip value="Length limit exceeded: Your input may exceed the limit allowed and could have been truncated, and it won't be saved for the next session."/>
+			<max-len-on-search-tip value="Your input may exceed the limit allowed and could have been truncated, and it won't be saved for the next session."/>
+			<max-len-on-save-tip value="The length of your input is very long and may not be saved for your next session."/>
 			<goto-setting-tip value="Find your setting here"/>
 		</MiscStrings>
 	</Native-Langue>

PowerEditor/installer/nativeLang/french.xml
@@ -1886,8 +1886,9 @@ Si vous sélectionnez le mode avancé sans modifier les fichiers des langues men
 			<statusbar-Sel value="Sel : "/>
 			<statusbar-Sel-number value="Sel"/>
 			<toolbar-accent-tip value="Cette option permet aux icônes de votre barre d'outils d'adopter la couleur d'accentuation du système Windows. La couleur d'accentuation est la couleur de surbrillance utilisée pour les boutons, les bordures et les tuiles du menu Démarrer sous Windows. Pour la modifier, accédez à Paramètres &gt; Personnalisation &gt; Couleurs, puis sélectionnez votre couleur d'accentuation préférée."/>
-			<max-len-on-search-tip value="Seuls $INT_REPLACE$ caractères sont autorisés pour la longueur du texte de recherche/remplacement - votre saisie pourrait être tronquée et ne serait pas enregistrée pour la session suivante."/>
-			<max-len-on-save-tip value="Cette entrée de recherche (> $INT_REPLACE$ caractères) ne sera pas enregistrée pour la prochaine session."/>
+			<len-limit-exceeded-tip value="Limite de longueur dépassée : Votre saisie peut dépasser la limite autorisée et avoir été tronquée, et elle ne sera pas enregistrée pour la prochaine session."/>
+			<max-len-on-search-tip value="Votre saisie peut dépasser la limite autorisée et avoir été tronquée, et elle ne sera pas enregistrée pour la prochaine session."/>
+			<max-len-on-save-tip value="La longueur de votre saisie est très longue et il se peut qu'elle ne soit pas enregistrée pour votre prochaine session."/>
 			<goto-setting-tip value="Réglage du paramètre ici"/>
 		</MiscStrings>
 	</Native-Langue>

PowerEditor/installer/nativeLang/taiwaneseMandarin.xml
@@ -1711,8 +1711,9 @@ C、C++、Java、C#、Objective-C、PHP、JavaScript、JSP、CSS、Perl、Rust�
 			<statusbar-Sel value="選取："/>
 			<statusbar-Sel-number value="選取"/>
 			<toolbar-accent-tip value="此選項可讓您的工具列圖示遵循 Windows 系統強調色。強調色是 Windows 中的按鈕、邊框和開始功能表圖塊所使用的反白顯示顏色。要更改它，請轉到「設定」&gt;「個性化」&gt;「顏色」，然後選擇您喜歡的強調色。"/>
-			<max-len-on-search-tip value="尋找/取代文字長度僅允許 $INT_REPLACE$ 個字元 - 您的輸入可能會被截斷，此搜尋輸入也將不會保存以供下次 Notepad++ 啟動時使用。"/>
-			<max-len-on-save-tip value="此搜尋輸入（ > $INT_REPLACE$ 個字元）將不會保存以供下次 Notepad++ 啟動時使用。"/>
+			<len-limit-exceeded-tip value="已超出長度限制：輸入內容可能超出允許的限制並已被截斷，此輸入也將不會保存以供下次 Notepad++ 啟動時使用。"/>
+			<max-len-on-search-tip value="輸入內容可能超出允許的限制並已被截斷，此輸入也將不會保存以供下次 Notepad++ 啟動時使用。"/>
+			<max-len-on-save-tip value="輸入內容過長，可能無法保存以供下次 Notepad++ 啟動時使用。"/>
 			<goto-setting-tip value="相關的偏好設定在此"/>
 		</MiscStrings>
 	</Native-Langue>

commit 0103f182f911f7fce85eaccaed3ff93672ff4c95

Fix "Select & Find Next" command not working regression
Regression introduced by:
https://github.com/notepad-plus-plus/notepad-plus-plus/commit/be59048c5e73f497526beb8a482fdb03d260525e#diff-ce3b24898ed0287ecacacc4d6c1712fb9eaf1ed67b36fd87315b8e80950de03aL1463

Fix #17200

PowerEditor/src/NppCommands.cpp
@@ -1443,8 +1443,8 @@ void Notepad_plus::command(int id)
 			if (isFirstTime)
 				_findReplaceDlg.doDialog(FIND_DLG, _nativeLangSpeaker.isRTL(), false);
 
-			const wchar_t* str = _findReplaceDlg.setSearchTextWithSettings();
-			if (!str) return;
+			wstring str = _findReplaceDlg.setSearchText();
+			if (str.empty()) return;
 
 			_findReplaceDlg._env->_str2Search = str;
 
@@ -1457,7 +1457,7 @@ void Notepad_plus::command(int id)
 			op._whichDirection = (id == IDM_SEARCH_SETANDFINDNEXT?DIR_DOWN:DIR_UP);
 
 			FindStatus status = FSNoMessage;
-			_findReplaceDlg.processFindNext(str, &op, &status);
+			_findReplaceDlg.processFindNext(str.c_str(), &op, &status);
 			if (status == FSEndReached)
 			{
 				wstring msg = _nativeLangSpeaker.getLocalizedStrFromID("find-status-end-reached", FIND_STATUS_END_REACHED_TEXT);

PowerEditor/src/ScintillaComponent/FindReplaceDlg.cpp
@@ -5375,10 +5375,27 @@ bool FindReplaceDlg::replaceInOpenDocsConfirmCheck()
 	return confirmed;
 }
 
-// return NULL if nothing to set in find field.
-// Otherwise return string pointer (wchar_t *) in which the selected text was copied.
-// Note that the string pointer don't need to and should not be deallocated.
-const wchar_t* FindReplaceDlg::setSearchTextWithSettings()
+// Expand selection (if needed) and set the selected text in Find What field.
+// Return empty string if nothing to set in find field.
+// Otherwise return string in which the selected text was copied.
+wstring FindReplaceDlg::setSearchText()
+{
+	const NppGUI& nppGui = NppParameters::getInstance().getNppGUI();
+	Sci_Position selStrCharNum = 0;
+	const wchar_t* selStr = (*_ppEditView)->getSelectedTextToWChar(true, &selStrCharNum);
+
+	if (selStr && selStrCharNum <= nppGui._fillFindWhatThreshold)
+	{
+		setSearchText(selStr);
+		return selStr;
+	}
+	return L"";
+}
+
+// Set the selected text in Find What field, according the Search settings.
+// Return empty string if nothing to set in find field.
+// Otherwise return string in which the selected text was copied.
+wstring FindReplaceDlg::setSearchTextWithSettings()
 {
 	const NppGUI& nppGui = NppParameters::getInstance().getNppGUI();
 	if (nppGui._fillFindFieldWithSelected)
@@ -5392,7 +5409,7 @@ const wchar_t* FindReplaceDlg::setSearchTextWithSettings()
 			return selStr;
 		}
 	}
-	return nullptr;
+	return L"";
 }
 
 wstring Finder::getHitsString(int count) const

PowerEditor/src/ScintillaComponent/FindReplaceDlg.h
@@ -419,7 +419,8 @@ public :
 	DIALOG_TYPE getCurrentStatus() { return _currentStatus; }
 	Finder* getFinderFrom(HWND hwnd);
 	int regexBackwardMsgBox();
-	const wchar_t* setSearchTextWithSettings();
+	std::wstring setSearchText();
+	std::wstring setSearchTextWithSettings();
 
 protected :
 	void resizeDialogElements();
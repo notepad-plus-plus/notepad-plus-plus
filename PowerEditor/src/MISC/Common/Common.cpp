notepad-plus-plus/notepad-plus-plus

commit 683e23f4aadb0306204085b31e1ba215a05574ad

Use pugixml for saving main config parameters
Ref: #16175

Close #17543

PowerEditor/src/WinControls/PluginsAdmin/pluginsAdmin.cpp
@@ -676,7 +676,7 @@ bool PluginsAdminDlg::initFromJson()
 
 	json j;
 
-#ifdef DEBUG // if not debug, then it's release
+#ifndef NDEBUG // if not debug, then it's release
 	
 	// load from nppPluginList.json instead of nppPluginList.dll
 #ifdef __MINGW32__

PowerEditor/src/Parameters.h
@@ -40,8 +40,6 @@
 #include <SciLexer.h>
 #include <Scintilla.h>
 
-#include <tinyxml.h>
-
 #include "ContextMenu.h"
 #include "DockingCont.h"
 #include "Notepad_plus_msgs.h"
@@ -582,11 +580,11 @@ public:
 
 	void now();
 
-	std::wstring toString() const // Return Notepad++ date format : YYYYMMDD
+	std::string toString() const // Return Notepad++ date format : YYYYMMDD
 	{
 		static constexpr size_t bufSize = 16;
-		wchar_t dateStr[bufSize]{};
-		std::swprintf(dateStr, bufSize, L"%04lu%02lu%02lu", _year, _month, _day);
+		char dateStr[bufSize]{};
+		std::snprintf(dateStr, bufSize, "%04lu%02lu%02lu", _year, _month, _day);
 		return dateStr;
 	}
 
@@ -1148,9 +1146,9 @@ struct FindHistory final
 
 	bool _isFifRecursive = true;
 	bool _isFifInHiddenFolder = false;
-    bool _isFifProjectPanel_1 = false;
-    bool _isFifProjectPanel_2 = false;
-    bool _isFifProjectPanel_3 = false;
+	bool _isFifProjectPanel_1 = false;
+	bool _isFifProjectPanel_2 = false;
+	bool _isFifProjectPanel_3 = false;
 
 	searchMode _searchMode = normal;
 	transparencyMode _transparencyMode = onLosingFocus;
@@ -1484,7 +1482,7 @@ public:
 	bool writeColumnEditorSettings();
 	bool writeFileBrowserSettings(const std::vector<std::wstring>& rootPaths, const std::wstring& latestSelectedItemPath);
 
-	TiXmlNode* getChildElementByAttribute(TiXmlNode *pere, const wchar_t *childName, const wchar_t *attributeName, const wchar_t *attributeVal) const;
+	static NppXml::Element getChildElementByAttribute(const NppXml::Element& element, const char* childName, const char* attributeName, const char* attributeVal);
 
 	bool writeScintillaParams();
 	void createXmlTreeFromGUIParams();
@@ -1797,7 +1795,7 @@ private:
 
 
 	XmlDocPath _pXmlDoc{}; // langs.xml
-	TiXmlDocument *_pXmlUserDoc = nullptr; // config.xml
+	XmlDocPath _xmlUserDoc{}; // config.xml
 	XmlDocPath _pXmlUserStylerDoc{}; // stylers.xml
 	XmlDocPath _pXmlUserLangDoc{}; // userDefineLang.xml
 	std::vector<UdlXmlFileState> _pXmlUserLangsDoc; // userDefineLang customized XMLs
@@ -2004,7 +2002,6 @@ private:
 	void feedFileListParameters(const NppXml::Element& element);
 	void feedScintillaParam(const NppXml::Element& element);
 	void feedDockingManager(const NppXml::Element& element);
-	void duplicateDockingManager(TiXmlNode *dockMngNode, TiXmlElement* dockMngElmt2Clone);
 	void feedFindHistoryParameters(const NppXml::Element& element);
 	void feedProjectPanelsParameters(const NppXml::Element& element);
 	void feedFileBrowserParameters(const NppXml::Element& element);
@@ -2031,10 +2028,10 @@ private:
 	static void insertUserCmd(NppXml::Element& userCmdRoot, const UserCommand& userCmd, const std::string& folderName);
 	static void insertScintKey(NppXml::Element& scintKeyRoot, const ScintillaKeyMap& scintKeyMap);
 	static void insertPluginCmd(NppXml::Element& pluginCmdRoot, const PluginCmdShortcut& pluginCmd);
-	TiXmlElement * insertGUIConfigBoolNode(TiXmlNode *r2w, const wchar_t *name, bool bVal);
-	void insertDockingParamNode(TiXmlNode *GUIRoot);
-	void writeExcludedLangList(TiXmlElement *element);
-	void writePrintSetting(TiXmlElement *element);
+	static NppXml::Element insertGUIConfigBoolNode(NppXml::Element& r2w, const char* name, bool bVal);
+	void insertDockingParamNode(NppXml::Element& GUIRoot) const;
+	void writeExcludedLangList(NppXml::Element& element) const;
+	void writePrintSetting(NppXml::Element& element) const;
 	void initMenuKeys();		//initialise menu keys and scintilla keys. Other keys are initialized on their own
 	void initScintillaKeys();	//these functions have to be called first before any modifications are loaded
 	static int getCmdIdFromMenuEntryItemName(HMENU mainMenuHandle, const std::wstring& menuEntryName, const std::wstring& menuItemName); // return -1 if not found

PowerEditor/src/localization.cpp
@@ -878,22 +878,22 @@ void NativeLangSpeaker::changeFindReplaceDlgLang(FindReplaceDlg& findReplaceDlg)
 				}
 				if (titre2 && titre2[0])
 				{
-					nppParam.getFindDlgTabTitles()._replace = string2wstring(titre2, _nativeLangEncoding);;
+					nppParam.getFindDlgTabTitles()._replace = string2wstring(titre2, _nativeLangEncoding);
 					findReplaceDlg.changeTabName(REPLACE_DLG, nppParam.getFindDlgTabTitles()._replace.c_str());
 				}
 				if (titre3 && titre3[0])
 				{
-					nppParam.getFindDlgTabTitles()._findInFiles = string2wstring(titre3, _nativeLangEncoding);;
+					nppParam.getFindDlgTabTitles()._findInFiles = string2wstring(titre3, _nativeLangEncoding);
 					findReplaceDlg.changeTabName(FINDINFILES_DLG, nppParam.getFindDlgTabTitles()._findInFiles.c_str());
 				}
 				if (titre4 && titre4[0])
 				{
-					nppParam.getFindDlgTabTitles()._findInProjects = string2wstring(titre4, _nativeLangEncoding);;
+					nppParam.getFindDlgTabTitles()._findInProjects = string2wstring(titre4, _nativeLangEncoding);
 					findReplaceDlg.changeTabName(FINDINPROJECTS_DLG, nppParam.getFindDlgTabTitles()._findInProjects.c_str());
 				}
 				if (titre5 && titre5[0])
 				{
-					nppParam.getFindDlgTabTitles()._mark = string2wstring(titre5, _nativeLangEncoding);;
+					nppParam.getFindDlgTabTitles()._mark = string2wstring(titre5, _nativeLangEncoding);
 					findReplaceDlg.changeTabName(MARK_DLG, nppParam.getFindDlgTabTitles()._mark.c_str());
 				}
 			}

commit 35239328c9ee961ba924c2ecd4847758de81bec0

Fix regression with DEBUG macro
- tweak warning flags in makefile for thirdparty code

Fix https://github.com/notepad-plus-plus/notepad-plus-plus/commit/683e23f4aadb0306204085b31e1ba215a05574ad#commitcomment-178390650

Regression from https://github.com/notepad-plus-plus/notepad-plus-plus/commit/683e23f4aadb0306204085b31e1ba215a05574ad

Close #17798

PowerEditor/gcc/makefile
@@ -47,7 +47,7 @@ else
   COMPILER_SUFFIX := gcc.
 endif
 
-CXXFLAGS := -include $(GCC_DIRECTORY)/gcc-fixes.h -std=c++20
+CXXFLAGS := -include $(GCC_DIRECTORY)/gcc-fixes.h -std=c++20 -Wpedantic -Wall -Wextra -Wconversion
 RC := $(CROSS_COMPILE)windres
 RCFLAGS := --codepage=65001
 CPP_PATH := $(SCINTILLA_DIRECTORY)/include $(LEXILLA_DIRECTORY)/include
@@ -71,42 +71,46 @@ BUILD_TYPE := debug
 BUILD_SUFFIX := -debug
 CXXFLAGS += -Og \
             -g \
-            -Wpedantic \
-            -Wall \
-            -Wextra \
             -Wcast-align \
-            -Wconversion \
             -Wextra-semi \
             -Wformat=2 \
             -Wmisleading-indentation \
             -Wunused
-CPP_DEFINE += DEBUG
+CPP_DEFINE += DEBUG _DEBUG
 endif
 
 ifeq ($(CXX),clang++)
-  CXXFLAGS += -Wpedantic \
-              -Wall \
-              -Wextra \
+  CXXFLAGS += -Wextra-semi \
+              -Wformat=2 \
               -Wimplicit-fallthrough \
+              -Wmisleading-indentation \
+              -Wunused \
               -Wno-c++98-compat \
               -Wno-c++98-compat-pedantic \
               -Wno-c99-extensions \
               -Wno-cast-align \
+              -Wno-cast-function-type-strict \
+              -Wno-covered-switch-default \
               -Wno-deprecated-declarations \
+              -Wno-exit-time-destructors \
+              -Wno-global-constructors \
               -Wno-nested-anon-types \
               -Wno-overloaded-virtual \
               -Wno-padded \
               -Wno-pragma-pack \
               -Wno-reserved-id-macro \
               -Wno-reserved-identifier \
+              -Wno-nrvo \
               -Wno-sign-conversion \
               -Wno-switch-default \
               -Wno-unsafe-buffer-usage \
               -Wno-unsafe-buffer-usage-in-libc-call \
-              -Wno-unused-command-line-argument
+              -Wno-unused-command-line-argument \
+              -Wno-weak-vtables
 else
   CXXFLAGS += -Wno-cast-function-type \
-              -Wno-overloaded-virtual
+              -Wno-overloaded-virtual \
+              -Wno-system-headers
 endif
 
 ifneq "$(filter-out 0,$(CLANGANALYZE))" ""
@@ -248,6 +252,23 @@ CXX_TARGETS := $(patsubst %.cpp,$(BUILD_DIRECTORY)/%.o,$(sort $(filter %.cpp,$(G
 vpath %.rc $(GCC_DIRECTORY) $(SRC_DIRECTORY)
 RC_TARGETS := $(patsubst %.rc,$(BUILD_DIRECTORY)/%.res,$(sort $(filter %.rc,$(GCC_SUBTREE) $(SRC_SUBTREE))))
 
+# for 3rd-party cpp files use only basic warning flags, for headers supress warnings
+THIRD_DIRS := $(SRC_DIRECTORY)/json \
+              $(SRC_DIRECTORY)/pugixml \
+              $(SRC_DIRECTORY)/uchardet \
+              $(SRC_DIRECTORY)/MISC/crc16 \
+              $(SRC_DIRECTORY)/MISC/md5 \
+              $(SRC_DIRECTORY)/MISC/sha1 \
+              $(SRC_DIRECTORY)/MISC/sha2 \
+              $(SRC_DIRECTORY)/MISC/sha512 \
+              $(SCINTILLA_DIRECTORY)/include \
+              $(LEXILLA_DIRECTORY)/include
+
+THIRD_ISYSTEM := $(addprefix -isystem ,$(THIRD_DIRS))
+CXXFLAGS += $(THIRD_ISYSTEM)
+
+thirdparty_flags = $(if $(filter $(foreach d,$(THIRD_DIRS),$(d)/%),$(1)), -w -Wpedantic -Wall -Wextra,)
+
 #
 # actions
 #
@@ -292,7 +313,7 @@ binary: $(TARGET_BINARY) data
 
 $(CXX_TARGETS): $(BUILD_DIRECTORY)/%.o: %.cpp | $(BUILD_DIRECTORY)
 	@echo + compiling $<
-	$(AT)$(CXX) $(CXXFLAGS) $(addprefix -I,$(CPP_PATH)) $(addprefix -D,$(CPP_DEFINE)) -MMD -c -o $@ $<
+	$(AT)$(CXX) $(CXXFLAGS) $(call thirdparty_flags,$<) $(addprefix -I,$(CPP_PATH)) $(addprefix -D,$(CPP_DEFINE)) -MMD -c -o $@ $<
 
 $(RC_TARGETS): $(BUILD_DIRECTORY)/%.res: %.rc | $(BUILD_DIRECTORY)
 	@echo + compiling $<

PowerEditor/src/MISC/Common/Common.cpp
@@ -1813,7 +1813,7 @@ Version::Version(const wstring& versionStr)
 			++i;
 		}
 	}
-#ifdef DEBUG
+#if !defined(NDEBUG) 
 	catch (const std::wstring& s)
 	{
 		_major = 0;
@@ -1830,7 +1830,7 @@ Version::Version(const wstring& versionStr)
 		_minor = 0;
 		_patch = 0;
 		_build = 0;
-#ifdef DEBUG
+#if !defined(NDEBUG) 
 		throw std::wstring(L"Unknown exception from \"Version::Version(const wstring& versionStr)\"");
 #endif
 	}

PowerEditor/src/MISC/Common/verifySignedfile.cpp
@@ -47,7 +47,7 @@ SecurityGuard::SecurityGuard()
 
 bool SecurityGuard::checkModule([[maybe_unused]] const std::wstring& filePath, [[maybe_unused]] NppModule module2check)
 {
-#ifndef _DEBUG
+#ifdef NDEBUG
 	if (_securityMode == sm_certif)
 		return verifySignedBinary(filePath);
 	else if (_securityMode == sm_sha256)

PowerEditor/src/NppBigSwitch.cpp
@@ -802,7 +802,7 @@ LRESULT Notepad_plus::process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPa
 					}
 					else
 					{
-#ifdef DEBUG 
+#if !defined(NDEBUG)  
 						printStr(L"sizeof(CmdLineParams) != cmdLineParamsSize\rCmdLineParams is formed by an instance of another version,\rwhereas your CmdLineParams has been modified in this instance.");
 #endif
 					}

PowerEditor/src/NppCommands.cpp
@@ -3744,7 +3744,7 @@ void Notepad_plus::command(int id)
 				pathAppend(updaterFullPath, L"gup.exe");
 
 
-#ifdef DEBUG // if not debug, then it's release
+#if !defined(NDEBUG)  // if not debug, then it's release
 				bool isCertifVerified = true;
 #else //RELEASE
 				// check the signature on updater

PowerEditor/src/Utf8_16.cpp
@@ -557,7 +557,7 @@ void Utf8_Iter::set(const ubyte* pBuf, size_t nLen, UniMode eEncoding)
 
 bool Utf8_Iter::get(utf16* c)
 {
-#ifdef _DEBUG
+#if !defined(NDEBUG)
 	assert(m_out1st != m_outLst);
 #endif
 	if (m_out1st == m_outLst) return false;

PowerEditor/src/WinControls/PluginsAdmin/pluginsAdmin.cpp
@@ -269,7 +269,7 @@ PluginsAdminDlg::PluginsAdminDlg()
 	// get plugin-list path
 	_pluginListFullPath = nppParameters.getPluginConfDir();
 
-#ifdef DEBUG // if not debug, then it's release
+#if !defined(NDEBUG)  // if not debug, then it's release
 	// load from nppPluginList.json instead of nppPluginList.dll
 	pathAppend(_pluginListFullPath, L"nppPluginList.json");
 #else //RELEASE
@@ -616,7 +616,7 @@ static bool loadFromJson(std::vector<PluginUpdateInfo*>& pl, std::wstring& verSt
 
 			pl.push_back(pi);
 		}
-#ifdef DEBUG
+#if !defined(NDEBUG) 
 		catch (const wstring& exceptionStr)
 		{
 			::MessageBox(NULL, exceptionStr.c_str(), L"Exception caught in: PluginsAdmin loadFromJson()", MB_ICONERROR);
@@ -631,7 +631,7 @@ static bool loadFromJson(std::vector<PluginUpdateInfo*>& pl, std::wstring& verSt
 #endif
 		catch (...) // If one of mandatory properties is missing or with the incorrect format, an exception is thrown then this plugin will be ignored
 		{
-#ifdef DEBUG
+#if !defined(NDEBUG) 
 			::MessageBoxA(NULL, "An unknown exception is just caught", "Unknown Exception", MB_OK);
 #endif
 			continue; 
@@ -676,7 +676,7 @@ bool PluginsAdminDlg::initFromJson()
 
 	json j;
 
-#ifndef NDEBUG // if not debug, then it's release
+#if !defined(NDEBUG) // if not debug, then it's release
 	
 	// load from nppPluginList.json instead of nppPluginList.dll
 #ifdef __MINGW32__

PowerEditor/src/WinControls/WindowsDlg/WinMgr.cpp
@@ -87,7 +87,7 @@ void CWinMgr::SetWindowPositions(HWND hWnd)
 						rc.left,rc.top,RectWidth(rc),RectHeight(rc),
 						SWP_NOZORDER);
 					InvalidateRect(hwndChild,NULL,TRUE); // repaint
-#ifndef NDEBUG
+#if !defined(NDEBUG)
 					++count;
 #endif
 				}
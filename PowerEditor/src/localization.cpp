notepad-plus-plus/notepad-plus-plus

commit b9df41ad3354c0657ab7762a4a5b5a0991a6d1d8

Code enhancement: Upgrade localization to TinyXML2
Remove tinyXmlA and use TinyXML2 instead.

Ref: #16175 (partially fixed)
Close #17233

PowerEditor/src/localization.h
@@ -17,10 +17,13 @@
 
 #pragma once
 
-#include <map>
-#include "Common.h"
-#include "tinyxmlA.h"
+#include <windows.h>
 
+#include <map>
+#include <string>
+#include <vector>
+
+#include "NppXml.h"
 
 class FindReplaceDlg;
 class PreferenceDlg;
@@ -28,37 +31,47 @@ class ShortcutMapper;
 class UserDefineDialog;
 class PluginsAdminDlg;
 
-class MenuPosition {
+class MenuPosition
+{
 public:
+	constexpr MenuPosition(int x, int y, int z, const char* id) noexcept
+		: _x(x), _y(y), _z(z), _id(id)
+	{}
+
 	int _x = -1; // menu
 	int _y = -1; // sub-menu
 	int _z = -1; // sub-sub-menu
-	char _id[64] = { '\0' }; // a unique string defined in localization XML file
+
+	static const MenuPosition& getMenuPosition(const char* id);
+
+private:
+	const char* _id = nullptr; // a unique string defined in localization XML file
 };
 
 
-class NativeLangSpeaker {
+class NativeLangSpeaker
+{
 public:
-	void init(TiXmlDocumentA *nativeLangDocRootA, bool loadIfEnglish = false);
-	void changeConfigLang(HWND hDlg);
+	void init(NppXml::Document nativeLangDocRoot, bool loadIfEnglish = false);
+	void changeConfigLang(HWND hDlg) const;
 	void changeLangTabContextMenu(HMENU hCM) const;
 	void getAlternativeNameFromTabContextMenu(std::wstring& output, int cmdID, bool isAlternative, const std::wstring& defaultValue) const;
-	TiXmlNodeA * searchDlgNode(TiXmlNodeA *node, const char *dlgTagName);
-	bool changeDlgLang(HWND hDlg, const char *dlgTagName, char *title = NULL, size_t titleMaxSize = 0);
-	void changeLangTabDropContextMenu(HMENU hCM);
-	void changeLangTrayIconContexMenu(HMENU hCM);
-	std::wstring getSubMenuEntryName(const char *nodeName) const;
+	static NppXml::Node searchDlgNode(NppXml::Node node, const char* dlgTagName);
+	bool changeDlgLang(HWND hDlg, const char* dlgTagName, char* title = nullptr, size_t titleMaxSize = 0);
+	void changeLangTabDropContextMenu(HMENU hCM) const;
+	void changeLangTrayIconContexMenu(HMENU hCM) const;
+	std::wstring getSubMenuEntryName(const char* nodeName) const;
 	std::wstring getNativeLangMenuString(int itemID, std::wstring inCaseOfFailureStr = L"", bool removeMarkAnd = false) const;
 	std::wstring getShortcutNameString(int itemID) const;
 
-	void changeMenuLang(HMENU menuHandle);
-	void changeShortcutLang();
-	void changeStyleCtrlsLang(HWND hDlg, int *idArray, const char **translatedText);
-	void changeUserDefineLang(UserDefineDialog *userDefineDlg);
-	void changeUserDefineLangPopupDlg(HWND hDlg);
-	void changeFindReplaceDlgLang(FindReplaceDlg & findReplaceDlg);
-	void changePreferenceDlgLang(PreferenceDlg & preference);
-	void changePluginsAdminDlgLang(PluginsAdminDlg & pluginsAdminDlg);
+	void changeMenuLang(HMENU menuHandle) const;
+	void changeShortcutLang() const;
+	static void changeStyleCtrlsLang(HWND hDlg, int* idArray, const char** translatedText);
+	void changeUserDefineLang(UserDefineDialog* userDefineDlg) const;
+	void changeUserDefineLangPopupDlg(HWND hDlg) const;
+	void changeFindReplaceDlgLang(FindReplaceDlg& findReplaceDlg);
+	void changePreferenceDlgLang(PreferenceDlg& preference);
+	void changePluginsAdminDlgLang(PluginsAdminDlg& pluginsAdminDlg);
 
 	bool getDoSaveOrNotStrings(std::wstring& title, std::wstring& msg);
 
@@ -70,44 +83,40 @@ public:
 		return _isEditZoneRTL;
 	}
 
-	const char * getFileName() const {
+	const char* getFileName() const {
 		return _fileName;
 	}
 
-	const TiXmlNodeA * getNativeLangA() {
-		return _nativeLangA;
+	const NppXml::Node& getNativeLang() const {
+		return _nativeLang;
 	}
 
-	int getLangEncoding() const {
+	static int getLangEncoding() {
 		return _nativeLangEncoding;
 	}
 
-	bool getMsgBoxLang(const char *msgBoxTagName, std::wstring & title, std::wstring & message);
-	std::wstring getShortcutMapperLangStr(const char *nodeName, const wchar_t *defaultStr) const;
-	std::wstring getProjectPanelLangMenuStr(const char * nodeName, int cmdID, const wchar_t *defaultStr) const;
+	bool getMsgBoxLang(const char* msgBoxTagName, std::wstring& title, std::wstring& message);
+	std::wstring getShortcutMapperLangStr(const char *nodeName, const wchar_t* defaultStr) const;
+	std::wstring getProjectPanelLangMenuStr(const char* nodeName, int cmdID, const wchar_t* defaultStr) const;
 	std::wstring getDlgLangMenuStr(const char* firstLevelNodeName, const char* secondLevelNodeName, int cmdID, const wchar_t *defaultStr) const;
 	std::wstring getCmdLangStr(std::vector<const char*> nodeNames, int cmdID, const wchar_t* defaultStr) const;
-	std::wstring getAttrNameStr(const wchar_t *defaultStr, const char *nodeL1Name, const char *nodeL2Name, const char *nodeL3Name = "name") const;
-	std::wstring getAttrNameByIdStr(const wchar_t *defaultStr, TiXmlNodeA *targetNode, const char *nodeL1Value, const char *nodeL1Name = "id", const char *nodeL2Name = "name") const;
-	std::wstring getLocalizedStrFromID(const char *strID, const std::wstring& defaultString) const;
-	void getMainMenuEntryName(std::wstring& dest, HMENU hMenu, const char* menuIdStr, const wchar_t* defaultDest);
+	std::wstring getAttrNameStr(const wchar_t* defaultStr, const char* nodeL1Name, const char* nodeL2Name, const char* nodeL3Name = "name") const;
+	static std::wstring getAttrNameByIdStr(const wchar_t* defaultStr, NppXml::Node targetNode, const char* nodeL1Value, const char* nodeL1Name = "id", const char* nodeL2Name = "name");
+	std::wstring getLocalizedStrFromID(const char* strID, const std::wstring& defaultString) const;
+	void getMainMenuEntryName(std::wstring& dest, HMENU hMenu, const char* menuId, const wchar_t* defaultDest);
 
 	void resetShortcutMenuNameMap() {
 		_shortcutMenuEntryNameMap.clear();
 	}
 
-	int messageBox(const char *msgBoxTagName, HWND hWnd, const wchar_t *message, const wchar_t *title, int msgBoxType, int intInfo = 0, const wchar_t *strInfo = NULL);
+	int messageBox(const char* msgBoxTagName, HWND hWnd, const wchar_t* defaultMessage, const wchar_t* defaultTitle, int msgBoxType, int intInfo = 0, const wchar_t* strInfo = nullptr);
 private:
-	TiXmlNodeA *_nativeLangA = nullptr;
-	int _nativeLangEncoding = CP_ACP;
+	NppXml::Node _nativeLang{};
+	static constexpr int _nativeLangEncoding = CP_UTF8; // tinyxml2 uses only UTF8
 	bool _isRTL = false; // for Notepad++ GUI
-	bool _isEditZoneRTL = false; // for Scitilla
-	const char *_fileName = nullptr;
+	bool _isEditZoneRTL = false; // for Scintilla
+	const char* _fileName = nullptr;
 	std::map<std::string, std::wstring> _shortcutMenuEntryNameMap;
 
 	static void resizeCheckboxRadioBtn(HWND hWnd);
 };
-
-
-MenuPosition & getMenuPosition(const char *id);
-

PowerEditor/gcc/makefile
@@ -46,7 +46,7 @@ CXXFLAGS := -include $(GCC_DIRECTORY)/gcc-fixes.h -std=c++20
 RC := $(CROSS_COMPILE)windres
 RCFLAGS := --codepage=65001
 CPP_PATH := $(SCINTILLA_DIRECTORY)/include $(LEXILLA_DIRECTORY)/include
-CPP_DEFINE := UNICODE _UNICODE OEMRESOURCE NOMINMAX _WIN32_WINNT=_WIN32_WINNT_WIN7 NTDDI_VERSION=NTDDI_WIN7 TIXML_USE_STL TIXMLA_USE_STL
+CPP_DEFINE := UNICODE _UNICODE OEMRESOURCE NOMINMAX _WIN32_WINNT=_WIN32_WINNT_WIN7 NTDDI_VERSION=NTDDI_WIN7 TIXML_USE_STL
 LD := $(CXX)
 LDFLAGS := -municode -mwindows
 LD_PATH :=

PowerEditor/src/CMakeLists.txt
@@ -28,6 +28,7 @@ SET(projIncludDir	../../scintilla/include/
 					./MISC/SysMsg/
 					./WinControls/StatusBar/
 					./WinControls/StaticDialog/RunDlg/
+					./tinyxml2/
 					./TinyXml/
 					./WinControls/ColourPicker/
 					./Win32Explr/
@@ -45,7 +46,6 @@ SET(projIncludDir	../../scintilla/include/
 					./WinControls/ToolTip/
 					./MISC/Exception/
 					./MISC/Common/
-					./TinyXml/tinyXmlA/
 					./WinControls/AnsiCharPanel/
 					./WinControls/ClipboardHistory/
 					./WinControls/FindCharsInRange/
@@ -172,14 +172,11 @@ SET(src_files
 			./WinControls/TabBar/TabBar.cpp
 			./WinControls/TaskList/TaskList.cpp
 			./WinControls/TaskList/TaskListDlg.cpp
+			./tinyxml2/tinyxml2.cpp
 			./TinyXml/tinystr.cpp
-			./TinyXml/tinyXmlA/tinystrA.cpp
 			./TinyXml/tinyxml.cpp
-			./TinyXml/tinyXmlA/tinyxmlA.cpp
 			./TinyXml/tinyxmlerror.cpp
-			./TinyXml/tinyXmlA/tinyxmlerrorA.cpp
 			./TinyXml/tinyxmlparser.cpp
-			./TinyXml/tinyXmlA/tinyxmlparserA.cpp
 			./WinControls/ToolBar/ToolBar.cpp
 			./WinControls/ToolTip/ToolTip.cpp
 			./WinControls/TrayIcon/trayIconControler.cpp
@@ -219,6 +216,7 @@ SET(include_files
 			./MISC/sha2/sha-256.h
 			./MISC/sha512/sha512.h
 			./NppDarkMode.h
+			./NppXml.h
 			./ScintillaComponent/columnEditor_rc.h
 			./ScintillaComponent/FindReplaceDlg_rc.h
 			./Utf8.h
@@ -334,10 +332,9 @@ SET(include_files
 			./WinControls/TaskList/TaskList.h
 			./WinControls/TaskList/TaskListDlg.h
 			./WinControls/TaskList/TaskListDlg_rc.h
+			./tinyxml2/tinyxml2.h
 			./TinyXml/tinystr.h
-			./TinyXml/tinyXmlA/tinystrA.h
 			./TinyXml/tinyxml.h
-			./TinyXml/tinyXmlA/tinyxmlA.h
 			./WinControls/ToolBar/ToolBar.h
 			./WinControls/ToolTip/ToolTip.h
 			./WinControls/TrayIcon/trayIconControler.h
@@ -397,13 +394,13 @@ IF (WIN32)
 if ( MSVC )
 	#do not use for mingw builds
 	SET(CMAKE_CXX_FLAGS "/EHa /MP /W4")
-	SET(defs -DUNICODE -D_UNICODE -D_WIN32_WINNT=_WIN32_WINNT_WIN7 -DNTDDI_VERSION=NTDDI_WIN7 -D_USE_64BIT_TIME_T -DTIXML_USE_STL -DTIXMLA_USE_STL -DNOMINMAX -DOEMRESOURCE -D_CRT_NONSTDC_NO_DEPRECATE -D_CRT_SECURE_NO_WARNINGS -D_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING )
+	SET(defs -DUNICODE -D_UNICODE -D_WIN32_WINNT=_WIN32_WINNT_WIN7 -DNTDDI_VERSION=NTDDI_WIN7 -D_USE_64BIT_TIME_T -DTIXML_USE_STL -DNOMINMAX -DOEMRESOURCE -D_CRT_NONSTDC_NO_DEPRECATE -D_CRT_SECURE_NO_WARNINGS -D_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING )
 	set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
 	set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
 else ( MSVC )
 	# For possible MinGW compilation
 	SET(CMAKE_CXX_FLAGS "-include../gcc/gcc-fixes.h -std=c++20 -fpermissive -municode")
-	SET(defs -DUNICODE -D_UNICODE -D_WIN32_WINNT=_WIN32_WINNT_WIN7 -DNTDDI_VERSION=NTDDI_WIN7 -D_USE_64BIT_TIME_T -DTIXML_USE_STL -DTIXMLA_USE_STL -DNOMINMAX -DOEMRESOURCE)
+	SET(defs -DUNICODE -D_UNICODE -D_WIN32_WINNT=_WIN32_WINNT_WIN7 -DNTDDI_VERSION=NTDDI_WIN7 -D_USE_64BIT_TIME_T -DTIXML_USE_STL -DNOMINMAX -DOEMRESOURCE)
 endif ( MSVC )
 ENDIF (WIN32)
 

PowerEditor/src/Notepad_plus.cpp
@@ -14,10 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include <ctime>
+#include "Notepad_plus.h"
+
 #include <shlwapi.h>
 #include <wininet.h>
-#include "Notepad_plus.h"
+
+#include <ctime>
+
+#include "NppXml.h"
 #include "Notepad_plus_Window.h"
 #include "CustomFileDialog.h"
 #include "Printer.h"
@@ -135,8 +139,8 @@ Notepad_plus::Notepad_plus()
 	ZeroMemory(&_prevSelectedRange, sizeof(_prevSelectedRange));
 
 	NppParameters& nppParam = NppParameters::getInstance();
-	TiXmlDocumentA *nativeLangDocRootA = nppParam.getNativeLangA();
-    _nativeLangSpeaker.init(nativeLangDocRootA);
+	NppXml::Document nativeLangDocRoot = nppParam.getNativeLang();
+	_nativeLangSpeaker.init(nativeLangDocRoot);
 
 	LocalizationSwitcher & localizationSwitcher = nppParam.getLocalizationSwitcher();
     const char *fn = _nativeLangSpeaker.getFileName();
@@ -7156,13 +7160,13 @@ bool Notepad_plus::reloadLang()
 		return false;
 	}
 
-	TiXmlDocumentA *nativeLangDocRootA = nppParam.getNativeLangA();
-	if (!nativeLangDocRootA)
+	NppXml::Document nativeLangDocRoot = nppParam.getNativeLang();
+	if (nativeLangDocRoot == nullptr)
 	{
 		return false;
 	}
 
-    _nativeLangSpeaker.init(nativeLangDocRootA, true);
+    _nativeLangSpeaker.init(nativeLangDocRoot, true);
 
     nppParam.reloadContextMenuFromXmlTree(_mainMenuHandle, _pluginsManager.getMenuHandle());
 
@@ -9187,7 +9191,7 @@ BOOL Notepad_plus::notifyTBShowMenu(LPNMTOOLBARW lpnmtb, const char* menuPosId)
 	::SendMessage(lpnmtb->hdr.hwndFrom, TB_GETRECT, static_cast<WPARAM>(lpnmtb->iItem), reinterpret_cast<LPARAM>(&rcItem));
 	::MapWindowPoints(lpnmtb->hdr.hwndFrom, HWND_DESKTOP, reinterpret_cast<LPPOINT>(&rcItem), 2);
 
-	const MenuPosition& menuPos = getMenuPosition(menuPosId);
+	const MenuPosition& menuPos = MenuPosition::getMenuPosition(menuPosId);
 	HMENU hSubMenuView = ::GetSubMenu(_mainMenuHandle, menuPos._x);
 	if (hSubMenuView != nullptr)
 	{

PowerEditor/src/NppCommands.cpp
@@ -3546,22 +3546,7 @@ void Notepad_plus::command(int id)
 
 			if (doAboutDlg)
 			{
-				//bool isFirstTime = !_aboutDlg.isCreated();
 				_aboutDlg.doDialog();
-				/*
-				if (isFirstTime && _nativeLangSpeaker.getNativeLangA())
-				{
-					if (_nativeLangSpeaker.getLangEncoding() == NPP_CP_BIG5)
-					{
-						const char *authorName = "«J¤µ§^";
-						HWND hItem = ::GetDlgItem(_aboutDlg.getHSelf(), IDC_AUTHOR_NAME);
-
-						WcharMbcsConvertor& wmc = WcharMbcsConvertor::getInstance();
-						const wchar_t *authorNameW = wmc.char2wchar(authorName, NPP_CP_BIG5);
-						::SetWindowText(hItem, authorNameW);
-					}
-				}
-				*/
 			}
 			break;
 		}

PowerEditor/src/NppNotification.cpp
@@ -15,7 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-
+#include "localization.h"
 #include "Notepad_plus_Window.h"
 #include "functionListPanel.h"
 #include "xmlMatchedTagsHighlighter.h"
@@ -178,7 +178,7 @@ BOOL Notepad_plus::notify(SCNotification *notification)
 			{
 				POINT p;
 				::GetCursorPos(&p);
-				MenuPosition& menuPos = getMenuPosition("search-bookmark");
+				const MenuPosition& menuPos = MenuPosition::getMenuPosition("search-bookmark");
 				HMENU hSearchMenu = ::GetSubMenu(_mainMenuHandle, menuPos._x);
 				if (hSearchMenu)
 				{
@@ -923,7 +923,7 @@ BOOL Notepad_plus::notify(SCNotification *notification)
 				{
 					POINT p;
 					::GetCursorPos(&p);
-					MenuPosition & menuPos = getMenuPosition("edit-eolConversion");
+					const MenuPosition& menuPos = MenuPosition::getMenuPosition("edit-eolConversion");
 					HMENU hEditMenu = ::GetSubMenu(_mainMenuHandle, menuPos._x);
 					if (!hEditMenu)
 						return TRUE;
@@ -966,7 +966,7 @@ BOOL Notepad_plus::notify(SCNotification *notification)
 				}
 				else if (lpnm->dwItemSpec == DWORD(STATUSBAR_EOF_FORMAT))
 				{
-					MenuPosition & menuPos = getMenuPosition("edit-eolConversion");
+					const MenuPosition& menuPos = MenuPosition::getMenuPosition("edit-eolConversion");
 					HMENU hEditMenu = ::GetSubMenu(_mainMenuHandle, menuPos._x);
 					if (!hEditMenu)
 						return TRUE;

PowerEditor/src/NppXml.h
@@ -1,0 +1,125 @@
+// This file is part of Notepad++ project
+// Copyright (c) 2025 ozone10 and Notepad++ team
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+#pragma once
+
+#include <tinyxml2.h>
+
+#include <cstdio>
+
+// Simple wrapper for TinyXML2
+namespace NppXml
+{
+	using NewDocument = tinyxml2::XMLDocument;
+	using Document = tinyxml2::XMLDocument*;
+	using Element = tinyxml2::XMLElement*;
+	using Node = tinyxml2::XMLNode*;
+
+	[[nodiscard]] inline bool loadFile(Document& doc, const wchar_t* filename) {
+		FILE* file = nullptr;
+		::_wfopen_s(&file, filename, L"rb");
+		bool result = false;
+		if (file != nullptr)
+		{
+			result = doc->LoadFile(file) == tinyxml2::XML_SUCCESS;
+			std::fclose(file);
+		}
+		return result;
+	}
+
+	[[nodiscard]] inline bool saveFile(Document& doc, const wchar_t* filename) {
+		FILE* file = nullptr;
+		::_wfopen_s(&file, filename, L"w");
+		bool result = false;
+		if (file != nullptr)
+		{
+			result = doc->SaveFile(file) == tinyxml2::XML_SUCCESS;
+			std::fclose(file);
+		}
+		return result;
+	}
+
+	[[nodiscard]] inline Element firstChildElement(const Document& doc, const char* name = nullptr) {
+		return doc->FirstChildElement(name);
+	}
+
+	[[nodiscard]] inline Element firstChildElement(const Node& node, const char* name = nullptr) {
+		return node->FirstChildElement(name);
+	}
+
+	[[nodiscard]] inline Element toElement(const Node& node) {
+		return node->ToElement();
+	}
+
+	[[nodiscard]] inline Element nextSiblingElement(const Node& node, const char* name = nullptr) {
+		return node->NextSiblingElement(name);
+	}
+
+	[[nodiscard]] inline Node firstChild(const Node& node) {
+		return node->FirstChild();
+	}
+
+	[[nodiscard]] inline Node nextSibling(const Node& node) {
+		return node->NextSibling();
+	}
+
+	[[nodiscard]] inline const char* value(const Node& node) {
+		return node->Value();
+	}
+
+	[[nodiscard]] inline const char* attribute(const Element& elem, const char* name) {
+		return elem->Attribute(name);
+	}
+
+	[[nodiscard]] inline int intAttribute(const Element& elem, const char* name, int defaultValue = 0) {
+		return elem->IntAttribute(name, defaultValue);
+	}
+
+	inline void setAttribute(Element& elem, const char* name, const char* value) {
+		elem->SetAttribute(name, value);
+	}
+
+	inline void setAttribute(Element& elem, const char* name, int value) {
+		elem->SetAttribute(name, value);
+	}
+
+	inline Node createNewDeclaration(Document& doc) {
+		return doc->LinkEndChild(doc->NewDeclaration(nullptr));
+	}
+
+	inline Element createChildElement(Document& doc, const char* name) {
+		Element elem = doc->GetDocument()->NewElement(name);
+		doc->InsertEndChild(elem);
+		return elem;
+	}
+
+	inline Element createChildElement(Node parent, const char* name) {
+		Element elem = parent->GetDocument()->NewElement(name);
+		parent->InsertEndChild(elem);
+		return elem;
+	}
+
+	inline Node createChildText(Node parent, const char* text) {
+		Node node = parent->GetDocument()->NewText(text);
+		parent->InsertEndChild(node);
+		return node;
+	}
+
+	inline void deleteChild(Node& parent, Node child) {
+		parent->DeleteChild(child);
+	}
+}

commit 9132d52c3af692e2cf75cd339cf70e8d663fca33

Fix regression with ProjectPanel Workspace text localization
Fix https://github.com/notepad-plus-plus/notepad-plus-plus/commit/b9df41ad3354c0657ab7762a4a5b5a0991a6d1d8#r178402812

Regression from https://github.com/notepad-plus-plus/notepad-plus-plus/commit/b9df41ad3354c0657ab7762a4a5b5a0991a6d1d8

Close #17799

PowerEditor/src/localization.cpp
@@ -625,9 +625,9 @@ void NativeLangSpeaker::changeLangTrayIconContexMenu(HMENU hCM) const
 		childNode;
 		childNode = NppXml::nextSiblingElement(childNode, "Item"))
 	{
-		const int id = NppXml::intAttribute(childNode, "id", -1);
+		const int id = NppXml::intAttribute(childNode, "id", -1); // id starts with <Item id="43101" name="Activate"/>
 		const char* name = NppXml::attribute(childNode, "name");
-		if (id > 0 && (name && name[0]))
+		if (id >= 0 && (name && name[0]))
 		{
 			const wchar_t* nameW = wmc.char2wchar(name, _nativeLangEncoding);
 			::ModifyMenu(hCM, id, MF_BYCOMMAND, id, nameW);
@@ -661,9 +661,9 @@ void NativeLangSpeaker::changeConfigLang(HWND hDlg) const
 		childNode;
 		childNode = NppXml::nextSiblingElement(childNode, "Item"))
 	{
-		const int id = NppXml::intAttribute(childNode, "id", -1);
+		const int id = NppXml::intAttribute(childNode, "id", -1); // id starts with <Item id="2" name="Cancel"/>
 		const char* name = NppXml::attribute(childNode, "name");
-		if (id > 0 && (name && name[0]))
+		if (id >= 0 && (name && name[0]))
 		{
 			HWND hItem = ::GetDlgItem(hDlg, id);
 			if (hItem)
@@ -680,9 +680,9 @@ void NativeLangSpeaker::changeConfigLang(HWND hDlg) const
 		childNode;
 		childNode = NppXml::nextSiblingElement(childNode, "Item"))
 	{
-		const int id = NppXml::intAttribute(childNode, "id", -1);
+		const int id = NppXml::intAttribute(childNode, "id", -1); // id starts with <Item id="2204" name="Bold"/>
 		const char* name = NppXml::attribute(childNode, "name");
-		if (id > 0 && (name && name[0]))
+		if (id >= 0 && (name && name[0]))
 		{
 			HWND hItem = ::GetDlgItem(hDlg, id);
 			if (hItem)
@@ -743,9 +743,9 @@ void NativeLangSpeaker::changeUserDefineLangPopupDlg(HWND hDlg) const
 		childNode;
 		childNode = NppXml::nextSiblingElement(childNode, "Item"))
 	{
-		const int id = NppXml::intAttribute(childNode, "id", -1);
+		const int id = NppXml::intAttribute(childNode, "id", -1); // id starts with <Item id="1" name="OK"/>
 		const char* name = NppXml::attribute(childNode, "name");
-		if (id > 0 && (name && name[0]))
+		if (id >= 0 && (name && name[0]))
 		{
 			HWND hItem = ::GetDlgItem(hDlg, id);
 			if (hItem)
@@ -784,26 +784,23 @@ void NativeLangSpeaker::changeUserDefineLang(UserDefineDialog* userDefineDlg) co
 		childNode;
 		childNode = NppXml::nextSiblingElement(childNode, "Item"))
 	{
-		const int id = NppXml::intAttribute(childNode, "id", -1);
+		const int id = NppXml::intAttribute(childNode, "id", -1); // id starts with <Item id="20001" name="Dock"/>
 		const char* name = NppXml::attribute(childNode, "name");
-		if (id > 0 && (name && name[0]))
+		if (id > 30 && (name && name[0]))
 		{
-			if (id > 30)
+			HWND hItem = ::GetDlgItem(hDlg, id);
+			if (hItem)
 			{
-				HWND hItem = ::GetDlgItem(hDlg, id);
-				if (hItem)
+				if (id == IDC_DOCK_BUTTON && userDefineDlg->isDocked())
 				{
-					if (id == IDC_DOCK_BUTTON && userDefineDlg->isDocked())
-					{
-						std::wstring undockStr = getAttrNameByIdStr(L"Undock", userDefineDlgNode, std::to_string(IDC_UNDOCK_BUTTON).c_str());
-						::SetWindowText(hItem, undockStr.c_str());
-					}
-					else
-					{
-						const wchar_t* nameW = wmc.char2wchar(name, _nativeLangEncoding);
-						::SetWindowText(hItem, nameW);
-						resizeCheckboxRadioBtn(hItem);
-					}
+					std::wstring undockStr = getAttrNameByIdStr(L"Undock", userDefineDlgNode, std::to_string(IDC_UNDOCK_BUTTON).c_str());
+					::SetWindowText(hItem, undockStr.c_str());
+				}
+				else
+				{
+					const wchar_t* nameW = wmc.char2wchar(name, _nativeLangEncoding);
+					::SetWindowText(hItem, nameW);
+					resizeCheckboxRadioBtn(hItem);
 				}
 			}
 		}
@@ -834,9 +831,13 @@ void NativeLangSpeaker::changeUserDefineLang(UserDefineDialog* userDefineDlg) co
 				childNode;
 				childNode = NppXml::nextSiblingElement(childNode, "Item"))
 			{
+				// "Folder" id starts with <Item id="21101" name="Default style"/>
+				// "Keywords" id starts with <Item id="22101" name="1st Group"/>
+				// "Comment" id starts with <Item id="23003" name="Line comment position"/>
+				// "Operator" id starts with <Item id="24101" name="Operators style"/>
 				const int id = NppXml::intAttribute(childNode, "id", -1);
 				const char* name = NppXml::attribute(childNode, "name");
-				if (id > 0 && (name && name[0]))
+				if (id >= 0 && (name && name[0]))
 				{
 					HWND hItem = ::GetDlgItem(hDlgArrary[i], id);
 					if (hItem)
@@ -966,7 +967,7 @@ void NativeLangSpeaker::changePluginsAdminDlgLang(PluginsAdminDlg& pluginsAdminD
 	}
 }
 
-void NativeLangSpeaker::changePreferenceDlgLang(PreferenceDlg& preference)
+void NativeLangSpeaker::changePreferenceDlgLang(PreferenceDlg& preference) const
 {
 	auto currentSel = preference.getListSelectedIndex();
 	changeDlgLang(preference.getHSelf(), "Preference");
@@ -1169,7 +1170,7 @@ void NativeLangSpeaker::changeShortcutLang() const
 	{
 		const int index = NppXml::intAttribute(childNode, "index", -1);
 		const int id = NppXml::intAttribute(childNode, "id", -1);
-		if (index > 0 && id > 0)
+		if (index >= 0 && id > 0)
 		{
 			if (static_cast<size_t>(index) < mainshortcuts.size()) //valid index only
 			{
@@ -1265,9 +1266,9 @@ bool NativeLangSpeaker::getDoSaveOrNotStrings(std::wstring& title, std::wstring&
 		childNode;
 		childNode = NppXml::nextSiblingElement(childNode, "Item"))
 	{
-		const int id = NppXml::intAttribute(childNode, "id", -1);
+		const int id = NppXml::intAttribute(childNode, "id", -1); // id starts with <Item id="2" name="&amp;Cancel"/>
 		const char* name = NppXml::attribute(childNode, "name");
-		if (id > 0 && (name && name[0]))
+		if (id >= 0 && (name && name[0]))
 		{
 			if (id == IDC_DOSAVEORNOTTEXT)
 			{
@@ -1312,9 +1313,9 @@ bool NativeLangSpeaker::changeDlgLang(HWND hDlg, const char* dlgTagName, char* t
 		childNode;
 		childNode = NppXml::nextSiblingElement(childNode, "Item"))
 	{
-		const int id = NppXml::intAttribute(childNode, "id", -1);
+		const int id = NppXml::intAttribute(childNode, "id", -1); // id starts with <Item id="1" name="depends on dialog"/>
 		const char* name = NppXml::attribute(childNode, "name");
-		if (id > 0 && (name && name[0]))
+		if (id >= 0 && (name && name[0]))
 		{
 			HWND hItem = ::GetDlgItem(hDlg, id);
 			if (hItem)
@@ -1414,8 +1415,10 @@ std::wstring NativeLangSpeaker::getDlgLangMenuStr(const char* firstLevelNodeName
 		childNode;
 		childNode = NppXml::nextSiblingElement(childNode, "Item"))
 	{
+		// depends on which menu (Find smap button, FolderAsWorkspace menu)
+		// smallest id starts with <Item id="1726" name="⇅ Swap Find with Replace"/>
 		const int id = NppXml::intAttribute(childNode, "id", -1);
-		if (id > 0 && id == cmdID)
+		if (id >= 0 && id == cmdID)
 		{
 			name = NppXml::attribute(childNode, "name");
 			break;
@@ -1454,8 +1457,10 @@ std::wstring NativeLangSpeaker::getCmdLangStr(const std::vector<const char*>& no
 		childNode;
 		childNode = NppXml::nextSiblingElement(childNode, "Item"))
 	{
-		const int id = NppXml::intAttribute(childNode, "id", -1);
-		if (id > 0 && id == cmdID)
+		// used by dark mode custom color reset button
+		// id starts with <Item id="7102" name="Black"/>
+		const int id = NppXml::intAttribute(childNode, "id", -1); 
+		if (id >= 0 && id == cmdID)
 		{
 			name = NppXml::attribute(childNode, "name");
 			break;
@@ -1487,8 +1492,8 @@ std::wstring NativeLangSpeaker::getProjectPanelLangMenuStr(const char* nodeName,
 		childNode;
 		childNode = NppXml::nextSiblingElement(childNode, "Item"))
 	{
-		const int id = NppXml::intAttribute(childNode, "id", -1);
-		if (id > 0 && id == cmdID)
+		const int id = NppXml::intAttribute(childNode, "id", -1); // id starts with <Item id="0" name="Workspace"/>
+		if (id >= 0 && id == cmdID)
 		{
 			name = NppXml::attribute(childNode, "name");
 			break;
@@ -1542,7 +1547,7 @@ std::wstring NativeLangSpeaker::getAttrNameByIdStr(const wchar_t* defaultStr, Np
 	return defaultStr;
 }
 
-int NativeLangSpeaker::messageBox(const char* msgBoxTagName, HWND hWnd, const wchar_t* defaultMessage, const wchar_t* defaultTitle, int msgBoxType, int intInfo, const wchar_t* strInfo)
+int NativeLangSpeaker::messageBox(const char* msgBoxTagName, HWND hWnd, const wchar_t* defaultMessage, const wchar_t* defaultTitle, int msgBoxType, int intInfo, const wchar_t* strInfo) const
 {
 	if (NppParameters::getInstance().isEndSessionCritical())
 		return IDCANCEL; // simulate Esc-key or Cancel-button as there should not be any big delay / code-flow block

PowerEditor/src/localization.h
@@ -70,7 +70,7 @@ public:
 	void changeUserDefineLang(UserDefineDialog* userDefineDlg) const;
 	void changeUserDefineLangPopupDlg(HWND hDlg) const;
 	void changeFindReplaceDlgLang(FindReplaceDlg& findReplaceDlg);
-	void changePreferenceDlgLang(PreferenceDlg& preference);
+	void changePreferenceDlgLang(PreferenceDlg& preference) const;
 	void changePluginsAdminDlgLang(PluginsAdminDlg& pluginsAdminDlg);
 
 	bool getDoSaveOrNotStrings(std::wstring& title, std::wstring& msg) const;
@@ -110,7 +110,7 @@ public:
 		_shortcutMenuEntryNameMap.clear();
 	}
 
-	int messageBox(const char* msgBoxTagName, HWND hWnd, const wchar_t* defaultMessage, const wchar_t* defaultTitle, int msgBoxType, int intInfo = 0, const wchar_t* strInfo = nullptr);
+	int messageBox(const char* msgBoxTagName, HWND hWnd, const wchar_t* defaultMessage, const wchar_t* defaultTitle, int msgBoxType, int intInfo = 0, const wchar_t* strInfo = nullptr) const;
 private:
 	NppXml::Element _nativeLang{};
 	static constexpr int _nativeLangEncoding = CP_UTF8; // all Notepad++ xml files should be UTF8
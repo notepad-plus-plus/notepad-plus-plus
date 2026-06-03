6ab5c2110168b4453443663d114ffc2088caae98
Don Ho <don.h@free.fr>
2026-06-04 00:48:27 +0200

    Notepad++ v8.9 . 6.4 Tiananmen Massacre Commemoration

--- PowerEditor/src/WinControls/AboutDlg/AboutDlg.cpp
+++ PowerEditor/src/WinControls/AboutDlg/AboutDlg.cpp
@@ -141,7 +141,8 @@ intptr_t CALLBACK AboutDlg::run_dlgProc(UINT message, WPARAM wParam, LPARAM lPar
 			//_pageLink.create(::GetDlgItem(_hSelf, IDC_AUTHOR_NAME), L"https://notepad-plus-plus.org/news/v881-we-are-with-ukraine/");
             
 			_pageLink.init(_hInst, _hSelf);
-            _pageLink.create(::GetDlgItem(_hSelf, IDC_HOME_ADDR), L"https://notepad-plus-plus.org/");
+			_pageLink.create(::GetDlgItem(_hSelf, IDC_AUTHOR_NAME), L"https://notepad-plus-plus.org/news/v8964-released/");
+            //_pageLink.create(::GetDlgItem(_hSelf, IDC_HOME_ADDR), L"https://notepad-plus-plus.org/");
 
 			return TRUE;
 		}

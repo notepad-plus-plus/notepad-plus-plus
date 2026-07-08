c3a2cfa9a8e796549f19b4606377b3e55ab98dd1
Vivyy <21315274+enginelesscc@users.noreply.github.com>
2026-07-08 12:27:16 +0200

    Properly mark lazyloaded files & unset after reload

--- PowerEditor/src/Notepad_plus.cpp
+++ PowerEditor/src/Notepad_plus.cpp
@@ -4232,6 +4232,10 @@ void Notepad_plus::setTitle()
 	{
 		result += L"*";
 	}
+	else if (buf->getStatus() == DOC_LAZYLOAD)
+	{
+		result += L"(L)";
+	}
 
 	if (nppGUI._shortTitlebar)
 	{
@@ -5252,12 +5256,17 @@ bool Notepad_plus::activateBuffer(BufferID id, int whichOne, bool forceApplyHili
 			return false;
 	}
 
-	if (reload)
+	if (reload || lazy)
 	{
 		performPostReload(whichOne);
 	}
 
 	notifyBufferActivated(id, whichOne);
+
+	int status = (int)pBuf->getStatus();
+	wchar_t buffer[1024];
+	_snwprintf(buffer, 1023, L"activateBuffer: %s, %d", pBuf->getCompactFileName(), status);
+	OutputDebugStringW(buffer);
 
 	return true;
 }

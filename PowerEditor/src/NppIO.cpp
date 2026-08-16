notepad-plus-plus/notepad-plus-plus

commit a449c5e8042b46a7ccabfe374f5d68f20c682a33

Fix breaking of the cloned file status after Notepad++ restart
Fix #16412, close #16440

PowerEditor/src/NppIO.cpp
@@ -2552,7 +2552,7 @@ bool Notepad_plus::loadSession(Session & session, bool isSnapshotMode, const wch
 			isWow64Off = true;
 		}
 #endif
-		if (doesFileExist(pFn))
+		if (doesFileExist(pFn) || (isSnapshotMode && doesFileExist(session._subViewFiles[k]._backupFilePath.c_str())))
 		{
 			//check if already open in main. If so, clone
 			BufferID clonedBuf = _mainDocTab.findBufferByName(pFn);
@@ -2568,10 +2568,6 @@ bool Notepad_plus::loadSession(Session & session, bool isSnapshotMode, const wch
 				else
 					lastOpened = doOpen(pFn, false, false, session._subViewFiles[k]._encoding);
 			}
-		}
-		else if (isSnapshotMode && doesFileExist(session._subViewFiles[k]._backupFilePath.c_str()))
-		{
-			lastOpened = doOpen(pFn, false, false, session._subViewFiles[k]._encoding, session._subViewFiles[k]._backupFilePath.c_str(), session._subViewFiles[k]._originalFileLastModifTimestamp);
 		}
 		else
 		{

commit bf5987d337efb44e28933ed107ef64c967baa213

Fix loading session for alone subview and doc/tab with untitle name
In an edge case, when the loaded session contained docs only for the subview and one of the docs had the same name as the implicit doc from the mainview ("new 1"), the unsaved doc backup snapshot for that doc had been lost due to invalid cloning of the mainview implicit empty doc.

Regression introduced by:
https://github.com/notepad-plus-plus/notepad-plus-plus/commit/a449c5e8042b46a7ccabfe374f5d68f20c682a33

Fix #18294, close #18295

PowerEditor/src/NppIO.cpp
@@ -2664,6 +2664,8 @@ bool Notepad_plus::loadSession(Session & session, bool isSnapshotMode, const wch
 	switchEditViewTo(SUB_VIEW);	//open files in sub
 	int subIndex2Update = -1;
 
+	bool shouldQueryMainViewForClones = (session.nbMainFiles() > 0); // only if it has real docs/tabs (a view has also its implicit doc/tab otherwise)
+
 	for (size_t k = 0 ; k < session.nbSubFiles() ; )
 	{
 		const wchar_t *pFn = session._subViewFiles[k]._fileName.c_str();
@@ -2685,7 +2687,10 @@ bool Notepad_plus::loadSession(Session & session, bool isSnapshotMode, const wch
 		if (doesFileExist(pFn) || (isSnapshotMode && doesFileExist(session._subViewFiles[k]._backupFilePath.c_str())))
 		{
 			//check if already open in main. If so, clone
-			BufferID clonedBuf = _mainDocTab.findBufferByName(pFn);
+			BufferID clonedBuf = BUFFER_INVALID;
+			if (shouldQueryMainViewForClones)
+				clonedBuf = _mainDocTab.findBufferByName(pFn);
+
 			if (clonedBuf != BUFFER_INVALID)
 			{
 				loadBufferIntoView(clonedBuf, SUB_VIEW);
ee0106b823575715842e4b45939ee355b8610ecf
Don Ho <don.h@free.fr>
2026-09-26 16:16:27 +0200

    Fix no warning display when deleting files from Notepad++ under Windows 7
    More details: https://community.notepad-plus-plus.org/topic/27668/notepad-release-8.9.8.1/7?_=1790424988795
    
    Fix https://community.notepad-plus-plus.org/topic/27668/notepad-release-8.9.8.1/4?_=1790424988793

--- PowerEditor/src/NppIO.cpp
+++ PowerEditor/src/NppIO.cpp
@@ -2352,13 +2352,7 @@ bool Notepad_plus::fileDelete(BufferID id)
 	Buffer * buf = MainFileManager.getBufferByID(bufferID);
 	const wchar_t *fileNamePath = buf->getFullPathName();
 
-	winVer winVersion = (NppParameters::getInstance()).getWinVersion();
-	bool goAhead = true;
-	if (winVersion >= WV_WIN8 || winVersion == WV_UNKNOWN)
-	{
-		// Windows 8 (and version after?) has no system alert, so we ask user's confirmation
-		goAhead = (doDeleteOrNot(fileNamePath) == IDOK);
-	}
+	bool goAhead = (doDeleteOrNot(fileNamePath) == IDOK);
 
 	if (goAhead)
 	{
